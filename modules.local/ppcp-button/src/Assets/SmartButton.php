8ff6ba320eed33970efc3314e115c8dc41d8eb77
David Remer <webmaster@websupporter.net>
2020-04-09 09:07:03 +0300

    add commit=false to script when not in checkout

--- modules.local/ppcp-button/src/Assets/SmartButton.php
+++ modules.local/ppcp-button/src/Assets/SmartButton.php
@@ -66,6 +66,9 @@ class SmartButton
             'client-id' => 'AcVzowpNCpTxFzLG7onQI4JD0sVcA0BkZv-D42qRZPv_gZ8cNfX9zGL_8bXmSu7cbJ5B2DH7sot8vDpw',
             'currency' => get_woocommerce_currency(),
         ];
+        if (! is_checkout()) {
+            $params['commit'] = 'false';
+        }
         $smartButtonUrl = add_query_arg($params, 'https://www.paypal.com/sdk/js');
 
         $localize = [
