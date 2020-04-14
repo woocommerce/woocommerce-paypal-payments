04cf4363a63b354fbffbfde442ef67775e004d58
Mészáros Róbert <hmm@meszarosrob.com>
2020-04-14 16:03:04 +0300

    Code Beautifier and Fixer

--- modules.local/ppcp-api-client/src/Entity/Authorization.php
+++ modules.local/ppcp-api-client/src/Entity/Authorization.php
@@ -13,6 +13,7 @@ class Authorization
         string $id,
         AuthorizationStatus $authorizationStatus
     ) {
+
         $this->id = $id;
         $this->authorizationStatus = $authorizationStatus;
     }
