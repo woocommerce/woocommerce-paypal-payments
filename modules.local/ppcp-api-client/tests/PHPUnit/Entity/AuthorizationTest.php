0494b5e71ee38ea5c7193fb2c37ccbcb430f1a0f
Mészáros Róbert <hmm@meszarosrob.com>
2020-04-15 16:42:27 +0300

    Test for Authorization

---
+++ modules.local/ppcp-api-client/tests/PHPUnit/Entity/AuthorizationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inpsyde\PayPalCommerce\ApiClient\Entity;
+
+use Inpsyde\PayPalCommerce\ApiClient\TestCase;
+
+class AuthorizationTest extends TestCase
+{
+    public function testIdandStatus()
+    {
+        $authorizationStatus = \Mockery::mock(AuthorizationStatus::class);
+        $testee = new Authorization('foo', $authorizationStatus);
+
+        $this->assertEquals('foo', $testee->id());
+        $this->assertEquals($authorizationStatus, $testee->status());
+    }
+
+    public function testToArray()
+    {
+        $authorizationStatus = \Mockery::mock(AuthorizationStatus::class);
+        $authorizationStatus->expects('name')->andReturn('CAPTURED');
+
+        $testee = new Authorization('foo', $authorizationStatus);
+
+        $expected = [
+            'id' => 'foo',
+            'status' => 'CAPTURED',
+        ];
+        $this->assertEquals($expected, $testee->toArray());
+    }
+}
