3d2d7255be9b170542e0a502a0624f84588a7f28
David Remer <webmaster@websupporter.net>
2020-04-28 15:08:27 +0300

    phpcs

--- src/PluginModule.php
+++ src/PluginModule.php
@@ -1,10 +1,10 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Inpsyde\PayPalCommerce;
 
 use Dhii\Container\ServiceProvider;
-use Dhii\Modular\Module\Exception\ModuleExceptionInterface;
 use Dhii\Modular\Module\ModuleInterface;
 use Interop\Container\ServiceProviderInterface;
 use Psr\Container\ContainerInterface;
