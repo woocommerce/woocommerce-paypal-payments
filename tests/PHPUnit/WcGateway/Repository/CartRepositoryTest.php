449d3440bdc136071fa5aac7ed352117f12747b3
David Naber <kontakt@dnaber.de>
2020-08-11 09:25:28 +0200

    Add empty test class for CartRepository

---
+++ tests/PHPUnit/WcGateway/Repository/CartRepositoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inpsyde\PayPalCommerce\ApiClient\Repository;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class CartRepositoryTest extends MockeryTestCase
+{
+    public function testAll()
+    {
+        self::markTestSkipped("Todo");
+    }
+}
