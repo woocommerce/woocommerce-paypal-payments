6ee4ca0b2a992a49d34086ce479c6ed2a35f8b8d
David Remer <webmaster@websupporter.net>
2020-09-24 08:02:32 +0300

    rename installation prefix to invoice prefix

--- modules/ppcp-wc-gateway/services.php
+++ modules/ppcp-wc-gateway/services.php
@@ -503,7 +503,7 @@ return array(
 				'gateway'      => 'paypal',
 			),
 			'prefix'                     => array(
-				'title'        => __( 'Installation prefix', 'paypal-payments-for-woocommerce' ),
+				'title'        => __( 'Invoice prefix', 'paypal-payments-for-woocommerce' ),
 				'type'         => 'text',
 				'desc_tip'     => true,
 				'description'  => __( 'If you use your PayPal account with more than one installation, please use a distinct prefix to seperate those installations. Please do not use numbers in your prefix.', 'paypal-payments-for-woocommerce' ),
