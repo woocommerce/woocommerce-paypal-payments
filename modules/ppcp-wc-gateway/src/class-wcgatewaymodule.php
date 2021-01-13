d8817134a95cbd5f5b209fb422e74952a4d055ff
dinamiko <e.castells@inpsyde.com>
2021-01-13 15:28:08 +0100

    Update `dhii\module-interface`

--- modules/ppcp-wc-gateway/src/class-wcgatewaymodule.php
+++ modules/ppcp-wc-gateway/src/class-wcgatewaymodule.php
@@ -53,7 +53,7 @@ class WcGatewayModule implements ModuleInterface {
 	 *
 	 * @param ContainerInterface|null $container The container.
 	 */
-	public function run( ContainerInterface $container = null ) {
+	public function run( ContainerInterface $container ) {
 		$this->register_payment_gateways( $container );
 		$this->register_order_functionality( $container );
 		$this->register_columns( $container );
