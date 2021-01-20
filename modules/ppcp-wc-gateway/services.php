1d17335a99059c5e76ae893ed7c45446ab5ae0bd
dinamiko <e.castells@inpsyde.com>
2021-01-20 09:48:47 +0100

    Fix phpcs errors

--- modules/ppcp-wc-gateway/services.php
+++ modules/ppcp-wc-gateway/services.php
@@ -1785,47 +1785,47 @@ return array(
 			unset( $fields['disable_funding']['options']['card'] );
 		}
 
-        /**
-         * Enable Pay in 3 for UK.
-         */
-        if ( 'GB' === $country ) {
-            $fields['message_heading'] = [
-                'heading'      => __( 'Pay Later Messaging on Checkout', 'woocommerce-paypal-payments' ),
-                'type'         => 'ppcp-heading',
-                'screens'      => array(
-                    State::STATE_PROGRESSIVE,
-                    State::STATE_ONBOARDED,
-                ),
-                'requirements' => array( 'messages' ),
-                'gateway'      => 'paypal',
-                'description'  => __( 'Display pay later messaging on your site for offers like Pay in 3, which lets customers pay with 3 interest-free monthly payments. We’ll show messages on your site to promote this feature for you. You may not promote pay later offers with any other content, marketing, or materials.', 'woocommerce-paypal-payments' ),
-                'class'        => array( 'ppcp-subheading' ),
-            ];
-            $fields['message_product_heading'] = [
-                'heading'      => __( 'Pay Later Messaging on Single Product Page', 'woocommerce-paypal-payments' ),
-                'type'         => 'ppcp-heading',
-                'screens'      => array(
-                    State::STATE_PROGRESSIVE,
-                    State::STATE_ONBOARDED,
-                ),
-                'requirements' => array( 'messages' ),
-                'gateway'      => 'paypal',
-                'description'  => __( 'Display pay later messaging on your site for offers like Pay in 3, which lets customers pay with 3 interest-free monthly payments. We’ll show messages on your site to promote this feature for you. You may not promote pay later offers with any other content, marketing, or materials.', 'woocommerce-paypal-payments' ),
-                'class'        => array( 'ppcp-subheading' ),
-            ];
-            $fields['message_cart_heading'] = [
-                'heading'      => __( 'Pay Later Messaging on Cart', 'woocommerce-paypal-payments' ),
-                'type'         => 'ppcp-heading',
-                'screens'      => array(
-                    State::STATE_PROGRESSIVE,
-                    State::STATE_ONBOARDED,
-                ),
-                'requirements' => array( 'messages' ),
-                'gateway'      => 'paypal',
-                'description'  => __( 'Display pay later messaging on your site for offers like Pay in 3, which lets customers pay with 3 interest-free monthly payments. We’ll show messages on your site to promote this feature for you. You may not promote pay later offers with any other content, marketing, or materials.', 'woocommerce-paypal-payments' ),
-                'class'        => array( 'ppcp-subheading' ),
-            ];
-        }
+		/**
+		 * Enable Pay in 3 for UK.
+		 */
+		if ( 'GB' === $country ) {
+			$fields['message_heading'] = array(
+				'heading'      => __( 'Pay Later Messaging on Checkout', 'woocommerce-paypal-payments' ),
+				'type'         => 'ppcp-heading',
+				'screens'      => array(
+					State::STATE_PROGRESSIVE,
+					State::STATE_ONBOARDED,
+				),
+				'requirements' => array( 'messages' ),
+				'gateway'      => 'paypal',
+				'description'  => __( 'Display pay later messaging on your site for offers like Pay in 3, which lets customers pay with 3 interest-free monthly payments. We’ll show messages on your site to promote this feature for you. You may not promote pay later offers with any other content, marketing, or materials.', 'woocommerce-paypal-payments' ),
+				'class'        => array( 'ppcp-subheading' ),
+			);
+			$fields['message_product_heading'] = array(
+				'heading'      => __( 'Pay Later Messaging on Single Product Page', 'woocommerce-paypal-payments' ),
+				'type'         => 'ppcp-heading',
+				'screens'      => array(
+					State::STATE_PROGRESSIVE,
+					State::STATE_ONBOARDED,
+				),
+				'requirements' => array( 'messages' ),
+				'gateway'      => 'paypal',
+				'description'  => __( 'Display pay later messaging on your site for offers like Pay in 3, which lets customers pay with 3 interest-free monthly payments. We’ll show messages on your site to promote this feature for you. You may not promote pay later offers with any other content, marketing, or materials.', 'woocommerce-paypal-payments' ),
+				'class'        => array( 'ppcp-subheading' ),
+			);
+			$fields['message_cart_heading'] = array(
+				'heading'      => __( 'Pay Later Messaging on Cart', 'woocommerce-paypal-payments' ),
+				'type'         => 'ppcp-heading',
+				'screens'      => array(
+					State::STATE_PROGRESSIVE,
+					State::STATE_ONBOARDED,
+				),
+				'requirements' => array( 'messages' ),
+				'gateway'      => 'paypal',
+				'description'  => __( 'Display pay later messaging on your site for offers like Pay in 3, which lets customers pay with 3 interest-free monthly payments. We’ll show messages on your site to promote this feature for you. You may not promote pay later offers with any other content, marketing, or materials.', 'woocommerce-paypal-payments' ),
+				'class'        => array( 'ppcp-subheading' ),
+			);
+		}
 
 		$dcc_applies = $container->get( 'api.helpers.dccapplies' );
 		/**
