77efce52dbc8b31e06c580a4f8714cb7c8aba53f
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-03-25 10:21:28 +0200

    allow to enable either vault or pay later message

---
+++ modules/ppcp-wc-gateway/src/Assets/class-settingspageassets.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Register and configure assets provided by this module.
+ */
+
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\WcGateway\Assets;
+
+class SettingsPageAssets
+{
+	/**
+	 * @var string
+	 */
+	private $module_url;
+
+	/**
+	 * Assets constructor.
+	 * @param string $module_url The url of this module.
+	 */
+	public function __construct(string $module_url)
+	{
+		$this->module_url = $module_url;
+	}
+
+	/**
+	 * Register assets provided by this module.
+	 */
+    public function register_assets(): void
+	{
+		if(is_admin() && ! is_ajax()){
+			$this->register_admin_assets();
+		}
+	}
+
+	/**
+	 * Register assets for admin pages.
+	 */
+	private function register_admin_assets(): void
+	{
+		add_action('admin_enqueue_scripts', function(){
+			wp_enqueue_script(
+				'ppcp-gateway-settings',
+				trailingslashit($this->module_url) . 'assets/js/gateway-settings.js',
+				[],
+				null,
+				true
+			);
+		});
+	}
+}
---
+++ modules/ppcp-wc-gateway/webpack.config.js
@@ -0,0 +1,24 @@
+const path         = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
+
+debugger
+
+module.exports = {
+    devtool: 'sourcemap',
+    mode: isProduction ? 'production' : 'development',
+    target: 'web',
+    entry: {
+        'gateway-settings': path.resolve('./resources/js/gateway-settings.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'assets/'),
+        filename: 'js/[name].js',
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }]
+    }
+};
