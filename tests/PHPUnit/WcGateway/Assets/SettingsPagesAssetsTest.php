c7fdfe1bcefa36efe5c6bf60be667fb04b34bea0
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-03-25 15:10:53 +0200

    add test

---
+++ tests/PHPUnit/WcGateway/Assets/SettingsPagesAssetsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WooCommerce\PayPalCommerce\WcGateway\Assets;
+
+use WooCommerce\PayPalCommerce\TestCase;
+use function Brain\Monkey\Functions\when;
+
+class SettingsPagesAssetsTest extends TestCase
+{
+	public function testRegisterAssets()
+	{
+		$moduleUrl = 'http://example.com/wp-content/plugins/woocommerce-paypal-payments/modules/ppcp-wc-gateway';
+		$modulePath = '/var/www/html/wp-content/plugins/woocommerce-paypal-payments/modules/ppcp-wc-gateway';
+
+		$testee = new SettingsPageAssets($moduleUrl, $modulePath);
+
+		when('is_admin')
+			->justReturn(true);
+		when('is_ajax')
+			->justReturn(false);
+
+		$testee->register_assets();
+
+		self::assertSame(has_action('admin_enqueue_scripts', "function()"), 10);
+	}
+}
