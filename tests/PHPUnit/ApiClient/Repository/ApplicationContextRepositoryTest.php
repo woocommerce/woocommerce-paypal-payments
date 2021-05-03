845a83acb4675a46c0c941bd8772d91c8fae55c0
dinamiko <e.castells@inpsyde.com>
2021-05-03 16:25:37 +0200

    Filter PayPal-supported language codes

---
+++ tests/PHPUnit/ApiClient/Repository/ApplicationContextRepositoryTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\ApiClient\Repository;
+
+use WooCommerce\PayPalCommerce\ApiClient\TestCase;
+use WooCommerce\PayPalCommerce\Button\Assets\SmartButton;
+use function Brain\Monkey\Functions\when;
+
+class ApplicationContextRepositoryTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function test_valid_bcp47_code($input, $output)
+    {
+        $testee = $this->getMockBuilder(ApplicationContextRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $method = new \ReflectionMethod($testee, 'valid_bcp47_code');
+        $method->setAccessible(true);
+
+        when('get_user_locale')->justReturn($input);
+
+        $this->assertSame($output, $method->invoke($testee));
+    }
+
+    public function provider()
+    {
+        return [
+            'de-DE' => ['de-DE', 'de-DE'],
+            'de-DE-formal' => ['de-DE-formal', 'de-DE'],
+        ];
+    }
+}
