57a49949c1c5d985941524287138fd1d79ce450d
Joshua Flowers <joshuatf@gmail.com>
2021-05-28 16:48:09 -0400

    Copy assets from resources using webpack copy plugin

--- modules/ppcp-onboarding/webpack.config.js
+++ modules/ppcp-onboarding/webpack.config.js
@@ -1,15 +1,14 @@
 const path = require( 'path' );
+const CopyPlugin = require( 'copy-webpack-plugin' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const WooCommerceDependencyExtractionWebpackPlugin = require( '@woocommerce/dependency-extraction-webpack-plugin' );
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		onboarding: path.resolve( './resources/js/onboarding.js' ),
 		'task-list-fill': path.resolve(
 			'./resources/js/task-list/task-list-fill.js'
 		),
-		settings: path.resolve( './resources/js/settings.js' ),
 	},
 	output: {
 		path: path.resolve( __dirname, 'assets/js/' ),
@@ -21,5 +20,16 @@ module.exports = {
 				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 		),
 		new WooCommerceDependencyExtractionWebpackPlugin(),
+		new CopyPlugin( [
+			{
+				from: path.resolve( './resources/js' ),
+				to: path.resolve( __dirname, 'assets/js' ),
+				ignore: [ 'task-list/**/*' ],
+			},
+			{
+				from: path.resolve( './resources/css' ),
+				to: path.resolve( __dirname, 'assets/css' ),
+			},
+		] ),
 	],
 };
