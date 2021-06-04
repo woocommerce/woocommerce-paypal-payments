f2b53f5983851b6391f5b87ae8f0fab75a51c700
Anton Ukhanev <xedin.unknown@gmail.com>
2021-06-04 14:28:03 +0200

    Decode response only if it is an array
    Avoids failure when response is `null`.
    https://github.com/XedinUnknown/woocommerce-paypal-payments/pull/1/checks?check_run_id=2745916372#step:7:15

--- modules/ppcp-api-client/src/Endpoint/IdentityToken.php
+++ modules/ppcp-api-client/src/Endpoint/IdentityToken.php
@@ -111,7 +111,7 @@ class IdentityToken {
 
 		$status_code = (int) wp_remote_retrieve_response_code( $response );
 		if ( 200 !== $status_code ) {
-			$json        = json_decode( $response['body'] );
+			$json        = is_array($response) ? json_decode( $response['body'] ) : null;
 			$error = new PayPalApiException(
 				$json,
 				$status_code
