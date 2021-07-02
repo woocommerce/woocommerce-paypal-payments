db4e1422d86cca406dd5657d6091fb2e67f8bacd
dinamiko <e.castells@inpsyde.com>
2021-07-02 12:36:59 +0200

    Remove unused class

--- modules/ppcp-wc-gateway/src/class-wcgatewaymodule.php
+++ modules/ppcp-wc-gateway/src/class-wcgatewaymodule.php
@@ -11,7 +11,6 @@ namespace WooCommerce\PayPalCommerce\WcGateway;
 
 use Dhii\Container\ServiceProvider;
 use Dhii\Modular\Module\ModuleInterface;
-use http\Exception\RuntimeException;
 use WooCommerce\PayPalCommerce\AdminNotices\Repository\Repository;
 use WooCommerce\PayPalCommerce\ApiClient\Helper\DccApplies;
 use WooCommerce\PayPalCommerce\ApiClient\Repository\PayPalRequestIdRepository;
