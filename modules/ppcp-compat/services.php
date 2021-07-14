3cc269e3c4206d970e4df1c57c6c25122b1ff5d8
Jorge A. Torres <jorge.torres@automattic.com>
2021-07-14 14:46:34 -0500

    Add compatibility module

---
+++ modules/ppcp-compat/services.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The compatibility module services.
+ *
+ * @package WooCommerce\PayPalCommerce\Compat
+ */
+
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\Compat;
+
+return array(
+);
