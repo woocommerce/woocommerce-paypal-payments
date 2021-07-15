f2ac9045bbb1093bd1e931559677c6796cb87f1d
dinamiko <e.castells@inpsyde.com>
2021-07-15 17:27:19 +0200

    Move hosted fields validation into `validityChange` event

--- modules/ppcp-button/resources/js/modules/ContextBootstrap/CheckoutBootstap.js
+++ modules/ppcp-button/resources/js/modules/ContextBootstrap/CheckoutBootstap.js
@@ -13,6 +13,10 @@ class CheckoutBootstap {
 
         this.render();
 
+        jQuery(document.body).on('updated_checkout', () => {
+            this.render()
+        });
+
         jQuery(document.body).
           on('updated_checkout payment_method_selected', () => {
               this.switchBetweenPayPalandOrderButton()
