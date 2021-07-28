0806ccf42963a784de5754478bd5841aa700a915
dinamiko <e.castells@inpsyde.com>
2021-07-28 12:51:00 +0200

    Fix unit test

--- tests/PHPUnit/StatusReport/RendererTest.php
+++ tests/PHPUnit/StatusReport/RendererTest.php
@@ -13,17 +13,20 @@ class RendererTest extends TestCase
         $items = [
             [
                 'label' => 'Foo',
-                'value' => 'Bar'
+                'description' => 'Bar',
+                'value' => 'Baz'
             ],
         ];
 
         when('esc_attr')->returnArg();
+        when('wc_help_tip')->returnArg();
 
         $testee = new Renderer();
         $result = $testee->render('Some title here', $items);
 
         self::assertStringContainsString('<h2>Some title here</h2>', $result);
         self::assertStringContainsString('<td data-export-label="Foo">Foo</td>', $result);
-        self::assertStringContainsString('<td>Bar</td>', $result);
+        self::assertStringContainsString('<td class="help">Bar</td>', $result);
+        self::assertStringContainsString('<td>Baz</td>', $result);
     }
 }
