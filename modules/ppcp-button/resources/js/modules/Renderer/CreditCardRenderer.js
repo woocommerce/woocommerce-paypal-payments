909699886cc7e03c40df166b7e2350ddd5df53fb
dinamiko <e.castells@inpsyde.com>
2021-09-20 11:14:35 +0200

    Check if hosted fields instance exist before setting attribute

--- modules/ppcp-button/resources/js/modules/Renderer/CreditCardRenderer.js
+++ modules/ppcp-button/resources/js/modules/Renderer/CreditCardRenderer.js
@@ -143,33 +143,37 @@ class CreditCardRenderer {
     }
 
     disableFields() {
-        this.currentHostedFieldsInstance.setAttribute({
-            field: 'number',
-            attribute: 'disabled'
-        })
-        this.currentHostedFieldsInstance.setAttribute({
-            field: 'cvv',
-            attribute: 'disabled'
-        })
-        this.currentHostedFieldsInstance.setAttribute({
-            field: 'expirationDate',
-            attribute: 'disabled'
-        })
+        if( this.currentHostedFieldsInstance) {
+            this.currentHostedFieldsInstance.setAttribute({
+                field: 'number',
+                attribute: 'disabled'
+            })
+            this.currentHostedFieldsInstance.setAttribute({
+                field: 'cvv',
+                attribute: 'disabled'
+            })
+            this.currentHostedFieldsInstance.setAttribute({
+                field: 'expirationDate',
+                attribute: 'disabled'
+            })
+        }
     }
 
     enableFields() {
-        this.currentHostedFieldsInstance.removeAttribute({
-            field: 'number',
-            attribute: 'disabled'
-        })
-        this.currentHostedFieldsInstance.removeAttribute({
-            field: 'cvv',
-            attribute: 'disabled'
-        })
-        this.currentHostedFieldsInstance.removeAttribute({
-            field: 'expirationDate',
-            attribute: 'disabled'
-        })
+        if( this.currentHostedFieldsInstance) {
+            this.currentHostedFieldsInstance.removeAttribute({
+                field: 'number',
+                attribute: 'disabled'
+            })
+            this.currentHostedFieldsInstance.removeAttribute({
+                field: 'cvv',
+                attribute: 'disabled'
+            })
+            this.currentHostedFieldsInstance.removeAttribute({
+                field: 'expirationDate',
+                attribute: 'disabled'
+            })
+        }
     }
 
     _submit(contextConfig) {
