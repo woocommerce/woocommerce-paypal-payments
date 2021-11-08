dddc8d6cf6e7e3355e412320c486bf69cf9a3378
dinamiko <e.castells@inpsyde.com>
2021-11-08 11:23:49 +0100

    Add init inside migrate action

--- modules/ppcp-vaulting/src/VaultingModule.php
+++ modules/ppcp-vaulting/src/VaultingModule.php
@@ -60,8 +60,13 @@ class VaultingModule implements ModuleInterface {
 		add_action(
 			'woocommerce_paypal_payments_gateway_migrate',
 			function () {
-				add_rewrite_endpoint( 'ppcp-paypal-payment-tokens', EP_PAGES );
-				flush_rewrite_rules();
+				add_action(
+					'init',
+					function () {
+						add_rewrite_endpoint( 'ppcp-paypal-payment-tokens', EP_PAGES );
+						flush_rewrite_rules();
+					}
+				);
 			}
 		);
 
