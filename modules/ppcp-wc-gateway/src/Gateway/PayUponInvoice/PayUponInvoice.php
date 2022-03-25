077203dd8efc70e21af71169b182fd8f1c7aab7e
dinamiko <e.castells@inpsyde.com>
2022-03-25 16:37:58 +0100

    Only add pui referrals data when ppcp in products

--- modules/ppcp-wc-gateway/src/Gateway/PayUponInvoice/PayUponInvoice.php
+++ modules/ppcp-wc-gateway/src/Gateway/PayUponInvoice/PayUponInvoice.php
@@ -38,19 +38,23 @@ class PayUponInvoice {
 		OrderEndpoint $order_endpoint,
 		LoggerInterface $logger
 	) {
-		$this->module_url = $module_url;
-		$this->fraud_net  = $fraud_net;
+		$this->module_url     = $module_url;
+		$this->fraud_net      = $fraud_net;
 		$this->order_endpoint = $order_endpoint;
-		$this->logger = $logger;
+		$this->logger         = $logger;
 	}
 
 	public function init() {
-		add_filter('ppcp_partner_referrals_data', function ($data) {
-			$data['products'][] = 'PAYMENT_METHODS';
-			$data['capabilities'][] = 'PAY_UPON_INVOICE';
-
-			return $data;
-		});
+		add_filter(
+			'ppcp_partner_referrals_data',
+			function ( $data ) {
+				if ( in_array( 'PPCP', $data['products'] ) ) {
+					$data['products'][]     = 'PAYMENT_METHODS';
+					$data['capabilities'][] = 'PAY_UPON_INVOICE';
+				}
+				return $data;
+			}
+		);
 
 		add_action(
 			'wp_footer',
@@ -62,54 +66,79 @@ class PayUponInvoice {
 			array( $this, 'register_assets' )
 		);
 
-		add_filter( 'woocommerce_billing_fields', function($billing_fields) {
-			$billing_fields['billing_birth_date'] = array(
-				'type'        => 'date',
-				'label'       => __('Birth date', 'woocommerce-paypal-payments'),
-				'class'       => array('form-row-wide'),
-				'required'    => true,
-				'clear'       => true,
-			);
+		add_filter(
+			'woocommerce_billing_fields',
+			function( $billing_fields ) {
+				$billing_fields['billing_birth_date'] = array(
+					'type'     => 'date',
+					'label'    => __( 'Birth date', 'woocommerce-paypal-payments' ),
+					'class'    => array( 'form-row-wide' ),
+					'required' => true,
+					'clear'    => true,
+				);
 
-			return $billing_fields;
-		});
-
-		add_filter( 'woocommerce_email_recipient_customer_on_hold_order', function( $recipient, $order, $email) {
-			if($order->get_payment_method() === PayUponInvoiceGateway::ID) {
-				return '';
+				return $billing_fields;
 			}
+		);
 
-			return $recipient;
-		}, 10, 3 );
+		add_filter(
+			'woocommerce_email_recipient_customer_on_hold_order',
+			function( $recipient, $order, $email ) {
+				if ( $order->get_payment_method() === PayUponInvoiceGateway::ID ) {
+					return '';
+				}
 
-		add_action('ppcp_payment_capture_completed_webhook_handler', function (WC_Order $wc_order, string $order_id) {
-			try {
-				$payment_instructions = $this->order_endpoint->order_payment_instructions($order_id);
-				$wc_order->update_meta_data( 'ppcp_ratepay_payment_instructions_payment_reference', $payment_instructions );
-				$this->logger->info("Ratepay payment instructions added to order #{$wc_order->get_id()}.");
-			} catch (RuntimeException $exception) {
-				$this->logger->error($exception->getMessage());
-			}
-		}, 10, 2);
+				return $recipient;
+			},
+			10,
+			3
+		);
 
-		add_filter( 'woocommerce_gateway_description', function($description, $id) {
-			if(PayUponInvoiceGateway::ID === $id) {
-				$description .= __( '<span style="float:left;margin-top:20px;margin-bottom:20px;">By clicking on the button, you agree to the <a href="https://www.ratepay.com/legal-payment-terms">terms of payment</a> and <a href="https://www.ratepay.com/legal-payment-dataprivacy">performance of a risk check</a> from the payment partner, Ratepay. You also agree to PayPal’s <a href="https://www.paypal.com/de/webapps/mpp/ua/privacy-full?locale.x=eng_DE&_ga=1.267010504.718583817.1563460395">privacy statement</a>. If your request to purchase upon invoice is accepted, the purchase price claim will be assigned to Ratepay, and you may only pay Ratepay, not the merchant.</span>', 'woocommerce-paypal-payments');
-			}
-			return $description;
-		}, 10, 2);
+		add_action(
+			'ppcp_payment_capture_completed_webhook_handler',
+			function ( WC_Order $wc_order, string $order_id ) {
+				try {
+					$payment_instructions = $this->order_endpoint->order_payment_instructions( $order_id );
+					$wc_order->update_meta_data( 'ppcp_ratepay_payment_instructions_payment_reference', $payment_instructions );
+					$this->logger->info( "Ratepay payment instructions added to order #{$wc_order->get_id()}." );
+				} catch ( RuntimeException $exception ) {
+					$this->logger->error( $exception->getMessage() );
+				}
+			},
+			10,
+			2
+		);
 
-		add_action('woocommerce_checkout_order_processed', function($order_id, $posted_data, $order) {
-			if($order->get_billing_country() !== 'DE') {
-				wp_send_json_error(array(
-					'result'   => 'failure',
-				));
-			}
-		}, 10, 3);
+		add_filter(
+			'woocommerce_gateway_description',
+			function( $description, $id ) {
+				if ( PayUponInvoiceGateway::ID === $id ) {
+					$description .= __( '<span style="float:left;margin-top:20px;margin-bottom:20px;">By clicking on the button, you agree to the <a href="https://www.ratepay.com/legal-payment-terms">terms of payment</a> and <a href="https://www.ratepay.com/legal-payment-dataprivacy">performance of a risk check</a> from the payment partner, Ratepay. You also agree to PayPal’s <a href="https://www.paypal.com/de/webapps/mpp/ua/privacy-full?locale.x=eng_DE&_ga=1.267010504.718583817.1563460395">privacy statement</a>. If your request to purchase upon invoice is accepted, the purchase price claim will be assigned to Ratepay, and you may only pay Ratepay, not the merchant.</span>', 'woocommerce-paypal-payments' );
+				}
+				return $description;
+			},
+			10,
+			2
+		);
+
+		add_action(
+			'woocommerce_checkout_order_processed',
+			function( $order_id, $posted_data, $order ) {
+				if ( $order->get_billing_country() !== 'DE' ) {
+					wp_send_json_error(
+						array(
+							'result' => 'failure',
+						)
+					);
+				}
+			},
+			10,
+			3
+		);
 	}
 
 	public function add_parameter_block() { ?>
-		<script type="application/json" fncls="fnparams-dede7cc5-15fd-4c75-a9f4-36c430ee3a99">{"f":"<?php echo esc_attr($this->fraud_net->sessionId()); ?>","s":"<?php echo esc_attr($this->fraud_net->sourceWebsiteId()); ?>"}</script>
+		<script type="application/json" fncls="fnparams-dede7cc5-15fd-4c75-a9f4-36c430ee3a99">{"f":"<?php echo esc_attr( $this->fraud_net->sessionId() ); ?>","s":"<?php echo esc_attr( $this->fraud_net->sourceWebsiteId() ); ?>"}</script>
 		<script type="text/javascript" src="https://c.paypal.com/da/r/fb.js"></script>
 		<?php
 	}
