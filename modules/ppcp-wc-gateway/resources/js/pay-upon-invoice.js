dcf1fd863c12c5bc61d1ef85e655c137d1f9d81b
dinamiko <e.castells@inpsyde.com>
2022-04-12 15:09:28 +0200

    Remove fraudnet session id if exist before creating it

--- modules/ppcp-wc-gateway/resources/js/pay-upon-invoice.js
+++ modules/ppcp-wc-gateway/resources/js/pay-upon-invoice.js
@@ -1,23 +1,42 @@
 window.addEventListener('load', function() {
-    setTimeout(() => {
+
+    const getSessionIdFromJson = () => {
+        const form = document.querySelector('form.checkout');
+        if(!form) {
+            return;
+        }
+
         const fncls = document.querySelector("[fncls='fnparams-dede7cc5-15fd-4c75-a9f4-36c430ee3a99']");
         const fncls_params = JSON.parse(fncls.textContent);
+
+        if(document.querySelector("[name='fraudnet-session-id']") !== null) {
+            document.querySelector("[name='fraudnet-session-id']").remove();
+        }
 
         const fraudnetSessionId = document.createElement('input');
         fraudnetSessionId.setAttribute('type', 'hidden');
         fraudnetSessionId.setAttribute('name', 'fraudnet-session-id');
         fraudnetSessionId.setAttribute('value', fncls_params.f);
 
-        const form = document.querySelector('form.checkout');
         form.appendChild(fraudnetSessionId);
+        console.log(fncls_params)
+    }
 
-        console.log(fncls_params.f)
-    }, 3000);
+    document.addEventListener('hosted_fields_loaded', (event) => {
+        getSessionIdFromJson();
+    });
+
+    getSessionIdFromJson();
 
     const replaceButtonLabel = () => {
-        const buttonLabel = document.querySelector('#place_order').textContent;
+        const form = document.querySelector('form.checkout');
+        if(!form) {
+            return;
+        }
+
+        const buttonLabel = document.querySelector('#place_order')?.textContent;
         const buttonLegalTextLabel = document.querySelector('#ppcp-legal-text-button-label');
-        if(buttonLabel && buttonLegalTextLabel) {
+        if (buttonLabel && buttonLegalTextLabel) {
             buttonLegalTextLabel.textContent = '"' + buttonLabel + '"';
         }
     }
