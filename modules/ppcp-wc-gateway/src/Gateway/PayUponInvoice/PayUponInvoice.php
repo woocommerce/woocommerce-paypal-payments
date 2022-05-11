ff2ff8d31f1231a3cf64ed1b09979be92aad6d7f
dinamiko <e.castells@inpsyde.com>
2022-05-11 10:37:16 +0200

    Update pui error notice

--- modules/ppcp-wc-gateway/src/Gateway/PayUponInvoice/PayUponInvoice.php
+++ modules/ppcp-wc-gateway/src/Gateway/PayUponInvoice/PayUponInvoice.php
@@ -337,7 +337,7 @@ class PayUponInvoice {
 
 					printf(
 						'<div class="notice notice-error"><p>%1$s</p></div>',
-						esc_html__( 'Could not enable gateway because Pay upon invoice is not active on PayPal.', 'woocommerce-paypal-payments' )
+						esc_html__( 'Could not enable gateway because the connected PayPal account is not activated for Pay upon Invoice. Reconnect your account while Onboard with Pay Upon Invoice is selected to try again.', 'woocommerce-paypal-payments' )
 					);
 				}
 			}
