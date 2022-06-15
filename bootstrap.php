c59683b5ea7aa4274c02d2fb7534585815bf88be
Alex P <alex.pantec@gmail.com>
2022-06-15 16:20:43 +0300

    Fix phpcs
    I guess the @since comments are not very critical for our hooks,
    and anyway it should not be an error, finding the first version for every hook would take lots of time now.
    not sure if there is a better way to suppress it in phpcs

--- bootstrap.php
+++ bootstrap.php
@@ -20,8 +20,10 @@ return function (
 ): ContainerInterface {
 	$modules = ( require "$root_dir/modules.php" )( $root_dir );
 
-	// Use this filter to add custom module or remove some of existing ones.
-	// Modules able to access container, add services and modify existing ones.
+	/**
+	 * Use this filter to add custom module or remove some of existing ones.
+	 * Modules able to access container, add services and modify existing ones.
+	 */
 	$modules = apply_filters( 'woocommerce_paypal_payments_modules', $modules );
 
 	$providers = array_map(
