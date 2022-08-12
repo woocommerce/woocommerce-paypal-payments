318f71b27c8e68f08bcf2297f76bb8c521607351
Narek Zakarian <narekzakarian@gmail.com>
2022-08-12 15:41:13 +0400

    Improve js functionality

--- modules/ppcp-order-tracking/resources/js/order-edit-page.js
+++ modules/ppcp-order-tracking/resources/js/order-edit-page.js
@@ -9,20 +9,25 @@ document.addEventListener(
             return;
         }
 
-        const submitButton = jQuery('.submit_tracking_info');
+        const transactionId = document.querySelector('.ppcp-tracking-transaction_id');
+        const trackingNumber = document.querySelector('.ppcp-tracking-tracking_number');
+        const status = document.querySelector('.ppcp-tracking-status');
+        const carrier = document.querySelector('.ppcp-tracking-carrier');
+        const orderId = document.querySelector('.ppcp-order_id');
+        const submitButton = document.querySelector('.submit_tracking_info');
 
-        jQuery('.submit_tracking_info').click(function() {
-            submitButton.prop( 'disabled', true );
+        submitButton.addEventListener('click', function (event) {
+            submitButton.setAttribute('disabled', 'disabled');
             fetch(config.ajax.tracking_info.endpoint, {
                 method: 'POST',
                 body: JSON.stringify({
                     nonce: config.ajax.tracking_info.nonce,
-                    transaction_id: jQuery('.ppcp-tracking-transaction_id').val(),
-                    tracking_number: jQuery('.ppcp-tracking-tracking_number').val(),
-                    status: jQuery('.ppcp-tracking-status').val(),
-                    carrier: jQuery('.ppcp-tracking-carrier').val(),
-                    order_id: jQuery('.ppcp-order_id').val(),
-                    action: jQuery('.submit_tracking_info').data('action'),
+                    transaction_id: transactionId ? transactionId.value : null,
+                    tracking_number: trackingNumber ? trackingNumber.value : null,
+                    status: status ? status.value : null,
+                    carrier: carrier ? carrier.value : null,
+                    order_id: orderId ? orderId.value : null,
+                    action: submitButton ? submitButton.dataset.action : null,
                 })
             }).then(function (res) {
                 return res.json();
@@ -35,8 +40,9 @@ document.addEventListener(
                 jQuery( "<span class='success tracking-info-message'>" + data.data.message + "</span>" ).insertAfter(submitButton);
                 setTimeout(()=> jQuery('.tracking-info-message').remove(),3000);
 
-                submitButton.html('update')
-                submitButton.prop( 'disabled', false );
+                submitButton.dataset.action = 'update';
+                submitButton.textContent = 'update';
+                submitButton.removeAttribute('disabled');
             });
         })
     },
