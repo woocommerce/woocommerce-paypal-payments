2140a85eedfba7fd3e448f70b29d274dae9cb08b
dinamiko <e.castells@inpsyde.com>
2022-09-09 12:32:38 +0200

    Update wc order status to failed for the given custom ids

--- modules/ppcp-webhooks/src/Handler/CheckoutOrderCompleted.php
+++ modules/ppcp-webhooks/src/Handler/CheckoutOrderCompleted.php
@@ -11,13 +11,15 @@ namespace WooCommerce\PayPalCommerce\Webhooks\Handler;
 
 use Psr\Log\LoggerInterface;
 use WooCommerce\PayPalCommerce\WcGateway\Gateway\PayUponInvoice\PayUponInvoiceGateway;
+use WP_REST_Request;
+use WP_REST_Response;
 
 /**
  * Class CheckoutOrderCompleted
  */
 class CheckoutOrderCompleted implements RequestHandler {
 
-	use PrefixTrait;
+	use PrefixTrait, RequestHandlerTrait;
 
 	/**
 	 * The logger.
@@ -51,97 +53,45 @@ class CheckoutOrderCompleted implements RequestHandler {
 	/**
 	 * Whether a handler is responsible for a given request or not.
 	 *
-	 * @param \WP_REST_Request $request The request.
+	 * @param WP_REST_Request $request The request.
 	 *
 	 * @return bool
 	 */
-	public function responsible_for_request( \WP_REST_Request $request ): bool {
+	public function responsible_for_request( WP_REST_Request $request ): bool {
 		return in_array( $request['event_type'], $this->event_types(), true );
 	}
 
 	/**
 	 * Responsible for handling the request.
 	 *
-	 * @param \WP_REST_Request $request The request.
+	 * @param WP_REST_Request $request The request.
 	 *
-	 * @return \WP_REST_Response
+	 * @return WP_REST_Response
 	 */
-	public function handle_request( \WP_REST_Request $request ): \WP_REST_Response {
-		$response   = array( 'success' => false );
-		$custom_ids = array_filter(
-			array_map(
-				static function ( array $purchase_unit ): string {
-					return isset( $purchase_unit['custom_id'] ) ?
-						(string) $purchase_unit['custom_id'] : '';
-				},
-				isset( $request['resource']['purchase_units'] ) ?
-					(array) $request['resource']['purchase_units'] : array()
-			),
-			static function ( string $order_id ): bool {
-				return ! empty( $order_id );
-			}
-		);
+	public function handle_request( WP_REST_Request $request ): WP_REST_Response {
+		$response = array( 'success' => false );
 
+		$custom_ids = $this->get_custom_ids_from_request( $request );
 		if ( empty( $custom_ids ) ) {
-			$message = sprintf(
-				// translators: %s is the PayPal webhook Id.
-				__(
-					'No order for webhook event %s was found.',
-					'woocommerce-paypal-payments'
-				),
-				isset( $request['id'] ) ? $request['id'] : ''
-			);
-			$this->logger->log(
-				'warning',
-				$message,
-				array(
-					'request' => $request,
-				)
-			);
-			$response['message'] = $message;
-			return rest_ensure_response( $response );
+			return $this->no_custom_ids_from_request( $request, $response );
 		}
 
-		$order_ids = array_map(
-			array(
-				$this,
-				'sanitize_custom_id',
-			),
-			$custom_ids
-		);
-		$args      = array(
-			'post__in' => $order_ids,
-			'limit'    => -1,
-		);
-		$wc_orders = wc_get_orders( $args );
+		$wc_orders = $this->get_wc_orders_from_custom_ids( $custom_ids );
 		if ( ! $wc_orders ) {
-			$message = sprintf(
-			// translators: %s is the PayPal order Id.
-				__( 'Order for PayPal order %s not found.', 'woocommerce-paypal-payments' ),
-				isset( $request['resource']['id'] ) ? $request['resource']['id'] : ''
-			);
-			$this->logger->log(
-				'warning',
-				$message,
-				array(
-					'request' => $request,
-				)
-			);
-			$response['message'] = $message;
-			return rest_ensure_response( $response );
+			return $this->no_wc_orders_from_custom_ids( $request, $response );
 		}
 
 		foreach ( $wc_orders as $wc_order ) {
 			if ( PayUponInvoiceGateway::ID === $wc_order->get_payment_method() ) {
 				continue;
 			}
-
 			if ( ! in_array( $wc_order->get_status(), array( 'pending', 'on-hold' ), true ) ) {
 				continue;
 			}
+
 			$wc_order->payment_complete();
-			$this->logger->log(
-				'info',
+
+			$this->logger->info(
 				sprintf(
 					// translators: %s is the order ID.
 					__(
@@ -149,15 +99,11 @@ class CheckoutOrderCompleted implements RequestHandler {
 						'woocommerce-paypal-payments'
 					),
 					(string) $wc_order->get_id()
-				),
-				array(
-					'request' => $request,
-					'order'   => $wc_order,
 				)
 			);
 		}
+
 		$response['success'] = true;
-		return rest_ensure_response( $response );
+		return new WP_REST_Response( $response );
 	}
-	// phpcs:enable Inpsyde.CodeQuality.FunctionLength.TooLong
 }
--- modules/ppcp-webhooks/src/Handler/CheckoutPaymentApprovalReversed.php
+++ modules/ppcp-webhooks/src/Handler/CheckoutPaymentApprovalReversed.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Handles CHECKOUT.PAYMENT-APPROVAL.REVERSED Webhook.
+ *
+ * @package WooCommerce\PayPalCommerce\Webhooks\Handler
+ */
+
+declare(strict_types=1);
 
 namespace WooCommerce\PayPalCommerce\Webhooks\Handler;
 
@@ -6,8 +13,13 @@ use Psr\Log\LoggerInterface;
 use WP_REST_Request;
 use WP_REST_Response;
 
-class CheckoutPaymentApprovalReversed implements RequestHandler
-{
+/**
+ * Class CheckoutPaymentApprovalReversed
+ */
+class CheckoutPaymentApprovalReversed implements RequestHandler {
+
+	use RequestHandlerTrait;
+
 	/**
 	 * The logger.
 	 *
@@ -16,28 +28,69 @@ class CheckoutPaymentApprovalReversed implements RequestHandler
 	protected $logger;
 
 	/**
+	 * CheckoutPaymentApprovalReversed constructor.
+	 *
 	 * @param LoggerInterface $logger The logger.
 	 */
-	public function __construct(LoggerInterface $logger)
-	{
+	public function __construct( LoggerInterface $logger ) {
 		$this->logger = $logger;
 	}
 
-	public function event_types(): array
-	{
+	/**
+	 * The event types a handler handles.
+	 *
+	 * @return string[]
+	 */
+	public function event_types(): array {
 		return array(
 			'CHECKOUT.PAYMENT-APPROVAL.REVERSED',
 		);
 	}
 
-	public function responsible_for_request(WP_REST_Request $request): bool
-	{
+	/**
+	 * Whether a handler is responsible for a given request or not.
+	 *
+	 * @param WP_REST_Request $request The request.
+	 *
+	 * @return bool
+	 */
+	public function responsible_for_request( WP_REST_Request $request ): bool {
 		return in_array( $request['event_type'], $this->event_types(), true );
 	}
 
-	public function handle_request(WP_REST_Request $request): WP_REST_Response
-	{
-		$this->logger->info(wc_print_r($request['resource'], true));
+	/**
+	 * Responsible for handling the request.
+	 *
+	 * @param WP_REST_Request $request The request.
+	 *
+	 * @return WP_REST_Response
+	 */
+	public function handle_request( WP_REST_Request $request ): WP_REST_Response {
+		$response = array( 'success' => false );
+
+		$custom_ids = $this->get_custom_ids_from_request( $request );
+		if ( empty( $custom_ids ) ) {
+			return $this->no_custom_ids_from_request( $request, $response );
+		}
+
+		$wc_orders = $this->get_wc_orders_from_custom_ids( $custom_ids );
+		if ( ! $wc_orders ) {
+			return $this->no_wc_orders_from_custom_ids( $request, $response );
+		}
+
+		foreach ( $wc_orders as $wc_order ) {
+			if ( in_array( $wc_order->get_status(), array( 'pending', 'on-hold' ), true ) ) {
+				$error_message = sprintf(
+					// translators: %1$s is the order id.
+					__( 'Failed to capture order %1$s through PayPal.', 'woocommerce-paypal-payments' ),
+					(string) $wc_order->get_id()
+				);
+
+				$this->logger->warning( 'CHECKOUT.PAYMENT-APPROVAL.REVERSED received. ' . $error_message );
+
+				$wc_order->update_status( 'failed', $error_message );
+			}
+		}
 
 		$response['success'] = true;
 		return new WP_REST_Response( $response );
