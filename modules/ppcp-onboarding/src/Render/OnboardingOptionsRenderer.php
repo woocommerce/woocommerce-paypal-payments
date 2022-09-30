1dec304595102c822b9740ec0c8b7ec584e607bf
dinamiko <e.castells@inpsyde.com>
2022-09-30 15:53:36 +0200

    Uncheck onboarding with pui checkbox by default

--- modules/ppcp-onboarding/src/Render/OnboardingOptionsRenderer.php
+++ modules/ppcp-onboarding/src/Render/OnboardingOptionsRenderer.php
@@ -80,10 +80,16 @@ class OnboardingOptionsRenderer {
 	 */
 	private function render_pui_option(): string {
 		if ( 'DE' === $this->country ) {
-			$checked = 'checked';
-			if ( $this->settings->has( 'ppcp-onboarding-pui' ) && $this->settings->get( 'ppcp-onboarding-pui' ) !== '1' ) {
+			$checked = '';
+			try {
+				$onboard_with_pui = $this->settings->get( 'ppcp-onboarding-pui' );
+				if ( $onboard_with_pui === '1' ) {
+					$checked = 'checked';
+				}
+			} catch ( NotFoundException $exception ) {
 				$checked = '';
 			}
+
 			return '<li><label><input type="checkbox" id="ppcp-onboarding-pui" ' . $checked . '> ' .
 				__( 'Onboard with Pay upon Invoice', 'woocommerce-paypal-payments' ) . '
 		</label></li>';
