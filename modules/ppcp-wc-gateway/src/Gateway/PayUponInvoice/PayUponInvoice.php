99aa80c3ecf506dbec315250abf86161bcc99f20
dinamiko <e.castells@inpsyde.com>
2022-10-10 15:10:00 +0200

    Use wc order update meta data intead of wp function

--- modules/ppcp-wc-gateway/src/Gateway/PayUponInvoice/PayUponInvoice.php
+++ modules/ppcp-wc-gateway/src/Gateway/PayUponInvoice/PayUponInvoice.php
@@ -248,7 +248,8 @@ class PayUponInvoice {
 						$wc_order->save_meta_data();
 						$paypal_fee = $breakdown->paypal_fee();
 						if ( $paypal_fee ) {
-							update_post_meta( $wc_order->get_id(), 'PayPal Transaction Key', $paypal_fee->value() );
+							$wc_order->update_meta_data( 'PayPal Transaction Key', $paypal_fee->value() );
+							$wc_order->save_meta_data();
 						}
 					}
 				} catch ( RuntimeException $exception ) {
