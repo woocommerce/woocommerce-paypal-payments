b1e53452229a481568e91cc3559981c62cea4172
Narek Zakarian <narekzakarian@gmail.com>
2022-11-17 15:28:20 +0400

    Change the text for Pay Later description source

--- modules/ppcp-wc-gateway/src/Gateway/PayPalGateway.php
+++ modules/ppcp-wc-gateway/src/Gateway/PayPalGateway.php
@@ -328,9 +328,11 @@ class PayPalGateway extends \WC_Payment_Gateway {
 
 		if ( $this->is_pay_later_tab() ) {
 			return sprintf(
-			// translators: %1$s and %2$s are </ br> HTML tags.
-				__( 'Let customers pay over time while you get paid up front — at no additional cost.%1$sPayPal’s pay later options are boosting merchant conversion rates and increasing cart sizes by 39%%. %1$sPayPal Q2 Earnings-2021.', 'woocommerce-paypal-payments' ),
-				'</ br>'
+			// translators: %1$s is </ br> HTML tag and %2$s, %3$s are the opening and closing of HTML <i> tag.
+				__( 'Let customers pay over time while you get paid up front — at no additional cost.%1$sPayPal’s pay later options are boosting merchant conversion rates and increasing cart sizes by 39%%. %2$s(PayPal Q2 Earnings-2021.)%3$s', 'woocommerce-paypal-payments' ),
+				'</ br>',
+				'<i>',
+				'</ i>'
 			);
 		}
 
