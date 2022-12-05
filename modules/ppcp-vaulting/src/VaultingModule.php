47cb0a2317140185bf3d5039cc13ccfedbc0bae1
emilicastells <e.castells@inpsyde.com>
2022-12-05 15:48:12 +0100

    Remove custom payment tokens implementation files

--- modules/ppcp-vaulting/src/VaultingModule.php
+++ modules/ppcp-vaulting/src/VaultingModule.php
@@ -56,61 +56,6 @@ class VaultingModule implements ModuleInterface {
 
 		$listener->listen();
 
-		add_filter(
-			'woocommerce_account_menu_items',
-			function( $menu_links ) {
-				$menu_links = array_slice( $menu_links, 0, 5, true )
-				+ array( 'ppcp-paypal-payment-tokens' => __( 'PayPal payments', 'woocommerce-paypal-payments' ) )
-				+ array_slice( $menu_links, 5, null, true );
-
-				return $menu_links;
-			},
-			40
-		);
-
-		add_action(
-			'init',
-			function () {
-				add_rewrite_endpoint( 'ppcp-paypal-payment-tokens', EP_PAGES );
-			}
-		);
-
-		add_action(
-			'woocommerce_paypal_payments_gateway_migrate',
-			function () {
-				add_action(
-					'init',
-					function () {
-						add_rewrite_endpoint( 'ppcp-paypal-payment-tokens', EP_PAGES );
-						flush_rewrite_rules();
-					}
-				);
-			}
-		);
-
-		add_action(
-			'woocommerce_paypal_payments_gateway_activate',
-			function () {
-				add_rewrite_endpoint( 'ppcp-paypal-payment-tokens', EP_PAGES );
-				flush_rewrite_rules();
-			}
-		);
-
-		add_action(
-			'woocommerce_account_ppcp-paypal-payment-tokens_endpoint',
-			function () use ( $container ) {
-				$payment_token_repository = $container->get( 'vaulting.repository.payment-token' );
-				$renderer                 = $container->get( 'vaulting.payment-tokens-renderer' );
-
-				$tokens = $payment_token_repository->all_for_user_id( get_current_user_id() );
-				if ( $tokens ) {
-					echo wp_kses_post( $renderer->render( $tokens ) );
-				} else {
-					echo wp_kses_post( $renderer->render_no_tokens() );
-				}
-			}
-		);
-
 		$subscription_helper = $container->get( 'subscription.helper' );
 		add_action(
 			'woocommerce_created_customer',
@@ -123,27 +68,6 @@ class VaultingModule implements ModuleInterface {
 				if ( $guest_customer_id && $subscription_helper->cart_contains_subscription() ) {
 					update_user_meta( $customer_id, 'ppcp_guest_customer_id', $guest_customer_id );
 				}
-			}
-		);
-
-		$asset_loader = $container->get( 'vaulting.assets.myaccount-payments' );
-		add_action(
-			'wp_enqueue_scripts',
-			function () use ( $asset_loader ) {
-				if ( is_account_page() && $this->is_payments_page() ) {
-					$asset_loader->enqueue();
-					$asset_loader->localize();
-				}
-			}
-		);
-
-		add_action(
-			'wc_ajax_' . DeletePaymentTokenEndpoint::ENDPOINT,
-			static function () use ( $container ) {
-				$endpoint = $container->get( 'vaulting.endpoint.delete' );
-				assert( $endpoint instanceof DeletePaymentTokenEndpoint );
-
-				$endpoint->handle_request();
 			}
 		);
 
