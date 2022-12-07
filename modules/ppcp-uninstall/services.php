5e2d4228efbb3788f0dcb1e73e35c9cb151579ce
Narek Zakarian <narekzakarian@gmail.com>
2022-12-07 19:10:08 +0400

    Add configuration services for option names and scheduled actions

--- modules/ppcp-uninstall/services.php
+++ modules/ppcp-uninstall/services.php
@@ -9,6 +9,37 @@ declare(strict_types=1);
 
 namespace WooCommerce\PayPalCommerce\Uninstall;
 
-return array(
+use WooCommerce\PayPalCommerce\ApiClient\Repository\PayPalRequestIdRepository;
+use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
+use WooCommerce\PayPalCommerce\WcGateway\Gateway\CardButtonGateway;
+use WooCommerce\PayPalCommerce\WcGateway\Gateway\CreditCardGateway;
+use WooCommerce\PayPalCommerce\WcGateway\Gateway\PayPalGateway;
+use WooCommerce\PayPalCommerce\WcGateway\Gateway\PayUponInvoice\PayUponInvoiceGateway;
+use WooCommerce\PayPalCommerce\WcGateway\Settings\Settings;
+use WooCommerce\PayPalCommerce\Webhooks\Status\WebhookSimulation;
+use WooCommerce\PayPalCommerce\Webhooks\WebhookRegistrar;
 
+return array(
+	'uninstall.ppcp-all-option-names'           => function( ContainerInterface $container ) : array {
+		return array(
+			$container->get( 'webhook.last-webhook-storage.key' ),
+			PayPalRequestIdRepository::KEY,
+			'woocommerce_ppcp-is_pay_later_settings_migrated',
+			'woocommerce_' . PayPalGateway::ID . '_settings',
+			'woocommerce_' . CreditCardGateway::ID . '_settings',
+			'woocommerce_' . PayUponInvoiceGateway::ID . '_settings',
+			'woocommerce_' . CardButtonGateway::ID . '_settings',
+			Settings::KEY,
+			'woocommerce-ppcp-version',
+			WebhookSimulation::OPTION_ID,
+			WebhookRegistrar::KEY,
+		);
+	},
+
+	'uninstall.ppcp-all-scheduled-action-names' => function( ContainerInterface $container ) : array {
+		return array(
+			'woocommerce_paypal_payments_check_pui_payment_captured',
+			'woocommerce_paypal_payments_check_saved_payment',
+		);
+	},
 );
