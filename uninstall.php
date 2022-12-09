309abd255cd67b9708eb2067f014631e5c26d95a
Narek Zakarian <narekzakarian@gmail.com>
2022-12-09 17:42:27 +0400

    Create uninstall plugin file.

---
+++ uninstall.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Uninstalls the plugin.
+ *
+ * @package WooCommerce\PayPalCommerce
+ */
+
+declare(strict_types=1);
+
+use WooCommerce\PayPalCommerce\Uninstall\ClearDatabaseInterface;
+use WooCommerce\PayPalCommerce\Vendor\Dhii\Container\CachingContainer;
+use WooCommerce\PayPalCommerce\WcGateway\Settings\Settings;
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die( 'Direct access not allowed.' );
+}
+
+( static function (): void {
+	$root_dir = __DIR__;
+
+	$autoload_filepath = "{$root_dir}/vendor/autoload.php";
+	if ( file_exists( $autoload_filepath ) && ! class_exists( '\WooCommerce\PayPalCommerce\PluginModule' ) ) {
+		require $autoload_filepath;
+	}
+
+	try {
+		$bootstrap = require "{$root_dir}/bootstrap.php";
+
+		$app_container = $bootstrap( $root_dir );
+		assert( $app_container instanceof CachingContainer );
+
+		$settings = $app_container->get( 'wcgateway.settings' );
+		assert( $settings instanceof Settings );
+
+		$should_clear_db = $settings->has( 'uninstall_clear_db_on_uninstall' ) && $settings->get( 'uninstall_clear_db_on_uninstall' );
+		if ( ! $should_clear_db ) {
+			return;
+		}
+
+		$clear_db = $app_container->get( 'uninstall.clear-db' );
+		assert( $clear_db instanceof ClearDatabaseInterface );
+
+		$option_names           = $app_container->get( 'uninstall.ppcp-all-option-names' );
+		$scheduled_action_names = $app_container->get( 'uninstall.ppcp-all-scheduled-action-names' );
+
+		$clear_db->delete_options( $option_names );
+		$clear_db->clear_scheduled_actions( $scheduled_action_names );
+	} catch ( Throwable $throwable ) {
+		$message = sprintf(
+			'<strong>Error:</strong> %s <br><pre>%s</pre>',
+			$throwable->getMessage(),
+			$throwable->getTraceAsString()
+		);
+
+		add_action(
+			'all_admin_notices',
+			static function () use ( $message ) {
+				$class = 'notice notice-error';
+				printf(
+					'<div class="%1$s"><p>%2$s</p></div>',
+					esc_attr( $class ),
+					wp_kses_post( $message )
+				);
+			}
+		);
+	}
+} )();
