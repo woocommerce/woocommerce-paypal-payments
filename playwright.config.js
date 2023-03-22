5a97bdfe877a20c9779970cad6daac8c99cc4c4c
Alex P <alex.pantec@gmail.com>
2023-03-22 11:22:22 +0200

    Increase playwright timeout
    sometimes it takes more time because of paypal loading

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: '.env.e2e' });
 
 const config = {
     testDir: './tests/playwright',
-    timeout: 30000,
+    timeout: 60000,
     use: {
         baseURL: process.env.BASEURL,
         ignoreHTTPSErrors: true,
