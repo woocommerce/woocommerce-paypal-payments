59b583b7aae267cdb9adc7bb08ab55cdcc5e051e
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2023-08-07 15:27:17 +0200

    Add products utility test

--- tests/Playwright/playwright.config.js
+++ tests/Playwright/playwright.config.js
@@ -7,6 +7,7 @@ require('dotenv').config({ path: '.env' });
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
+    timeout: 60000,
     testDir: './tests',
     /* Run tests in files in parallel */
     fullyParallel: true,
