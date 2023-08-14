32aaad4d32d0dbd997fd1fce7cf630cea609a119
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2023-08-14 14:30:57 +0200

    Fix phpcs

--- modules/ppcp-wc-gateway/src/Gateway/OXXO/OXXO.php
+++ modules/ppcp-wc-gateway/src/Gateway/OXXO/OXXO.php
@@ -161,7 +161,7 @@ class OXXO {
 				 * @psalm-suppress UndefinedClass
 				 * @psalm-suppress UndefinedFunction
 				 */
-				$screen = class_exists(CustomOrdersTableController::class) && wc_get_container()->get( CustomOrdersTableController::class )->custom_orders_table_usage_is_enabled()
+				$screen = class_exists( CustomOrdersTableController::class ) && wc_get_container()->get( CustomOrdersTableController::class )->custom_orders_table_usage_is_enabled()
 					? wc_get_page_screen_id( 'shop-order' )
 					: 'shop_order';
 
