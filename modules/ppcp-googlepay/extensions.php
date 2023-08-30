d6f6d05431c0d80989442054a97ac8b162d9d16c
Pedro Silva <p.silva@inpsyde.com>
2023-08-30 11:28:18 +0100

    Refactor APM admin settings

--- modules/ppcp-googlepay/extensions.php
+++ modules/ppcp-googlepay/extensions.php
@@ -14,29 +14,31 @@ use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
 
 
 return array(
-	'wcgateway.settings.fields' => static function ( ContainerInterface $container, array $fields ): array {
-		$fields['googlepay_heading'] = array(
-			'heading'      => __( 'Google Pay', 'woocommerce-paypal-payments' ),
-			'description'  =>
-				__(
-					'Customize the behaviour of the GooglePay button.',
-					'woocommerce-paypal-payments'
-				),
-			'type'         => 'ppcp-heading',
-			'screens'      => array( State::STATE_ONBOARDED ),
-			'requirements' => array(),
-			'gateway'      => 'paypal',
-		);
-		$fields['googlepay_button_enabled'] = array(
-			'title'        => __( 'Google Pay Button', 'woocommerce-paypal-payments' ),
-			'type'         => 'checkbox',
-			'label'        => __( 'Enable Google Pay button', 'woocommerce-paypal-payments' ),
-			'default'      => 'yes',
-			'screens'      => array( State::STATE_ONBOARDED ),
-			'gateway'      => 'paypal',
-			'requirements' => array(),
-		);
 
-		return $fields;
+	'wcgateway.settings.fields' => function ( ContainerInterface $container, array $fields ): array {
+		$insert_after = function( array $array, string $key, array $new ): array {
+			$keys = array_keys( $array );
+			$index = array_search( $key, $keys, true );
+			$pos = false === $index ? count( $array ) : $index + 1;
+
+			return array_merge( array_slice( $array, 0, $pos ), $new, array_slice( $array, $pos ) );
+		};
+
+		return $insert_after(
+			$fields,
+			'allow_card_button_gateway',
+			array(
+				'googlepay_button_enabled' => array(
+					'title'        => __( 'Google Pay Button', 'woocommerce-paypal-payments' ),
+					'type'         => 'checkbox',
+					'label'        => __( 'Enable Google Pay button', 'woocommerce-paypal-payments' ),
+					'default'      => 'yes',
+					'screens'      => array( State::STATE_ONBOARDED ),
+					'gateway'      => 'paypal',
+					'requirements' => array(),
+				),
+			)
+		);
 	},
+
 );
