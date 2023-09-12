e605da13a1fc6e84504cf4dcd50e7e38c94b1168
carmenmaymo <mayen22@gmail.com>
2023-09-12 14:07:30 +0200

    Reorder setting and add descriptions

--- modules/ppcp-applepay/extensions.php
+++ modules/ppcp-applepay/extensions.php
@@ -31,7 +31,15 @@ return array(
 				'applepay_button_enabled'          => array(
 					'title'             => __( 'Apple Pay Button', 'woocommerce-paypal-payments' ),
 					'type'              => 'checkbox',
-					'label'             => __( 'Enable Apple Pay button', 'woocommerce-paypal-payments' ),
+					'label'             => __( 'Enable Apple Pay button', 'woocommerce-paypal-payments' )
+						. '<p class="description">'
+						. sprintf(
+						// translators: %1$s and %2$s are the opening and closing of HTML <a> tag.
+							__( 'Buyers can use %1$sApple Pay%2$s to make payments on the web using a web browser.', 'woocommerce-paypal-payments' ),
+							'<a href="https://woocommerce.com/document/woocommerce-paypal-payments/#apple-pay" target="_blank">',
+							'</a>'
+						)
+						. '</p>',
 					'default'           => 'yes',
 					'screens'           => array( State::STATE_ONBOARDED ),
 					'gateway'           => 'paypal',
@@ -70,9 +78,30 @@ return array(
 					'gateway'      => 'paypal',
 					'requirements' => array(),
 				),
+				'applepay_button_type'             => array(
+					'title'        => str_repeat( '&nbsp;', 6 ) . __( 'Button Type', 'woocommerce-paypal-payments' ),
+					'type'         => 'select',
+					'desc_tip'     => true,
+					'description'  => __(
+						'This controls the label of the Apple Pay button.',
+						'woocommerce-paypal-payments'
+					),
+					'class'        => array(),
+					'input_class'  => array( 'wc-enhanced-select' ),
+					'default'      => 'pay',
+					'options'      => PropertiesDictionary::button_types(),
+					'screens'      => array( State::STATE_ONBOARDED ),
+					'gateway'      => 'paypal',
+					'requirements' => array(),
+				),
 				'applepay_button_color'            => array(
 					'title'        => str_repeat( '&nbsp;', 6 ) . __( 'Button Color', 'woocommerce-paypal-payments' ),
 					'type'         => 'select',
+					'desc_tip'     => true,
+					'description'  => __(
+						'Apple Pay payment buttons styles: black, white and white-outline. To provide contrast, use dark buttons on light backgrounds and light buttons on dark or colorful backgrounds.',
+						'woocommerce-paypal-payments'
+					),
 					'label'        => '',
 					'input_class'  => array( 'wc-enhanced-select' ),
 					'class'        => array(),
@@ -82,20 +111,14 @@ return array(
 					'gateway'      => 'paypal',
 					'requirements' => array(),
 				),
-				'applepay_button_type'             => array(
-					'title'        => str_repeat( '&nbsp;', 6 ) . __( 'Button Type', 'woocommerce-paypal-payments' ),
-					'type'         => 'select',
-					'class'        => array(),
-					'input_class'  => array( 'wc-enhanced-select' ),
-					'default'      => 'pay',
-					'options'      => PropertiesDictionary::button_types(),
-					'screens'      => array( State::STATE_ONBOARDED ),
-					'gateway'      => 'paypal',
-					'requirements' => array(),
-				),
 				'applepay_button_language'         => array(
 					'title'        => str_repeat( '&nbsp;', 6 ) . __( 'Button Language', 'woocommerce-paypal-payments' ),
 					'type'         => 'select',
+					'desc_tip'     => true,
+					'description'  => __(
+						'A type that indicates the languages and regions that you can specify for the Apple Pay button. The default value is the current language and region setting in a browser.',
+						'woocommerce-paypal-payments'
+					),
 					'class'        => array(),
 					'input_class'  => array( 'wc-enhanced-select' ),
 					'default'      => 'en',
