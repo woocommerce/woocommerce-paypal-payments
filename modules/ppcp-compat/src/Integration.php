347c412c407d725da3b77f540080c8028dfe98c6
Narek Zakarian <narekzakarian@gmail.com>
2023-10-16 15:19:28 +0400

    Create tracking integration for Germanized

--- modules/ppcp-compat/src/Integration.php
+++ modules/ppcp-compat/src/Integration.php
@@ -9,10 +9,10 @@ declare(strict_types=1);
 
 namespace WooCommerce\PayPalCommerce\Compat;
 
-interface Integration
-{
-    /**
-     * Integrates some (possibly external) service with PayPal Payments.
-     */
-    public function integrate(): void;
+interface Integration {
+
+	/**
+	 * Integrates some (possibly external) service with PayPal Payments.
+	 */
+	public function integrate(): void;
 }
