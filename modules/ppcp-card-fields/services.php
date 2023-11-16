9b58eca1b0378b237eb71a05ebb15bf116dc7478
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2023-11-16 17:01:39 +0100

    Fix phpcs

--- modules/ppcp-card-fields/services.php
+++ modules/ppcp-card-fields/services.php
@@ -13,9 +13,9 @@ use WooCommerce\PayPalCommerce\CardFields\Helper\CardFieldsApplies;
 use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
 
 return array(
-	'card-fields.eligible'                      => static function ( ContainerInterface $container ): bool {
+	'card-fields.eligible'                             => static function ( ContainerInterface $container ): bool {
 		$save_payment_methods_applies = $container->get( 'card-fields.helpers.save-payment-methods-applies' );
-		assert( $save_payment_methods_applies instanceof CardFieldsApplies);
+		assert( $save_payment_methods_applies instanceof CardFieldsApplies );
 
 		return $save_payment_methods_applies->for_country_currency();
 	},
@@ -26,7 +26,7 @@ return array(
 			$container->get( 'api.shop.country' )
 		);
 	},
-	'card-fields.supported-country-currency-matrix' => static function ( ContainerInterface $container ) : array {
+	'card-fields.supported-country-currency-matrix'    => static function ( ContainerInterface $container ) : array {
 		return apply_filters(
 			'woocommerce_paypal_payments_card_fields_supported_country_currency_matrix',
 			array(
