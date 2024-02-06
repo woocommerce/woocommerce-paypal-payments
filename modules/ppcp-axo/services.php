fc7b8022e8bf31d8f65caadb638531c6b3f2b127
Pedro Silva <p.silva@inpsyde.com>
2024-02-06 12:01:38 +0000

    Add AXO module

---
+++ modules/ppcp-axo/services.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The Axo module services.
+ *
+ * @package WooCommerce\PayPalCommerce\Axo
+ */
+
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\Axo;
+
+return array(
+
+);
---
+++ modules/ppcp-axo/src/AxoModule.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The Axo module.
+ *
+ * @package WooCommerce\PayPalCommerce\Axo
+ */
+
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\Axo;
+
+use WooCommerce\PayPalCommerce\Vendor\Dhii\Container\ServiceProvider;
+use WooCommerce\PayPalCommerce\Vendor\Dhii\Modular\Module\ModuleInterface;
+use WooCommerce\PayPalCommerce\Vendor\Interop\Container\ServiceProviderInterface;
+use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
+
+/**
+ * Class AxoModule
+ */
+class AxoModule implements ModuleInterface {
+	/**
+	 * {@inheritDoc}
+	 */
+	public function setup(): ServiceProviderInterface {
+		return new ServiceProvider(
+			require __DIR__ . '/../services.php',
+			require __DIR__ . '/../extensions.php'
+		);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function run( ContainerInterface $c ): void {
+
+	}
+
+	/**
+	 * Returns the key for the module.
+	 *
+	 * @return string|void
+	 */
+	public function getKey() {
+	}
+}
