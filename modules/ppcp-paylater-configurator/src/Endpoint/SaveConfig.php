155677934abbd831a9f95b7b0a66dbdc7e448c68
Narek Zakarian <narekzakarian@gmail.com>
2024-04-02 15:09:52 +0400

    Fix saving the block config

--- modules/ppcp-paylater-configurator/src/Endpoint/SaveConfig.php
+++ modules/ppcp-paylater-configurator/src/Endpoint/SaveConfig.php
@@ -99,9 +99,12 @@ class SaveConfig {
 		$this->settings->set( 'pay_later_messaging_enabled', true );
 
 		$enabled_locations = array();
-
 		foreach ( $config as $placement => $data ) {
 			$this->save_config_for_location( $data, $placement );
+
+			if ( $placement === 'custom_placement' ) {
+				$data = $data[0] ?? array();
+			}
 
 			if ( $data['status'] === 'enabled' ) {
 				$enabled_locations[] = $placement;
