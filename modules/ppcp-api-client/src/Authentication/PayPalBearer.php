526364a8ad64e1f593a24a96a908a41b433638aa
Pedro Silva <p.silva@inpsyde.com>
2024-04-12 16:20:27 +0100

    Revert AXO bearer

--- modules/ppcp-api-client/src/Authentication/PayPalBearer.php
+++ modules/ppcp-api-client/src/Authentication/PayPalBearer.php
@@ -118,7 +118,7 @@ class PayPalBearer implements Bearer {
 	private function newBearer(): Token {
 		$key    = $this->settings->has( 'client_id' ) && $this->settings->get( 'client_id' ) ? $this->settings->get( 'client_id' ) : $this->key;
 		$secret = $this->settings->has( 'client_secret' ) && $this->settings->get( 'client_secret' ) ? $this->settings->get( 'client_secret' ) : $this->secret;
-		$url    = trailingslashit( $this->host ) . 'v1/oauth2/token?grant_type=client_credentials&intent=sdk_init';
+		$url    = trailingslashit( $this->host ) . 'v1/oauth2/token?grant_type=client_credentials';
 
 		$args     = array(
 			'method'  => 'POST',
