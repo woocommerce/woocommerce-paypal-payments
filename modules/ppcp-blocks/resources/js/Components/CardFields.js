3f619608809f0ec1fcc196c52f3fd88ba02ac5c9
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2024-04-17 17:35:30 +0200

    Add card fields component (WIP)

--- modules/ppcp-blocks/resources/js/Components/CardFields.js
+++ modules/ppcp-blocks/resources/js/Components/CardFields.js
@@ -1,13 +1,82 @@
 import {useEffect} from '@wordpress/element';
 
-export function CardFields({config}) {
+export function CardFields({config, eventRegistration, emitResponse}) {
+    const {onPaymentSetup, onCheckoutFail, onCheckoutValidation} = eventRegistration;
+    const {responseTypes} = emitResponse;
+
+    const createOrder = async () => {
+        try {
+            const res = await fetch(config.scriptData.ajax.create_order.endpoint, {
+                method: 'POST',
+                credentials: 'same-origin',
+                body: JSON.stringify({
+                    nonce: config.scriptData.ajax.create_order.nonce,
+                    context: config.scriptData.context,
+                    payment_method: 'ppcp-credit-card-gateway',
+                    createaccount: false
+                }),
+            });
+
+            const json = await res.json();
+            if (!json.success) {
+                console.error(json)
+            }
+
+            console.log(json.data.id)
+
+            return json.data.id;
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleApprove = async (data, actions) => {
+        try {
+            const res = await fetch(config.scriptData.ajax.approve_order.endpoint, {
+                method: 'POST',
+                credentials: 'same-origin',
+                body: JSON.stringify({
+                    nonce: config.scriptData.ajax.approve_order.nonce,
+                    order_id: data.orderID,
+                })
+            });
+
+            const json = await res.json();
+            if (!json.success) {
+                console.error(json)
+            }
+
+            console.log(json)
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     const cardField = paypal.CardFields({
-        createOrder: () => {},
-        onApprove: () => {},
+        createOrder: () => {
+            return createOrder();
+        },
+        onApprove: (data, actions) => {
+            return handleApprove(data, actions);
+        },
         onError: function (error) {
             console.error(error)
         }
     });
+
+    useEffect(() => {
+        const unsubscribe = onPaymentSetup(() => {
+
+            cardField.submit()
+                .catch((error) => {
+                    console.error(error)
+                    return {type: responseTypes.ERROR};
+                });
+
+            return true;
+        });
+        return unsubscribe;
+    }, [onPaymentSetup]);
 
     useEffect(() => {
         if (cardField.isEligible()) {
