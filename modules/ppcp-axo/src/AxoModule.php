f9066b15aedaea3a4b74030b2d09e5d5605de978
Daniel Dudzic <d.dudzic@syde.com>
2024-05-13 16:23:16 +0200

    Refine the condition for setting Fastlane as default

--- modules/ppcp-axo/src/AxoModule.php
+++ modules/ppcp-axo/src/AxoModule.php
@@ -23,6 +23,7 @@ use WooCommerce\PayPalCommerce\Vendor\Interop\Container\ServiceProviderInterface
 use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
 use WooCommerce\PayPalCommerce\WcGateway\Gateway\CreditCardGateway;
 use WooCommerce\PayPalCommerce\WcGateway\Settings\Settings;
+use WooCommerce\PayPalCommerce\WcGateway\Helper\CartCheckoutDetector;
 
 /**
  * Class AxoModule
@@ -202,8 +203,11 @@ class AxoModule implements ModuleInterface {
 				// Set Axo as the default payment method on checkout for guest customers.
 				add_action(
 					'template_redirect',
-					function () {
-						if ( ! is_user_logged_in() && is_checkout() && ! is_wc_endpoint_url() ) {
+					function () use ( $c ) {
+						$settings = $c->get( 'wcgateway.settings' );
+						assert( $settings instanceof Settings );
+
+						if ( $this->should_render_fastlane( $settings ) ) {
 							WC()->session->set( 'chosen_payment_method', AxoGateway::ID );
 						}
 					}
@@ -270,11 +274,22 @@ class AxoModule implements ModuleInterface {
 	 * @return bool
 	 */
 	private function hide_credit_card_when_using_fastlane( array $methods, Settings $settings ): bool {
+		return $this->should_render_fastlane( $settings ) && isset( $methods[ CreditCardGateway::ID ] );
+	}
+
+	/**
+	 * Condition to evaluate if Fastlane should be rendered.
+	 *
+	 * Fastlane should only render on the classic checkout, when Fastlane is enabled in the settings and also only for guest customers.
+	 *
+	 * @param Settings $settings The settings.
+	 * @return bool
+	 */
+	private function should_render_fastlane( Settings $settings): bool {
 		$is_axo_enabled = $settings->has( 'axo_enabled' ) && $settings->get( 'axo_enabled' ) ?? false;
 
-		return ! is_admin()
-			&& is_user_logged_in() === false
-			&& isset( $methods[ CreditCardGateway::ID ] )
+		return ! is_user_logged_in()
+			&& CartCheckoutDetector::has_classic_checkout()
 			&& $is_axo_enabled;
 	}
 }
