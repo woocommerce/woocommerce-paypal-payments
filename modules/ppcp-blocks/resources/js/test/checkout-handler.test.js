986918f86bcfbd25ace29d9614e9c49e65f82615
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2024-06-22 15:53:46 +0200

    Add test for checkout handler component

--- modules/ppcp-blocks/resources/js/test/checkout-handler.test.js
+++ modules/ppcp-blocks/resources/js/test/checkout-handler.test.js
@@ -1,6 +1,15 @@
-import { render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
 import {CheckoutHandler} from "../Components/checkout-handler";
 
-test('checkout handler', () => {
-    render(<CheckoutHandler getCardFieldsForm={() => {}}/>)
+test('checkout handler label display given text', async () => {
+    render(
+        <CheckoutHandler
+            getCardFieldsForm={() => {}}
+            saveCardText="Foo"
+            is_vaulting_enabled={true}
+        />
+    );
+
+    await expect(screen.getByLabelText('Foo')).toBeInTheDocument();
 });
