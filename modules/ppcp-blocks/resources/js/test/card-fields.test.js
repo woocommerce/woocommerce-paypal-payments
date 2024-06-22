429e7b99a4b212b02c7ce7c088c1ee1fd792a66d
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2024-06-22 16:51:56 +0200

    Remove card fields test for now

--- modules/ppcp-blocks/resources/js/test/card-fields.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
-
-import {CardFields} from "../Components/card-fields";
-
-test('card fields component', () => {
-    render(<CardFields />);
-});
