d3cad1105a4e114d1040b8934d136909c2ee679a
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2024-06-22 16:11:00 +0200

    Add test for checkout handler component

--- modules/ppcp-blocks/resources/js/test/checkout-handler.test.js
+++ modules/ppcp-blocks/resources/js/test/checkout-handler.test.js
@@ -1,8 +1,9 @@
 import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import {CheckoutHandler} from "../Components/checkout-handler";
 
-test('checkout handler label display given text', async () => {
+test('checkbox label displays the given text', async () => {
     render(
         <CheckoutHandler
             getCardFieldsForm={() => {}}
@@ -12,4 +13,22 @@ test('checkout handler label display given text', async () => {
     );
 
     await expect(screen.getByLabelText('Foo')).toBeInTheDocument();
+});
+
+test('click checkbox calls function passing checked value', async () => {
+    const getSavePayment = jest.fn();
+
+    render(
+        <CheckoutHandler
+            getSavePayment={getSavePayment}
+            getCardFieldsForm={() => {}}
+            saveCardText="Foo"
+            is_vaulting_enabled={true}
+        />
+    );
+
+    await userEvent.click(screen.getByLabelText('Foo'));
+
+    await expect(getSavePayment.mock.calls).toHaveLength(1);
+    await expect(getSavePayment.mock.calls[0][0]).toBe(true);
 });
