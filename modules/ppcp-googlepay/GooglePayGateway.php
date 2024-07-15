311d513fbf224b13fd9b9d4ad16df1340c0f6526
Emili Castells Guasch <e.castells@inpsyde.com>
2024-07-15 11:40:09 +0200

    Add WC gateway

---
+++ modules/ppcp-googlepay/GooglePayGateway.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Google Pay Payment Gateway
+ *
+ * @package WooCommerce\PayPalCommerce\Googlepay
+ */
+
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\Googlepay;
+
+use WC_Payment_Gateway;
+
+class GooglePayGateway extends WC_Payment_Gateway {
+	const ID = 'ppcp-googlepay';
+
+
+}
