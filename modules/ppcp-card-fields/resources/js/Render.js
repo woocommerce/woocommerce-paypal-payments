e00a4a302c47d782ec5d98160feeb59eb4ce0618
Philipp Stracker <p.stracker@syde.com>
2024-09-09 12:16:27 +0200

    Replace existing logic with new helper method

--- modules/ppcp-card-fields/resources/js/Render.js
+++ modules/ppcp-card-fields/resources/js/Render.js
@@ -16,51 +16,20 @@ function renderField( cardField, inputField ) {
 }
 
 export function renderFields( cardFields ) {
-	const nameField = document.getElementById(
-		'ppcp-credit-card-gateway-card-name'
+	renderField(
+		cardFields.NameField,
+		document.getElementById( 'ppcp-credit-card-gateway-card-name' )
 	);
-	if ( nameField && nameField.hidden !== true ) {
-		const styles = cardFieldStyles( nameField );
-		cardFields
-			.NameField( { style: { input: styles } } )
-			.render( nameField.parentNode );
-		hide( nameField, true );
-		nameField.hidden = true;
-	}
-
-	const numberField = document.getElementById(
-		'ppcp-credit-card-gateway-card-number'
+	renderField(
+		cardFields.NumberField,
+		document.getElementById( 'ppcp-credit-card-gateway-card-number' )
 	);
-	if ( numberField && numberField.hidden !== true ) {
-		const styles = cardFieldStyles( numberField );
-		cardFields
-			.NumberField( { style: { input: styles } } )
-			.render( numberField.parentNode );
-		hide( numberField, true );
-		numberField.hidden = true;
-	}
-
-	const expiryField = document.getElementById(
-		'ppcp-credit-card-gateway-card-expiry'
+	renderField(
+		cardFields.ExpiryField,
+		document.getElementById( 'ppcp-credit-card-gateway-card-expiry' )
 	);
-	if ( expiryField && expiryField.hidden !== true ) {
-		const styles = cardFieldStyles( expiryField );
-		cardFields
-			.ExpiryField( { style: { input: styles } } )
-			.render( expiryField.parentNode );
-		hide( expiryField, true );
-		expiryField.hidden = true;
-	}
-
-	const cvvField = document.getElementById(
-		'ppcp-credit-card-gateway-card-cvc'
+	renderField(
+		cardFields.CVVField,
+		document.getElementById( 'ppcp-credit-card-gateway-card-cvc' )
 	);
-	if ( cvvField && cvvField.hidden !== true ) {
-		const styles = cardFieldStyles( cvvField );
-		cardFields
-			.CVVField( { style: { input: styles } } )
-			.render( cvvField.parentNode );
-		hide( cvvField, true );
-		cvvField.hidden = true;
-	}
 }
