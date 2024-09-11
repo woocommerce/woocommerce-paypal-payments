4fb62c5c72f872ad6eeef455874e424e0f62a72f
Narek Zakarian <narekzakarian@gmail.com>
2024-09-11 19:41:12 +0400

    Add stubs for WC Task

---
+++ tests/stubs/Task.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace stubs;
+
+class Task
+{
+
+	/**
+	 * ID.
+	 *
+	 * @return string
+	 */
+	abstract public function get_id();
+
+	/**
+	 * Title.
+	 *
+	 * @return string
+	 */
+	abstract public function get_title();
+
+	/**
+	 * Content.
+	 *
+	 * @return string
+	 */
+	abstract public function get_content();
+
+	/**
+	 * Time.
+	 *
+	 * @return string
+	 */
+	abstract public function get_time();
+}
