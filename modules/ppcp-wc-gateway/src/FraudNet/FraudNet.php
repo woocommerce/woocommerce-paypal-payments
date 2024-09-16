660ce00887b8b12863616f7b2c699427e8c646b6
Emili Castells Guasch <e.castells@inpsyde.com>
2024-09-16 12:42:25 +0200

    Fix psalm

--- modules/ppcp-wc-gateway/src/FraudNet/FraudNet.php
+++ modules/ppcp-wc-gateway/src/FraudNet/FraudNet.php
@@ -40,15 +40,16 @@ class FraudNet {
 			return '';
 		}
 
-		if ( WC()->session->get( 'ppcp_fraudnet_session_id' ) ) {
-			return WC()->session->get( 'ppcp_fraudnet_session_id' );
+		$fraudnet_session_id = WC()->session->get( 'ppcp_fraudnet_session_id' );
+		if ( is_string( $fraudnet_session_id ) && $fraudnet_session_id !== '' ) {
+			return $fraudnet_session_id;
 		}
 
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		if ( isset( $_GET['pay_for_order'] ) && 'true' === $_GET['pay_for_order'] ) {
+		if ( isset( $_GET['pay_for_order'] ) && $_GET['pay_for_order'] === 'true' ) {
 			// phpcs:ignore WordPress.Security.NonceVerification.Missing
 			$pui_pay_for_order_session_id = wc_clean( wp_unslash( $_POST['pui_pay_for_order_session_id'] ?? '' ) );
-			if ( $pui_pay_for_order_session_id && '' !== $pui_pay_for_order_session_id ) {
+			if ( is_string( $pui_pay_for_order_session_id ) && $pui_pay_for_order_session_id !== '' ) {
 				return $pui_pay_for_order_session_id;
 			}
 		}
