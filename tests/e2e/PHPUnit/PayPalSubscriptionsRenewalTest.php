3d018945f49d2227d834fa6dfd5b82de5793239f
Emili Castells Guasch <e.castells@inpsyde.com>
2024-09-17 12:05:35 +0200

    Create subscription via rest api for testing

--- tests/e2e/PHPUnit/PayPalSubscriptionsRenewalTest.php
+++ tests/e2e/PHPUnit/PayPalSubscriptionsRenewalTest.php
@@ -24,18 +24,73 @@ class PayPalSubscriptionsRenewalTest extends TestCase
 
 	private function createSubscription()
 	{
-		$order = wc_create_order();
-		$order->set_customer_id(1);
-		$order->save();
+		$args = [
+			'method' => 'POST',
+			'headers' => [
+				'Authorization' => 'Basic ' . base64_encode( 'admin:admin' ),
+				'Content-Type' => 'application/json',
+			],
+			'body' => wp_json_encode([
+				'customer_id' => 1,
+				'set_paid' => true,
+				'payment_method' => 'ppcp-gateway',
+				'billing' => [
+					'first_name' => 'John',
+					'last_name' => 'Doe',
+					'address_1' => '969 Market',
+					'address_2' => '',
+					'city' => 'San Francisco',
+					'state' => 'CA',
+					'postcode' => '94103',
+					'country' => 'US',
+					'email' => 'john.doe@example.com',
+					'phone' => '(555) 555-5555'
+				],
+				'line_items' => [
+					[
+						'product_id' => 156,
+						'quantity' => 1
+					]
+				],
+			]),
+		];
 
-		return wcs_create_subscription(
-			array(
-				'order_id' => $order->get_id(),
+		$response = wp_remote_request(
+			'https://woocommerce-paypal-payments.ddev.site/wp-json/wc/v3/orders',
+			$args
+		);
+
+		$body = json_decode( $response['body'] );
+
+		$args = [
+			'method' => 'POST',
+			'headers' => [
+				'Authorization' => 'Basic ' . base64_encode( 'admin:admin' ),
+				'Content-Type' => 'application/json',
+			],
+			'body' => wp_json_encode([
+				'parent_id' => $body->id,
+				'customer_id' => 1,
 				'status' => 'active',
 				'billing_period' => 'day',
-				'billing_interval' => '1',
-				'customer_id' => $order->get_customer_id(),
-			)
+				'billing_interval' => 1,
+				'payment_method' => 'ppcp-gateway',
+				'line_items' => [
+					[
+						'product_id' => $_ENV['PAYPAL_SUBSCRIPTIONS_PRODUCT_ID'],
+						'quantity' => 1
+					]
+				],
+			]),
+		];
+
+		$response = wp_remote_request(
+			'https://woocommerce-paypal-payments.ddev.site/wp-json/wc/v3/subscriptions?per_page=1',
+			$args
 		);
+
+		$body = json_decode( $response['body'] );
+
+		return wcs_get_subscription($body->id);
 	}
 }
