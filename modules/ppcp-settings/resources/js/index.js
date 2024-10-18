5e803643cbb1ad09fef6e06bf7e17a4fe8f2ecf4
Emili Castells Guasch <e.castells@inpsyde.com>
2024-10-18 17:36:54 +0200

    Fix phpunit

--- modules/ppcp-settings/resources/js/index.js
+++ modules/ppcp-settings/resources/js/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { App } from './App';
-
-ReactDOM.createRoot(
-	document.getElementById( 'ppcp-settings-container' )
-).render( <App /> );
-
-/*
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 createRoot( document.getElementById( 'ppcp-settings-container' ) ).render(
 	<App />
 );
-
- */
