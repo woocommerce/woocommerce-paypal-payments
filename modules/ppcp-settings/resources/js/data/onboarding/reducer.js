e20a6fdbce32679280aef815c8e3bd56f8503107
Philipp Stracker <p.stracker@syde.com>
2024-10-23 18:14:25 +0200

    🎨 Reorganize code

--- modules/ppcp-settings/resources/js/data/onboarding/reducer.js
+++ modules/ppcp-settings/resources/js/data/onboarding/reducer.js
@@ -14,16 +14,18 @@ export const onboardingReducer = (
 	{ type, ...action }
 ) => {
 	switch ( type ) {
-		case ACTION_TYPES.SET_ONBOARDING_DETAILS:
-			return {
-				...state,
-				data: action.payload,
-			};
-
+		// Transient data.
 		case ACTION_TYPES.SET_IS_SAVING_ONBOARDING_DETAILS:
 			return {
 				...state,
 				isSaving: action.isSaving,
+			};
+
+		// Persistent data.
+		case ACTION_TYPES.SET_ONBOARDING_DETAILS:
+			return {
+				...state,
+				data: action.payload,
 			};
 
 		case ACTION_TYPES.SET_ONBOARDING_STEP:
