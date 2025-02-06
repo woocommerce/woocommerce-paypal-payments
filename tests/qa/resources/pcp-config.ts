fe9f827ebf16ba26a328b08de20f861df96c75bf
Misha Utkin <m.utkin@inpsyde.com>
2025-02-06 12:01:43 +0100

    Remove irrelevant merchants

--- tests/qa/resources/pcp-config.ts
+++ tests/qa/resources/pcp-config.ts
@@ -1,26 +1,8 @@
 /**
  * Internal dependencies
  */
-import { merchants, Pcp } from '.';
-
-const country = 'germany';
-
-export const pcpConfigDefault: Pcp.Admin.Config = {
-	merchant: merchants[ country ],
-};
-
-export const pcpConfigGermany = {
-	...pcpConfigDefault,
-	merchant: merchants.germany,
-	enablePayUponInvoice: true, // true for Germany
-};
-
-export const pcpConfigMexico = {
-	...pcpConfigDefault,
-	merchant: merchants.mexico,
-};
+import { merchants } from './pcp-merchants';
 
 export const pcpConfigUsa = {
-	...pcpConfigDefault,
 	merchant: merchants.usa,
 };
