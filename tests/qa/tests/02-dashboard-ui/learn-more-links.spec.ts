1e4550a033970260323f72da0e422bd10b67fd6f
Sedat <s.beser@inpsyde.com>
2025-02-28 12:51:52 +0300

    fix: .only removed lint

--- tests/qa/tests/02-dashboard-ui/learn-more-links.spec.ts
+++ tests/qa/tests/02-dashboard-ui/learn-more-links.spec.ts
@@ -11,35 +11,40 @@ test.describe( () => {
 	const countries = Object.keys( learnMoreLinksByCountry );
 
 	for ( const country of countries ) {
-		test.only( learnMoreLinksByCountry[country].testTitle, async ( {
-			pcpOnboarding,
-			wooCommerceApi,
-		} ) => {
-			const expectedLinks = learnMoreLinksByCountry[ country ].links;
+		test(
+			learnMoreLinksByCountry[ country ].testTitle,
+			async ( { pcpOnboarding, wooCommerceApi } ) => {
+				const expectedLinks = learnMoreLinksByCountry[ country ].links;
 
-			await wooCommerceApi.updateGeneralSettings( {
-				woocommerce_default_country: country,
-				woocommerce_currency: 'USD'
-			} );
+				await wooCommerceApi.updateGeneralSettings( {
+					woocommerce_default_country: country,
+					woocommerce_currency: 'USD',
+				} );
 
-			await pcpOnboarding.visit();
-			await pcpOnboarding.page.waitForLoadState( 'networkidle' );
-			for ( const { url, title } of expectedLinks ) {
-				const link = await pcpOnboarding.page.locator( `a[href="${ url }"]` );
-				await expect.soft( link ).toBeVisible();
-				
-				if (await link.isVisible()) {
-				const newContext = await pcpOnboarding.page.context().browser()?.newContext();
-					const newPage = await newContext?.newPage();
-					await newPage?.goto( url );
+				await pcpOnboarding.visit();
+				await pcpOnboarding.page.waitForLoadState( 'networkidle' );
+				for ( const { url, title } of expectedLinks ) {
+					const link = await pcpOnboarding.page.locator(
+						`a[href="${ url }"]`
+					);
+					await expect.soft( link ).toBeVisible();
 
-				expect.soft( newPage.url() ).toBe( url );
-				expect.soft( await newPage.title() ).toContain( title );
-				
-				await newPage.close();
-				await newContext.close();
+					if ( await link.isVisible() ) {
+						const newContext = await pcpOnboarding.page
+							.context()
+							.browser()
+							?.newContext();
+						const newPage = await newContext?.newPage();
+						await newPage?.goto( url );
+
+						expect.soft( newPage.url() ).toBe( url );
+						expect.soft( await newPage.title() ).toContain( title );
+
+						await newPage.close();
+						await newContext.close();
+					}
 				}
 			}
-		} );
+		);
 	}
 } );
