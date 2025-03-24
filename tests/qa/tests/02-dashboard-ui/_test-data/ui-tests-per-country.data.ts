38898cfaa67f86f545b42448cc0d5d85d5449825
Sedat <s.beser@inpsyde.com>
2025-03-24 16:07:15 +0300

    Test keys updated
    Test keys for badge values test cases updated

--- tests/qa/tests/02-dashboard-ui/_test-data/ui-tests-per-country.data.ts
+++ tests/qa/tests/02-dashboard-ui/_test-data/ui-tests-per-country.data.ts
@@ -5,42 +5,42 @@ import { shopSettings } from '@inpsyde/playwright-utils/build';
 
 export const badgeTestsData = [
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4319',
 		wooCommerceCountryCode: 'US:SC',
 		countryCode: 'US',
 	},
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4320',
 		wooCommerceCountryCode: 'GB',
 		countryCode: 'UK',
 	},
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4321',
 		wooCommerceCountryCode: 'CA:AB',
 		countryCode: 'CA',
 	},
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4322',
 		wooCommerceCountryCode: 'AU:NSW',
 		countryCode: 'AU',
 	},
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4323',
 		wooCommerceCountryCode: 'FR',
 		countryCode: 'FR',
 	},
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4324',
 		wooCommerceCountryCode: 'IT:GE',
 		countryCode: 'IT',
 	},
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4326',
 		wooCommerceCountryCode: 'ES:GR',
 		countryCode: 'ES',
 	},
 	{
-		testKey: 'PCP-0000',
+		testKey: 'PCP-4325',
 		wooCommerceCountryCode: 'DE:DE-BE',
 		countryCode: 'DE',
 	},
