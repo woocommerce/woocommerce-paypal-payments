cff9f3b1a107c47f7dfa5d9a8da79d1f7875b1b6
Misha Utkin <m.utkin@inpsyde.com>
2025-04-18 13:57:04 +0200

    Add transaction spect for block and classic pages

---
+++ tests/qa/tests/05-transactions/transaction-usa.spec.ts
@@ -0,0 +1,55 @@
+/**
+ * Internal dependencies
+ */
+import { test } from '../../utils';
+import {
+	merchants,
+	storeConfigUsa,
+	gateways,
+	taxSettings
+} from '../../resources';
+import { transactionsOnCheckout } from './_test-scenarios';
+import { payPalCheckout, payPalCheckoutExcludingTax, payPalCheckoutIntentAuthorized } from './_test-data/paypal';
+
+const { payPal, venmo, fastlane } = gateways;
+
+test.beforeAll( async ( { utils, pcpApi } ) => {
+	await utils.configureStore( storeConfigUsa );
+	await utils.installAndActivatePcp();
+	await pcpApi.resetDb();
+	await pcpApi.connectMerchant(
+		merchants.usa.client_id,
+		merchants.usa.client_secret
+	);
+	await pcpApi.updatePcpPaymentMethods( {
+		[ payPal.id ]: { id: payPal.id, enabled: true },
+		[ venmo.id ]: { id: venmo.id, enabled: true },
+		[ fastlane.id ]: { id: fastlane.id, enabled: false },
+	} );
+} );
+
+transactionsOnCheckout( payPalCheckout );
+
+test.describe( 'Excluding Tax', () => {
+	test.beforeAll( async ( { wooCommerceUtils } ) => {
+		await wooCommerceUtils.setTaxes( taxSettings.excluding );
+	} );
+
+	transactionsOnCheckout( payPalCheckoutExcludingTax );
+
+	test.afterAll( async ( { wooCommerceUtils } ) => {
+		await wooCommerceUtils.setTaxes( taxSettings.including );
+	} );
+} );
+
+test.describe( 'Intent Authorized', () => {
+	test.beforeAll( async ( { pcpApi } ) => {
+		await pcpApi.updatePcpSettings( { authorizeOnly: true } );
+	} );
+
+	transactionsOnCheckout( payPalCheckoutIntentAuthorized );
+
+	test.afterAll( async ( { pcpApi } ) => {
+		await pcpApi.updatePcpSettings( { authorizeOnly: false } );
+	} );
+} );
