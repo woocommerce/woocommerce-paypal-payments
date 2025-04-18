2876fd83f45f59a0c255aa64baa3ff555cb9fa52
Misha Utkin <m.utkin@inpsyde.com>
2025-04-18 13:55:52 +0200

    Add transaction tests scenarios for all pages

---
+++ tests/qa/tests/05-transactions/_test-scenarios/classic-cart.ts
@@ -0,0 +1,53 @@
+/**
+ * Internal dependencies
+ */
+import { annotateVisitor, test } from '../../../utils';
+
+export const transactionsOnClassicCart = ( testsData ) => {
+	for ( const testData of testsData ) {
+		test(
+			testData.title,
+			annotateVisitor( testData.customer ),
+			async ( {
+				classicCart,
+				classicCheckout,
+				wooCommerceApi,
+				orderReceived,
+				payPalApi,
+				wooCommerceOrderEdit,
+				utils,
+			} ) => {
+				await utils.fillVisitorsCart( testData.products );
+
+				await classicCart.makeOrder( testData );
+				await classicCheckout.fillCheckoutForm( testData.customer );
+				await classicCheckout.placeOrder();
+				// Expect Order Received page to be loaded
+				await orderReceived.assertOrderDetails( testData );
+
+				const orderId = await orderReceived.getOrderNumber();
+				const orderJson = await wooCommerceApi.getOrder( orderId );
+
+				const pcpData = {
+					transactionId: orderJson.transaction_id,
+					payPalFee: await payPalApi.getFee(
+						orderJson.transaction_id,
+						testData
+					),
+					payPalPayout: await payPalApi.getPayout(
+						orderJson.transaction_id,
+						testData
+					),
+				};
+
+				await payPalApi.assertOrder( orderJson, testData );
+				await payPalApi.assertPayment( orderJson.transaction_id, testData );
+				await wooCommerceOrderEdit.assertOrderDetails(
+					orderId,
+					testData,
+					pcpData
+				);
+			}
+		);
+	}
+};
