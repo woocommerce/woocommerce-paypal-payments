8936438bd321bfd1ddb3114b1ccda623c61e57e7
Misha Utkin <m.utkin@inpsyde.com>
2025-05-06 12:10:07 +0200

    Add separate spec with badges tests

--- tests/qa/tests/02-dashboard-ui/_test-data/index.ts
+++ tests/qa/tests/02-dashboard-ui/_test-data/index.ts
@@ -2,3 +2,4 @@ export * from './learn-more-links.data';
 export * from './pay-later-messaging.data';
 export * from './payment-methods.data';
 export * from './ui-tests-per-country.data';
+export * from './badges.data';
