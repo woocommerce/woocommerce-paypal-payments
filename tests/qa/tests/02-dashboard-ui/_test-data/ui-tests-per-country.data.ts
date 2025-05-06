af4d8f4ab3a88c9f78a1d83d6b63fd92268e43ea
Misha Utkin <m.utkin@inpsyde.com>
2025-05-06 12:09:35 +0200

    Update `visual-onboarding-ui.spec`
    - Replace percy
    - Move out badges tests

--- tests/qa/tests/02-dashboard-ui/_test-data/ui-tests-per-country.data.ts
+++ tests/qa/tests/02-dashboard-ui/_test-data/ui-tests-per-country.data.ts
@@ -3,63 +3,25 @@
  */
 import { shopSettings } from '@inpsyde/playwright-utils/build';
 
-export const badgeTestsData = [
-	{
-		testKey: 'PCP-4319',
-		wooCommerceCountryCode: 'US:SC',
-		country: 'United States',
-	},
-	{
-		testKey: 'PCP-4320',
-		wooCommerceCountryCode: 'UK',
-		country: 'United Kingdom',
-	},
-	{
-		testKey: 'PCP-4321',
-		wooCommerceCountryCode: 'CA:AB',
-		country: 'Canada',
-	},
-	{
-		testKey: 'PCP-4322',
-		wooCommerceCountryCode: 'AU:NSW',
-		country: 'Australia',
-	},
-	{
-		testKey: 'PCP-4323',
-		wooCommerceCountryCode: 'FR',
-		country: 'France',
-	},
-	{
-		testKey: 'PCP-4324',
-		wooCommerceCountryCode: 'IT:GE',
-		country: 'Italy',
-	},
-	{
-		testKey: 'PCP-4326',
-		wooCommerceCountryCode: 'ES:GR',
-		country: 'Spain',
-	},
-];
-
 export const defaultUiTestData = [
 	{
 		testSummary:
-			'PCP-4311 | Settings - Croatia - Onboarding initial page - Default UI @percy',
+			'PCP-4311 | Settings - Croatia - Onboarding initial page - Default UI',
 		wooCommerceGeneralSettings: shopSettings.croatia.general,
 	},
 	{
 		testSummary:
-			'PCP-4309 | Settings - Germany - Onboarding initial page - Default UI @percy',
+			'PCP-4309 | Settings - Germany - Onboarding initial page - Default UI',
 		wooCommerceGeneralSettings: shopSettings.germany.general,
 	},
 	{
 		testSummary:
-			'PCP-4310 | Settings - UK - Onboarding initial page - Default UI @percy',
+			'PCP-4310 | Settings - UK - Onboarding initial page - Default UI',
 		wooCommerceGeneralSettings: shopSettings.uk.general,
 	},
 	{
 		testSummary:
-			'PCP-4308 | Settings - US - Onboarding initial page - Default UI @percy',
+			'PCP-4308 | Settings - US - Onboarding initial page - Default UI',
 		wooCommerceGeneralSettings: shopSettings.usa.general,
 	},
 ];
