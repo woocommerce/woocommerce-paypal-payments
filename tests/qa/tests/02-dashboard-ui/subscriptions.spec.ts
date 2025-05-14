23e5580a8f3328a7a6794022c9a436071ed02703
Sedat <s.beser@inpsyde.com>
2025-05-14 14:49:19 +0300

    fix: unused fixtures removed

--- tests/qa/tests/02-dashboard-ui/subscriptions.spec.ts
+++ tests/qa/tests/02-dashboard-ui/subscriptions.spec.ts
@@ -50,8 +50,6 @@ test.describe( () => {
 
 	test( 'PCP-4357 | Subscription - Settings - US - Onboarding - Connect with business account, all product types, card payment enabled', async ( {
 		pcpOnboarding,
-		percy,
-		utils,
 		pcpPaymentMethods,
 		pcpSettings,
 		pcpStyling,
