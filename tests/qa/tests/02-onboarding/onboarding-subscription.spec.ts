ece6f4105aaf3c6d03d37ba18e4be66560cc0966
Sedat <s.beser@inpsyde.com>
2025-05-20 12:02:04 +0300

    Add PCP-4360 and various updates

--- tests/qa/tests/02-onboarding/onboarding-subscription.spec.ts
+++ tests/qa/tests/02-onboarding/onboarding-subscription.spec.ts
@@ -8,6 +8,7 @@ import {
 	merchants,
 	Pcp,
 } from '../../resources';
+import { onboardingSubscriptionComparisonByOPM } from './_test-data';
 
 test.beforeAll( async ( { utils, pcpApi } ) => {
 	await utils.configureStore( storeConfigDefault );
@@ -34,8 +35,10 @@ test.describe( () => {
 			'* Business account is required for subscriptions.'
 		);
 		await pcpOnboarding.personalAccountRadio().click();
-		await pcpOnboarding.snapshotContent(
-			`${ testInfo.title } - Set up store type`
+		await pcpOnboarding.snapshotLocator(
+			pcpOnboarding.onboardingContentContainer(),
+			`${ testInfo.title } - Set up store type`,
+			{ timeout: 3000 }
 		);
 		await pcpOnboarding.continueButton().click();
 
@@ -43,78 +46,95 @@ test.describe( () => {
 		await expect( pcpOnboarding.subscriptionsCheckbox() ).toHaveAttribute(
 			'disabled'
 		);
-		await pcpOnboarding.snapshotContent(
-			`${ testInfo.title } - Select product types - Subscription type of product not allowed`
+		await pcpOnboarding.snapshotLocator(
+			pcpOnboarding.onboardingContentContainer(),
+			`${ testInfo.title } - Select product types - Subscription type of product not allowed`,
+			{ timeout: 3000 }
 		);
 	} );
 
-	test.afterAll( async ( { requestUtils } ) => {
-		await requestUtils.deactivatePlugin( subscriptionsPlugin.slug );
-	} );
+	test.describe( () => {
+		test.beforeEach( async ( { utils, pcpApi } ) => {
+			await pcpApi.resetDb();
+		} );
 
-	test.only( 'PCP-4357 | Subscription - Settings - US - Onboarding - Connect with business account, all product types, card payment enabled', async ( {
-		pcpOnboarding,
-		pcpPaymentMethods,
-		pcpSettings,
-		pcpStyling,
-		pcpApi,
-	}, testInfo ) => {
-		await pcpOnboarding.visit();
+		for ( const testData of onboardingSubscriptionComparisonByOPM ) {
+			const { testSummary, optionalPaymentsEnabled } = testData;
+			test(
+				testSummary,
+				async (
+					{
+						pcpOnboarding,
+						pcpPaymentMethods,
+						pcpSettings,
+						pcpStyling,
+						pcpApi,
+					},
+					testInfo
+				) => {
+					await pcpOnboarding.visit();
+					await pcpOnboarding.activatePayPalPaymentsButton().click();
 
-		await pcpOnboarding.activatePayPalPaymentsButton().click();
+					await pcpOnboarding.businessRadio().click();
+					await pcpOnboarding.continueButton().click();
 
-		await pcpOnboarding.businessRadio().click();
-		await pcpOnboarding.continueButton().click();
+					await pcpOnboarding.physicalGoodsCheckbox().check();
+					await pcpOnboarding.subscriptionsCheckbox().check();
+					await pcpOnboarding.continueButton().click();
 
-		await pcpOnboarding.physicalGoodsCheckbox().check();
-		await pcpOnboarding.subscriptionsCheckbox().check();
-		await pcpOnboarding.continueButton().click();
+					await pcpOnboarding.chooseOptionalPaymentMethods(
+						optionalPaymentsEnabled
+					);
+					await pcpOnboarding.continueButton().click();
 
-		await pcpOnboarding.chooseOptionalPaymentMethods( true );
-		await pcpOnboarding.continueButton().click();
+					await pcpOnboarding.gotoInitialOnboardingPage();
 
-		await pcpOnboarding.gotoInitialOnboardingPage();
+					await pcpApi.connectMerchant(
+						merchants.usa.client_id,
+						merchants.usa.client_secret
+					);
 
-		await pcpApi.connectMerchant(
-			merchants.usa.client_id,
-			merchants.usa.client_secret
-		);
+					await pcpOnboarding.page.reload();
+					await pcpOnboarding.snapshotContent(
+						`${ testInfo.title } - Overview`,
+						3000
+					);
+					await pcpSettings.visit();
+					await pcpOnboarding.snapshotContent(
+						`${ testInfo.title } - Settings - Pay Now enabled by default`,
+						3000
+					);
 
-		await pcpOnboarding.page.reload();
-		await pcpOnboarding.snapshotContent(
-			`${ testInfo.title } - Overview`,
-			3000
-		);
+					await pcpPaymentMethods.visit();
+					await pcpOnboarding.snapshotContent(
+						`${ testInfo.title } - Payment methods - PayPal, Venmo enabled`,
+						3000
+					);
 
-		await pcpSettings.visit();
-		await pcpOnboarding.snapshotContent(
-			`${ testInfo.title } - Settings - Pay Now enabled by default`,
-			3000
-		);
+					const snapshotName = testInfo.title;
+					const locations: Pcp.Admin.Styling.Location[] = [
+						'Cart',
+						'Classic Checkout',
+						'Express Checkout',
+						'Mini Cart',
+						'Product Page',
+					];
 
-		await pcpPaymentMethods.visit();
-		await pcpOnboarding.snapshotContent(
-			`${ testInfo.title } - Payment methods - PayPal, Venmo enabled`,
-			3000
-		);
+					await pcpStyling.visit();
+					await expect( pcpStyling.configContainer() ).toBeVisible();
+					await expect(
+						pcpStyling.locationSelectbox()
+					).toBeVisible();
 
-		const snapshotName = testInfo.title;
-		const locations: Pcp.Admin.Styling.Location[] = [
-			'Cart',
-			'Classic Checkout',
-			'Express Checkout',
-			'Mini Cart',
-			'Product Page',
-		];
-
-		await pcpStyling.visit();
-		await expect( pcpStyling.configContainer() ).toBeVisible();
-		await expect( pcpStyling.locationSelectbox() ).toBeVisible();
-
-		for ( const location of locations ) {
-			await pcpStyling.locationSelectbox().selectOption( location );
-			await pcpStyling.snapshotStylingConfigurator(
-				`${ snapshotName } - ${ location }`
+					for ( const location of locations ) {
+						await pcpStyling
+							.locationSelectbox()
+							.selectOption( location );
+						await pcpStyling.snapshotStylingConfigurator(
+							`${ snapshotName } - ${ location }`
+						);
+					}
+				}
 			);
 		}
 	} );
