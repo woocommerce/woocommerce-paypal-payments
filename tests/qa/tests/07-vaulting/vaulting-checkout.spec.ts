192a281e00582e6d5faabab6cb1dc09a5a0c264d
Misha Utkin <m.utkin@inpsyde.com>
2025-06-06 14:30:55 +0200

    Add spec files

---
+++ tests/qa/tests/07-vaulting/vaulting-checkout.spec.ts
@@ -0,0 +1,52 @@
+/**
+ * Internal dependencies
+ */
+import { test } from '../../utils';
+import { merchants, storeConfigUsa } from '../../resources';
+import { vaultingCheckout } from './_test-data';
+import { testVaultingCheckout } from './_test-scenarios';
+
+const {
+	savePaymentMethodData,
+	acdcAdditionalCardData,
+	vaultedPaymentMethodData,
+} = vaultingCheckout;
+
+const {
+	testSavePaymentMethod,
+	testAcdcAdditionalCard,
+	testVaultedPaymentMethod,
+} = testVaultingCheckout;
+
+test.beforeAll( async ( { utils, pcpApi } ) => {
+	await utils.configureStore( {
+		...storeConfigUsa,
+		classicPages: true,
+	} );
+	await utils.installAndActivatePcp();
+	await pcpApi.resetDb();
+	await pcpApi.connectMerchant(
+		merchants.usa.client_id,
+		merchants.usa.client_secret,
+		{
+			isCasualSeller: false,
+			areOptionalPaymentMethodsEnabled: true,
+		}
+	);
+	await pcpApi.updatePcpSettings( {
+		savePaypalAndVenmo: true,
+		saveCardDetails: true,
+	} );
+} );
+
+for ( const testData of savePaymentMethodData ) {
+	testSavePaymentMethod( testData );
+}
+
+for ( const testData of acdcAdditionalCardData ) {
+	testAcdcAdditionalCard( testData );
+}
+
+for ( const testData of vaultedPaymentMethodData ) {
+	testVaultedPaymentMethod( testData );
+}
