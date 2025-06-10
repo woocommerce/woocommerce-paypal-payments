d8f3cd879c4eb06cd82d65fe9e45d91a33a40102
carmenmaymo <mayen22@gmail.com>
2025-06-10 09:53:33 +0200

    remove utils/data.js and refactor

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/PaymentMethodIcon.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/PaymentMethodIcon.js
@@ -1,11 +1,17 @@
 import { Icon } from '@wordpress/components';
-import data from '../../utils/data';
+
+const imageUrl = ( name ) => {
+	const filename = `icon-button-${ name }.svg`;
+	const pathToImages = global.ppcpSettings.assets.imagesUrl;
+	const className = '';
+
+	return (
+		<img className={ className } alt="" src={ pathToImages + filename } />
+	);
+};
 
 const PaymentMethodIcon = ( { type } ) => (
-	<Icon
-		icon={ data().getImage( `icon-button-${ type }.svg` ) }
-		className="ppcp--method-icon"
-	/>
+	<Icon icon={ imageUrl( type ) } className="ppcp--method-icon" />
 );
 
 export default PaymentMethodIcon;
