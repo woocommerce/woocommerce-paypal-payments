11ef14b13ce0714e4aac1c9c08c88cf48f051328
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2025-08-22 17:37:38 +0200

    Fix psalm false positive

--- modules/ppcp-api-client/src/Factory/PurchaseUnitFactory.php
+++ modules/ppcp-api-client/src/Factory/PurchaseUnitFactory.php
@@ -175,6 +175,7 @@ class PurchaseUnitFactory {
 
 		$shipping = null;
 		$customer = \WC()->customer;
+		/** @psalm-suppress RedundantConditionGivenDocblockType False positive. Ignored because $customer can be null as well. */
 		if ( $this->shipping_needed( ...array_values( $items ) ) && is_a( $customer, \WC_Customer::class ) ) {
 			$shipping         = $this->shipping_factory->from_wc_customer( \WC()->customer, $with_shipping_options );
 			$shipping_address = $shipping->address();
