0789114ad6a764945618dcc8884af72406fd4bb2
Narek Zakarian <narekzakarian@gmail.com>
2025-09-10 12:04:34 +0400

    refactor: The `fastlane_cardholder_name` to just `cardholder_name`

--- modules/ppcp-settings/resources/js/data/payment/hooks.js
+++ modules/ppcp-settings/resources/js/data/payment/hooks.js
@@ -129,16 +129,14 @@ export const usePaymentMethodsModal = () => {
 	const { usePersistent } = useStoreData();
 
 	const [ paypalShowLogo ] = usePersistent( 'paypalShowLogo' );
-	const [ fastlaneCardholderName ] = usePersistent(
-		'fastlaneCardholderName'
-	);
+	const [ cardholderName ] = usePersistent( 'cardholderName' );
 	const [ fastlaneDisplayWatermark ] = usePersistent(
 		'fastlaneDisplayWatermark'
 	);
 
 	return {
 		paypalShowLogo,
-		fastlaneCardholderName,
+		cardholderName,
 		fastlaneDisplayWatermark,
 	};
 };
