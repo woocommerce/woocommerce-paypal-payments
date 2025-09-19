29315ed7c9f1a52fb0f6e70e9214d620a3cc5107
Philipp Stracker <p.stracker@syde.com>
2025-09-19 18:21:30 +0200

    🎉 Add empty “Agentic Commerce” module

---
+++ modules/ppcp-agentic-commerce/src/AgenticCommerceModule.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The agentic commerce module.
+ *
+ * @package WooCommerce\PayPalCommerce\AgenticCommerce
+ */
+
+declare( strict_types = 1 );
+
+namespace WooCommerce\PayPalCommerce\AgenticCommerce;
+
+use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ExecutableModule;
+use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ServiceModule;
+use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ModuleClassNameIdTrait;
+use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
+
+/**
+ * Class AgenticCommerceModule
+ */
+class AgenticCommerceModule implements ServiceModule, ExecutableModule {
+	use ModuleClassNameIdTrait;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function services(): array {
+		return require __DIR__ . '/../services.php';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function run( ContainerInterface $c ): bool {
+		// Add hooks.
+
+		return true;
+	}
+}
