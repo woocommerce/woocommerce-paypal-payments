fdf0f7d373176a094f0f2e6f6c16a616db0edfa7
Philipp Stracker <p.stracker@syde.com>
2025-09-19 18:41:35 +0200

    ✨ New feature flag for “Agentic Commerce” module

--- modules.php
+++ modules.php
@@ -11,7 +11,7 @@ use WooCommerce\PayPalCommerce\PayLaterBlock\PayLaterBlockModule;
 use WooCommerce\PayPalCommerce\PayLaterWCBlocks\PayLaterWCBlocksModule;
 use WooCommerce\PayPalCommerce\PayLaterConfigurator\PayLaterConfiguratorModule;
 
-return function ( string $root_dir ): iterable {
+return static function ( string $root_dir ): iterable {
 	$modules_dir = "$root_dir/modules";
 
 	$modules = array(
@@ -90,6 +90,13 @@ return function ( string $root_dir ): iterable {
 	) ) {
 		$modules[] = ( require "$modules_dir/ppcp-axo/module.php" )();
 		$modules[] = ( require "$modules_dir/ppcp-axo-block/module.php" )();
+	}
+
+	if ( apply_filters(
+		'woocommerce.feature-flags.woocommerce_paypal_payments.agentic_commerce_enabled',
+		getenv( 'PCP_AGENTIC_ENABLED' ) === '1'
+	) ) {
+		$modules[] = ( require "$modules_dir/ppcp-agentic-commerce/module.php" )();
 	}
 
 	return $modules;
