cfc05dfcaf7e00f43cf4cf3a560d16bb7769b377
Philipp Stracker <p.stracker@syde.com>
2025-09-24 11:58:14 +0200

    ♻️ Fully refactor the schema base class

--- modules/ppcp-agentic-commerce/src/Schema/AgenticSchema.php
+++ modules/ppcp-agentic-commerce/src/Schema/AgenticSchema.php
@@ -9,38 +9,76 @@ declare( strict_types = 1 );
 
 namespace WooCommerce\PayPalCommerce\AgenticCommerce\Schema;
 
-use WooCommerce\PayPalCommerce\AgenticCommerce\Errors\AgenticError;
+use WooCommerce\PayPalCommerce\AgenticCommerce\Validation\ValidationIssue;
 
 /**
  * Agentic schema classes must enforce immutability - only constructor can set properties!
  */
 abstract class AgenticSchema {
+	/**
+	 * Holds toe raw input data passed to the `from_array` factory method.
+	 */
+	private array $raw_data;
+
+	/**
+	 * Collection of all validation issues, populated by the parse_fields method.
+	 */
+	private array $validation_issues = array();
+
+	/**
+	 * Private constructor to enforce use of `from_array` factory method.
+	 *
+	 * @param array $raw_data The raw input data.
+	 */
+	final private function __construct( array $raw_data ) {
+		$this->raw_data = $raw_data;
+	}
+
+	/**
+	 * Performs the data validation during the object construction.
+	 *
+	 * @param array    $input     The raw input data.
+	 * @param callable $add_issue The callback to add a new ValidationIssue.
+	 */
+	abstract protected function parse_fields( array $input, callable $add_issue ): void;
 
 	/**
 	 * Returns a list of validation errors or an empty array when the object is valid.
 	 */
-	abstract public function validate(): array;
+	final public function validate(): array {
+		return $this->validation_issues;
+	}
 
 	/**
 	 * Returns a key-value array that represents the object's internal state.
 	 */
-	abstract public function to_array(): array;
+	final public function to_array(): array {
+		return $this->raw_data;
+	}
 
 	/**
 	 * Factory method to create a new object from the key-value array.
 	 *
 	 * @param array $data Key-value array.
-	 * @return static|AgenticError New instance, or error details.
+	 * @return static New instance, or error details.
 	 */
-	abstract public static function from_array( array $data );
+	final public static function from_array( array $data ): self {
+		$instance  = new static( $data );
+		$add_issue = static function ( ValidationIssue $issue ) use ( $instance ): void {
+			$instance->validation_issues[] = $issue;
+		};
+		$instance->parse_fields( $data, $add_issue );
+
+		return $instance;
+	}
 
 	/**
 	 * Helper that creates a new instance with a specific change based on the current object.
 	 *
 	 * @param array $changes Key-value set of changes to apply.
-	 * @return static|AgenticError New instance, or error details.
+	 * @return static New instance, or error details.
 	 */
-	public function with( array $changes ) {
+	final public function with( array $changes ): self {
 		$current = $this->to_array();
 		$merged  = array_merge( $current, $changes );
 
