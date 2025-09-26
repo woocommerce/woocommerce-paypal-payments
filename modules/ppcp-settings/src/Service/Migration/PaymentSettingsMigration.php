5fe76d02892065160f6249751981643ba8b40730
Narek Zakarian <narekzakarian@gmail.com>
2025-09-26 12:31:17 +0400

    feat: add BCDC migration support for ACDC merchants
    - Add `is_bcdc_enabled_for_acdc_merchant()` method to check if BCDC is enabled for ACDC merchants
    - Automatically enable CreditCardGateway when BCDC is switched on for ACDC merchants during settings migration

--- modules/ppcp-settings/src/Service/Migration/PaymentSettingsMigration.php
+++ modules/ppcp-settings/src/Service/Migration/PaymentSettingsMigration.php
@@ -9,11 +9,14 @@ declare( strict_types = 1 );
 
 namespace WooCommerce\PayPalCommerce\Settings\Service\Migration;
 
+use WooCommerce\PayPalCommerce\ApiClient\Helper\DccApplies;
 use WooCommerce\PayPalCommerce\Applepay\ApplePayGateway;
 use WooCommerce\PayPalCommerce\Axo\Gateway\AxoGateway;
 use WooCommerce\PayPalCommerce\Googlepay\GooglePayGateway;
 use WooCommerce\PayPalCommerce\Settings\Data\PaymentSettings;
+use WooCommerce\PayPalCommerce\WcGateway\Gateway\CardButtonGateway;
 use WooCommerce\PayPalCommerce\WcGateway\Gateway\CreditCardGateway;
+use WooCommerce\PayPalCommerce\WcGateway\Helper\DCCProductStatus;
 use WooCommerce\PayPalCommerce\WcGateway\Settings\Settings;
 
 /**
@@ -25,6 +28,8 @@ class PaymentSettingsMigration implements SettingsMigrationInterface {
 
 	protected Settings $settings;
 	protected PaymentSettings $payment_settings;
+	protected DccApplies $dcc_applies;
+	protected DCCProductStatus $dcc_status;
 
 	/**
 	 * The list of local apm methods.
@@ -36,10 +41,14 @@ class PaymentSettingsMigration implements SettingsMigrationInterface {
 	public function __construct(
 		Settings $settings,
 		PaymentSettings $payment_settings,
+		DccApplies $dcc_applies,
+		DCCProductStatus $dcc_status,
 		array $local_apms
 	) {
 		$this->settings         = $settings;
 		$this->payment_settings = $payment_settings;
+		$this->dcc_applies      = $dcc_applies;
+		$this->dcc_status       = $dcc_status;
 		$this->local_apms       = $local_apms;
 	}
 
@@ -59,6 +68,10 @@ class PaymentSettingsMigration implements SettingsMigrationInterface {
 					}
 				}
 			}
+		}
+
+		if ( $this->is_bcdc_enabled_for_acdc_merchant() ) {
+			$this->payment_settings->toggle_method_state( CreditCardGateway::ID, true );
 		}
 
 		foreach ( $this->map() as $old_key => $method_name ) {
@@ -83,5 +96,28 @@ class PaymentSettingsMigration implements SettingsMigrationInterface {
 			'googlepay_button_enabled' => GooglePayGateway::ID,
 			'pay_later_button_enabled' => 'pay-later',
 		);
+	}
+
+	/**
+	 * Checks if BCDC is enabled for ACDC merchant.
+	 *
+	 * This method verifies two conditions:
+	 * 1. The merchant is an ACDC merchant - determined by
+	 *    checking if DCC applies for the current country/currency and DCC status is active
+	 * 2. The Card Button Gateway is enabled in WooCommerce settings
+	 *
+	 * @return bool True if BCDC is enabled for ACDC merchant, false otherwise.
+	 */
+	protected function is_bcdc_enabled_for_acdc_merchant(): bool {
+		$is_acdc_merchant = $this->dcc_applies->for_country_currency() && $this->dcc_status->is_active();
+		if ( ! $is_acdc_merchant ) {
+			return false;
+		}
+
+		$card_button_gateway_id = CardButtonGateway::ID;
+		$card_button_gateway_settings = get_option( "woocommerce_{$card_button_gateway_id}_settings", array() );
+		$card_button_gateway_enabled  = $card_button_gateway_settings['enabled'] ?? false;
+
+		return $card_button_gateway_enabled === 'yes';
 	}
 }
