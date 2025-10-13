8a82f170b66b59fd0e67a0578c9d1898e8298430
Philipp Stracker <p.stracker@syde.com>
2025-10-13 16:41:43 +0200

    ✨ Add empty GiftOptions schema class

---
+++ modules/ppcp-agentic-commerce/src/Schema/GiftOptions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Defines the gift option schema.
+ *
+ * @package WooCommerce\PayPalCommerce\AgenticCommerce\Schema
+ */
+
+declare( strict_types = 1 );
+
+namespace WooCommerce\PayPalCommerce\AgenticCommerce\Schema;
+
+/**
+ * @see GiftOptionsTest - Unit tests for this class.
+ */
+class GiftOptions extends AgenticSchema {
+	protected function parse_fields( array $input, callable $add_issue ): void {
+		// TODO: Implement parse_fields() method.
+	}
+
+}
