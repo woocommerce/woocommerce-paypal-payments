12458f98acb24d4806b554cb18ea87d143b59282
Philipp Stracker <p.stracker@syde.com>
2025-10-13 16:41:47 +0200

    🧪 Expect is_gift storage

--- tests/PHPUnit/AgenticCommerce/Schema/GiftOptionsTest.php
+++ tests/PHPUnit/AgenticCommerce/Schema/GiftOptionsTest.php
@@ -15,4 +15,14 @@ class GiftOptionsTest extends SchemaTestCase {
 	protected function get_valid_data(): array {
 		return array();
 	}
+
+	/**
+	 * Tests that GiftOptions stores and returns the is_gift flag.
+	 */
+	public function test_is_gift_accessor(): void {
+		$data    = array( 'is_gift' => true );
+		$options = GiftOptions::from_array( $data );
+
+		$this->assertTrue( $options->is_gift() );
+	}
 }
