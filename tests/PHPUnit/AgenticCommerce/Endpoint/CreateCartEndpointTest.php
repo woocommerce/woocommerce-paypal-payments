1cf2a16d03964166c849a66b51210c50fd51d004
Philipp Stracker <p.stracker@syde.com>
2025-10-16 16:32:23 +0200

    🧪 First test for CreateCartEndpoint

--- tests/PHPUnit/AgenticCommerce/Endpoint/CreateCartEndpointTest.php
+++ tests/PHPUnit/AgenticCommerce/Endpoint/CreateCartEndpointTest.php
@@ -12,10 +12,11 @@ use function Brain\Monkey\Functions\when;
  */
 class CreateCartEndpointTest extends TestCase {
 
-	public function test_create_cart_returns_valid_response(): void {
+	public function test_create_cart_returns_201_created(): void {
 		$endpoint = new CreateCartEndpoint();
 
 		when( 'wp_generate_password' )->justReturn( 'random-string' );
+
 		$request = new WP_REST_Request( 'POST', '/wp-json/paypal/v1/merchant-cart' );
 		$request->set_body( json_encode( array(
 			'items'          => array(
@@ -40,5 +41,7 @@ class CreateCartEndpointTest extends TestCase {
 		$this->assertEmpty( $data['validation_issues'] );
 		$this->assertSame( 'CREATED', $data['status'] );
 		$this->assertSame( 'VALID', $data['validation_status'] );
+
+		$this->assertSame( 201, $response->get_status() );
 	}
 }
