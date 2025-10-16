7a55ecc75b90fa21f14ee3b0e7be7f9cec1b478d
Philipp Stracker <p.stracker@syde.com>
2025-10-16 15:24:57 +0200

    🚧 The NewCartResponse generates a random ID

--- modules/ppcp-agentic-commerce/src/Response/NewCartResponse.php
+++ modules/ppcp-agentic-commerce/src/Response/NewCartResponse.php
@@ -10,21 +10,28 @@ declare( strict_types = 1 );
 namespace WooCommerce\PayPalCommerce\AgenticCommerce\Response;
 
 use WooCommerce\PayPalCommerce\AgenticCommerce\Schema\PayPalCart;
+use WooCommerce\PayPalCommerce\AgenticCommerce\Schema\PaymentMethod;
 
 class NewCartResponse extends CartResponse {
 	public function __construct( PayPalCart $cart, string $token ) {
 		parent::__construct( $cart );
 		$this->token = $token;
+
+		// todo - the cart_id is generated when we persist the new cart. Fix this after cart persistence is done.
+		$this->cart_id = wp_generate_password( 12, false );
 	}
 
 	public function to_array(): array {
 		$data = parent::to_array();
 
-		$data['payment_method'] = array(
-			'type'         => 'paypal',
-			'token'        => $this->token,
-			'approval_url' => 'not-implemented',
+		$method = PaymentMethod::from_array(
+			array(
+				'type'  => 'paypal', // hard-coded.
+				'token' => $this->token,
+			)
 		);
+
+		$data['payment_method'] = $method->to_array();
 
 		return $data;
 	}
