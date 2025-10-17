dddfd006599b31c2149cf7abe59aa86be6e65d14
Philipp Stracker <p.stracker@syde.com>
2025-10-17 17:19:00 +0200

    ♻️ Ensure tests do not access DB

--- tests/PHPUnit/Settings/Service/Migration/BcdcOverrideTest.php
+++ tests/PHPUnit/Settings/Service/Migration/BcdcOverrideTest.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 namespace WooCommerce\PayPalCommerce\Settings\Service\Migration;
 
 use WooCommerce\PayPalCommerce\TestCase;
+use function Brain\Monkey\Functions\when;
 
 /**
  * @covers BcdcOverride
@@ -11,12 +12,14 @@ use WooCommerce\PayPalCommerce\TestCase;
 class BcdcOverrideTest extends TestCase {
 
 	public function test_is_active_returns_false_by_default(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 
 		$this->assertFalse( $override->is_active() );
 	}
 
 	public function test_is_active_returns_true_after_activate(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 
 		$override->activate( 'plugin_update' );
@@ -25,6 +28,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_is_active_returns_false_after_deactivate(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 
@@ -34,6 +38,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_activate_with_empty_reason_does_not_change_state(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 
 		$override->activate( '' );
@@ -42,6 +47,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_deactivate_with_empty_reason_does_not_change_state(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 
@@ -51,6 +57,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_describe_returns_inactive_status_by_default(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 
 		$description = $override->describe();
@@ -61,6 +68,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_describe_includes_activation_reason_when_active(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 
 		$override->activate( 'plugin_update' );
@@ -74,6 +82,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_describe_includes_different_activation_reason(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 
 		$override->activate( 'ui_migration' );
@@ -87,6 +96,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_describe_includes_deactivation_reason(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 
@@ -103,6 +113,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_describe_includes_activation_timestamp(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 
 		$override->activate( 'plugin_update' );
@@ -114,6 +125,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_activate_when_already_active_does_not_change_state(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 		$first_description = $override->describe();
@@ -127,6 +139,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_deactivate_when_already_inactive_does_not_change_state(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 		$override->deactivate( 'migration_complete' );
@@ -141,6 +154,7 @@ class BcdcOverrideTest extends TestCase {
 	}
 
 	public function test_activate_clears_deactivate_fields(): void {
+		when( 'get_option' )->justReturn( false );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 		$override->deactivate( 'migration_complete' );
