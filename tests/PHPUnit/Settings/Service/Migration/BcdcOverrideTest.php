f605b709b8c2752e618f4705b08b9ad608c409c8
Philipp Stracker <p.stracker@syde.com>
2025-10-17 17:32:04 +0200

    ♻️ Allow tests to ignore DB update attempts

--- tests/PHPUnit/Settings/Service/Migration/BcdcOverrideTest.php
+++ tests/PHPUnit/Settings/Service/Migration/BcdcOverrideTest.php
@@ -16,6 +16,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_is_active_returns_false_by_default(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 
 		$this->assertFalse( $override->is_active() );
@@ -23,6 +24,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_is_active_returns_true_after_activate(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 
 		$override->activate( 'plugin_update' );
@@ -32,6 +34,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_is_active_returns_false_after_deactivate(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 
@@ -42,6 +45,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_activate_with_empty_reason_does_not_change_state(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 
 		$override->activate( '' );
@@ -51,6 +55,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_deactivate_with_empty_reason_does_not_change_state(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 
@@ -61,6 +66,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_describe_returns_inactive_status_by_default(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 
 		$description = $override->describe();
@@ -72,6 +78,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_describe_includes_activation_reason_when_active(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 
 		$override->activate( 'plugin_update' );
@@ -86,6 +93,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_describe_includes_different_activation_reason(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 
 		$override->activate( 'ui_migration' );
@@ -100,6 +108,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_describe_includes_deactivation_reason(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 
@@ -117,6 +126,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_describe_includes_activation_timestamp(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 
 		$override->activate( 'plugin_update' );
@@ -129,6 +139,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_activate_when_already_active_does_not_change_state(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 		$first_description = $override->describe();
@@ -143,6 +154,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_deactivate_when_already_inactive_does_not_change_state(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 		$override->deactivate( 'migration_complete' );
@@ -158,6 +170,7 @@ class BcdcOverrideTest extends TestCase {
 
 	public function test_activate_clears_deactivate_fields(): void {
 		when( 'get_option' )->justReturn( false );
+		when( 'update_option' )->justReturn( true );
 		$override = new BcdcOverride();
 		$override->activate( 'plugin_update' );
 		$override->deactivate( 'migration_complete' );
