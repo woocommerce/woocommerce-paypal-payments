3a36a9f039619410bd9e2a0ab889874c9ced8474
Philipp Stracker <p.stracker@syde.com>
2025-10-21 17:50:41 +0200

    ✨ Add the PayPalJwtProvider class

---
+++ modules/ppcp-agentic-commerce/src/Auth/PayPalJwkProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WooCommerce\PayPalCommerce\AgenticCommerce\Auth;
+
+class PayPalJwkProvider {
+
+	public function keys(): array {
+		return array();
+	}
+}
