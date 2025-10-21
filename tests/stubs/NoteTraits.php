528d009cf46775dc4d150e58f9601b432102e8bb
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2025-10-21 16:07:24 +0400

    Refactor NoteTrait stub

--- tests/stubs/NoteTraits.php
+++ tests/stubs/NoteTraits.php
@@ -1,28 +1,7 @@
 <?php
 
 namespace Automattic\WooCommerce\Admin\Notes;
-/**
- * Stub for WooCommerce Admin NoteTraits
- */
+
 trait NoteTraits {
-	/**
-	 * Get the note.
-	 */
-	public static function get_note() {
-		return null;
-	}
 
-	/**
-	 * Possibly add the note.
-	 */
-	public static function possibly_add_note() {
-		// Stub method
-	}
-
-	/**
-	 * Can the note be added?
-	 */
-	public static function can_be_added() {
-		return false;
-	}
 }
