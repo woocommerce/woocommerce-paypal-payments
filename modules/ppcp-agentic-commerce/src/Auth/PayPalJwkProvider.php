ee02150d20a041deac34576cd972b0defec7bb12
Philipp Stracker <p.stracker@syde.com>
2025-10-22 18:17:23 +0200

    ✅ Fix the JWK provider logic

--- modules/ppcp-agentic-commerce/src/Auth/PayPalJwkProvider.php
+++ modules/ppcp-agentic-commerce/src/Auth/PayPalJwkProvider.php
@@ -7,7 +7,7 @@ namespace WooCommerce\PayPalCommerce\AgenticCommerce\Auth;
 use Firebase\JWT\Key;
 
 class PayPalJwkProvider {
-	private const ALGORITHM = 'HS256';
+	private const ALGORITHM = 'RS256';
 
 	private ?Key $cache = null;
 
@@ -40,6 +40,7 @@ class PayPalJwkProvider {
 	protected function fetch_key_material(): string {
 		// todo - the string should be fetched from a .well-known URL.
 		return <<<'EOF'
+-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvv7Pi1nWWrJj4n5+6gX9
 B7BQpctaPEg9VdVK1kzc9xBNwZobeWEgEmiUGtkrn8S5R6Q4NmB4hnb8F5jeCX5O
 kyA49mgzw4wNXUPGTGMY5Eoxt9zu1Heaivkljh4+wN6d01oIFkHT6E7VjEJOG2RA
@@ -47,6 +48,7 @@ kyA49mgzw4wNXUPGTGMY5Eoxt9zu1Heaivkljh4+wN6d01oIFkHT6E7VjEJOG2RA
 1YBqFG+HlSG5hWu880IzGQE9Pds5qekIawJJyed08otq29hDHlFd28B0fFhdzcu8
 cN83NxddXBlh77b8+a7gaWC5/Iw45THRpIsiG41uX0r0INEDcnR3qCUkz6m9LOVW
 kQIDAQAB
+-----END PUBLIC KEY-----
 EOF;
 	}
 }
