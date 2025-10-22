30482d0cd2ac5852b39a87a43cbf688295742ce6
Philipp Stracker <p.stracker@syde.com>
2025-10-22 16:20:52 +0200

    ✅ Implement caching methods into JwkProvider

--- modules/ppcp-agentic-commerce/src/Auth/PayPalJwkProvider.php
+++ modules/ppcp-agentic-commerce/src/Auth/PayPalJwkProvider.php
@@ -7,6 +7,17 @@ namespace WooCommerce\PayPalCommerce\AgenticCommerce\Auth;
 class PayPalJwkProvider {
 
 	public function keys(): array {
+		return $this->get_cached() ?? $this->fetch_keys();
+	}
+
+	protected function get_cached(): ?array {
+		return array(
+			'key1' => 'value1',
+			'key2' => 'value2',
+		);
+	}
+
+	protected function fetch_keys(): array {
 		return array();
 	}
 }
