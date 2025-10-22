08398f881d873b326b10607e28c6110d6d374e08
Philipp Stracker <p.stracker@syde.com>
2025-10-22 16:19:47 +0200

    🧪 Expect that cached keys are returned

--- tests/PHPUnit/AgenticCommerce/Auth/PayPalJwkProviderTest.php
+++ tests/PHPUnit/AgenticCommerce/Auth/PayPalJwkProviderTest.php
@@ -9,18 +9,38 @@ namespace WooCommerce\PayPalCommerce\AgenticCommerce\Auth;
 
 use WooCommerce\PayPalCommerce\TestCase;
 
+/**
+ * @covers \WooCommerce\PayPalCommerce\AgenticCommerce\Auth\PayPalJwkProvider
+ */
 class PayPalJwkProviderTest extends TestCase {
 
 	/**
-	 * GIVEN PayPalJwkProvider instance
+	 * GIVEN cached keys are available
 	 * WHEN keys() is called
-	 * THEN should return an array
+	 * THEN should return the cached keys without fetching
 	 */
-	public function test_keys_returns_array(): void {
-		$provider = new PayPalJwkProvider();
+	public function test_returns_cached_keys_when_available(): void {
+		$cached_keys = array(
+			'key1' => 'value1',
+			'key2' => 'value2',
+		);
+
+		$provider = $this->getMockBuilder( PayPalJwkProvider::class )
+			->onlyMethods( array( 'get_cached', 'fetch_keys' ) )
+			->getMock();
+
+		$provider->method( 'get_cached' )
+			->willReturn( $cached_keys );
+
+		$provider->expects( $this->never() )
+			->method( 'fetch_keys' );
 
 		$result = $provider->keys();
 
-		$this->assertIsArray( $result );
+		$this->assertSame( $cached_keys, $result );
+
+		$result = $provider->keys();
+
+		$this->assertSame( $fresh_keys, $result );
 	}
 }
