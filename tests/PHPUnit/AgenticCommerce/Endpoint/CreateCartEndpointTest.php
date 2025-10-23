9e7efcccee08c51c5e55644e66c4cafb217d69f4
Philipp Stracker <p.stracker@syde.com>
2025-10-23 13:52:11 +0200

    🧪 Fix failing CreateCartEndpoint tests

--- tests/PHPUnit/AgenticCommerce/Endpoint/CreateCartEndpointTest.php
+++ tests/PHPUnit/AgenticCommerce/Endpoint/CreateCartEndpointTest.php
@@ -5,6 +5,9 @@ namespace WooCommerce\PayPalCommerce\AgenticCommerce\Endpoint;
 
 use WooCommerce\PayPalCommerce\TestCase;
 use WooCommerce\PayPalCommerce\AgenticCommerce\Auth\JwtAuthService;
+use WooCommerce\PayPalCommerce\AgenticCommerce\Response\ResponseFactory;
+use WooCommerce\PayPalCommerce\AgenticCommerce\Response\NewCartResponse;
+use WooCommerce\PayPalCommerce\AgenticCommerce\Schema\PayPalCart;
 use WP_REST_Request;
 use Mockery;
 use function Brain\Monkey\Functions\when;
@@ -15,14 +18,8 @@ use function Brain\Monkey\Functions\when;
 class CreateCartEndpointTest extends TestCase {
 
 	public function test_create_cart_returns_201_created(): void {
-		$auth = Mockery::mock( JwtAuthService::class );
-
-		$endpoint = new CreateCartEndpoint( $auth );
-
-		when( 'wp_generate_password' )->justReturn( 'random-string' );
-
-		$request = new WP_REST_Request( 'POST', '/wp-json/paypal/v1/merchant-cart' );
-		$request->set_body( json_encode( array(
+		$sample_token = 'random-string';
+		$cart_data    = array(
 			'items'          => array(
 				array(
 					'item_id'  => 'TEST-001',
@@ -36,7 +33,25 @@ class CreateCartEndpointTest extends TestCase {
 			'payment_method' => array(
 				'type' => 'paypal',
 			),
-		) ) );
+		);
+
+		when( 'wp_generate_password' )->justReturn( $sample_token );
+
+		$auth             = Mockery::mock( JwtAuthService::class );
+		$response_factory = Mockery::mock( ResponseFactory::class );
+
+		$response_factory->shouldReceive( 'new_cart' )
+			->once()
+			->withArgs( fn( $cart ) => $cart instanceof PayPalCart )
+			->andReturnUsing( fn( $cart ) => new NewCartResponse(
+				$cart,
+				$sample_token
+			) );
+
+		$endpoint = new CreateCartEndpoint( $auth, $response_factory );
+
+		$request = new WP_REST_Request( 'POST', '/wp-json/paypal/v1/merchant-cart' );
+		$request->set_body( json_encode( $cart_data ) );
 
 		$response = $endpoint->create_cart( $request );
 		$data     = $response->get_data();
