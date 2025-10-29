395490f391c20dacbb9966f0c1ec898baa47dcfa
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2025-10-29 17:20:45 +0400

    Remove current_date and use gmdate instead

--- modules/ppcp-api-client/services.php
+++ modules/ppcp-api-client/services.php
@@ -827,9 +827,9 @@ return array(
 	},
 	'api.paylater.is-canada-released'                => static function ( ContainerInterface $container ): bool {
 		// Check if current date is after November 12th, 2025 (Expected PayPal release date).
-		// @todo Remove this logic after the next release
+		// @todo Remove this logic after the next release.
 		$release_date = '2025-11-12';
-		$current_date = current_time( 'Y-m-d' );
+		$current_date = gmdate( 'Y-m-d' );
 
 		return $current_date >= $release_date;
 	},
