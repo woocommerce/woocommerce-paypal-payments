8f2cdb69c5033afff88aa82d1ad60617bd48cf48
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2025-10-29 17:14:46 +0400

    Remove unnecessary constructor args and refactor variable name and extra space

--- modules/ppcp-settings/src/Data/Definition/FeaturesDefinition.php
+++ modules/ppcp-settings/src/Data/Definition/FeaturesDefinition.php
@@ -62,8 +62,7 @@ class FeaturesDefinition {
 		FeaturesEligibilityService $eligibilities,
 		GeneralSettings $settings,
 		array $merchant_capabilities,
-		SettingsModel $plugin_settings,
-		array $paylater_countries = array()
+		SettingsModel $plugin_settings
 	) {
 		$this->eligibilities         = $eligibilities;
 		$this->settings              = $settings;
@@ -94,7 +93,7 @@ class FeaturesDefinition {
 	 * @return array[] The array of all available features.
 	 */
 	public function all_available_features(): array {
-		$paylater_documentation_countries = array(
+		$paylater_documentation_supported_countries = array(
 			'UK',
 			'ES',
 			'IT',
@@ -104,9 +103,9 @@ class FeaturesDefinition {
 			'AU',
 		);
 
-		$store_country         = $this->settings->get_woo_settings()['country'];
-		$country_location      = in_array( $store_country, $paylater_documentation_countries, true ) ? strtolower( $store_country ) : 'us';
-		$save_paypal_and_venmo = $this->plugin_settings->get_save_paypal_and_venmo();
+		$store_country                  = $this->settings->get_woo_settings()['country'];
+		$paylater_docs_country_location = in_array( $store_country, $paylater_documentation_supported_countries, true ) ? strtolower( $store_country ) : 'us';
+		$save_paypal_and_venmo          = $this->plugin_settings->get_save_paypal_and_venmo();
 
 		$feature_items = array(
 			'save_paypal_and_venmo'           => array(
@@ -314,7 +313,7 @@ class FeaturesDefinition {
 					array(
 						'type'  => 'tertiary',
 						'text'  => __( 'Learn more', 'woocommerce-paypal-payments' ),
-						'url'   => "https://www.paypal.com/$country_location/business/accept-payments/checkout/installments",
+						'url'   => "https://www.paypal.com/$paylater_docs_country_location/business/accept-payments/checkout/installments",
 						'class' => 'small-button',
 					),
 				),
@@ -368,8 +367,7 @@ class FeaturesDefinition {
 						'text'     => __( 'Sign up', 'woocommerce-paypal-payments' ),
 						'urls'     => array(
 							'sandbox' => 'https://www.sandbox.paypal.com/bizsignup/add-product?product=CRYPTO_PYMTS',
-							'live'    => 'https://www.paypal.com/bizsignup/add-product?product=CRYPTO_PYMTS
-',
+							'live'    => 'https://www.paypal.com/bizsignup/add-product?product=CRYPTO_PYMTS',
 						),
 						'showWhen' => 'disabled',
 						'class'    => 'small-button',
