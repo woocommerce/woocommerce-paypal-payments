37b50128673abf8505e5f92f2bae52111581e107
Philipp Stracker <p.stracker@syde.com>
2025-11-04 16:52:31 +0100

    🧪 Expect is_registered returns false

---
+++ tests/PHPUnit/AgenticCommerce/Registration/RegistrationServiceTest.php
@@ -0,0 +1,41 @@
+<?php
+declare( strict_types = 1 );
+
+namespace WooCommerce\PayPalCommerce\AgenticCommerce\Registration;
+
+use Mockery;
+use WooCommerce\PayPalCommerce\AgenticCommerce\Merchant\MerchantMetadata;
+use WooCommerce\PayPalCommerce\AgenticCommerce\Merchant\MerchantMetadataProvider;
+use WooCommerce\PayPalCommerce\TestCase;
+use WooCommerce\PayPalCommerce\WcGateway\Helper\ConnectionState;
+use WP_Error;
+use function Brain\Monkey\Functions\expect;
+use function Brain\Monkey\Functions\when;
+
+/**
+ * @covers RegistrationService
+ */
+class RegistrationServiceTest extends TestCase {
+
+	private ConnectionState $connection_state;
+	private MerchantMetadataProvider $metadata_provider;
+	private RegistrationService $testee;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->connection_state  = Mockery::mock( ConnectionState::class );
+		$this->metadata_provider = Mockery::mock( MerchantMetadataProvider::class );
+
+		$this->testee = new RegistrationService(
+			$this->connection_state,
+			$this->metadata_provider
+		);
+	}
+
+	public function test_is_registered_returns_false(): void {
+		$result = $this->testee->is_registered();
+
+		$this->assertFalse( $result );
+	}
+}
