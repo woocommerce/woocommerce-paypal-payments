05c8a406978dc96731facff25c00db5f67176244
Misha Utkin <m.utkin@inpsyde.com>
2025-11-19 08:55:27 +0100

    Reduce CI test retries to 1

--- tests/qa/playwright.config.ts
+++ tests/qa/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig< BaseExtend >( {
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !! process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 1 : 0,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : 1,
 	/* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot */
