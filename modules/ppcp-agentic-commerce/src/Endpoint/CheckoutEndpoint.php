e9fa102c800c7dec68fa32838a0d1c5f42c997e0
Philipp Stracker <p.stracker@syde.com>
2025-12-04 18:43:33 +0100

    🐛 Fix missing status-code in respose function

--- modules/ppcp-agentic-commerce/src/Endpoint/CheckoutEndpoint.php
+++ modules/ppcp-agentic-commerce/src/Endpoint/CheckoutEndpoint.php
@@ -122,7 +122,7 @@ class CheckoutEndpoint extends AgenticRestEndpoint {
 		if ( $cart->issues() ) {
 			$cart_response = $this->response_factory->from_cart( $cart );
 
-			return $this->cart_details( $cart_response );
+			return $this->cart_details( $cart_response, 200 );
 		}
 
 		$order = $this->create_wc_order( $cart, $payment_method, $session['ec_token'] );
