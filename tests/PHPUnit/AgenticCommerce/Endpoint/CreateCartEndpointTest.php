75a4cc15c366c1b36a682c45f2febf5512c34c0d
Philipp Stracker <p.stracker@syde.com>
2025-12-10 15:25:36 +0100

    ✅ Fix unit test

--- tests/PHPUnit/AgenticCommerce/Endpoint/CreateCartEndpointTest.php
+++ tests/PHPUnit/AgenticCommerce/Endpoint/CreateCartEndpointTest.php
@@ -47,6 +47,7 @@ class CreateCartEndpointTest extends AgenticEndpointTestCase {
 			->andReturnUsing( fn( $cart, $cart_id, $ec_token ) => new NewCartResponse(
 				$cart,
 				$cart_id,
+				null,
 				$ec_token
 			) );
 
