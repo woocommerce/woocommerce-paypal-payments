a039424e26db6535490576bef57f3f4fc1959261
Daniel Dudzic <d.dudzic@syde.com>
2025-12-16 02:25:37 +0100

    🐛 Restore conditional loading of agentic-commerce module

--- modules.php
+++ modules.php
@@ -87,7 +87,14 @@ return static function ( string $root_dir ): iterable {
 		$modules[] = ( require "$modules_dir/ppcp-axo-block/module.php" )();
 	}
 
+	if ( ! SettingsModule::should_use_the_old_ui()
+		&& apply_filters(
+			'woocommerce.feature-flags.woocommerce_paypal_payments.agentic_commerce_enabled',
+			getenv( 'PCP_AGENTIC_COMMERCE_ENABLED' ) === '1'
+		)
+	) {
 		$modules[] = ( require "$modules_dir/ppcp-agentic-commerce/module.php" )();
+	}
 
 	return $modules;
 };
