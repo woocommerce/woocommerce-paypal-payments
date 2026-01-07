7008413ff240144a2e7b4b5b1c26e0adefa8dde6
Philipp Stracker <p.stracker@syde.com>
2026-01-07 18:21:04 +0100

    ✅ Implement the ResultCache class

---
+++ modules/ppcp-api-client/src/Helper/ProductStatusResultCache.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Caches the API results for the ProductStatus class.
+ *
+ * @package WooCommerce\PayPalCommerce\ApiClient\Helper
+ */
+
+declare( strict_types = 1 );
+
+namespace WooCommerce\PayPalCommerce\ApiClient\Helper;
+
+class ProductStatusResultCache {
+}
