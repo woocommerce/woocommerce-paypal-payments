c547e3fb3d2b30afd1c0d5f6eb9b2bd880664d58
Philipp Stracker <p.stracker@syde.com>
2026-01-07 18:30:37 +0100

    🧪 Asssert that ::get() returns an empty string

--- tests/PHPUnit/ApiClient/Helper/ProductStatusResultCacheTest.php
+++ tests/PHPUnit/ApiClient/Helper/ProductStatusResultCacheTest.php
@@ -11,4 +11,13 @@ class ProductStatusResultCacheTest extends TestCase {
 		$testee = new ProductStatusResultCache();
 		$this->assertInstanceOf( ProductStatusResultCache::class, $testee );
 	}
+
+	public function test_get_returns_empty_string_for_non_existent_key(): void {
+		$testee = new ProductStatusResultCache();
+
+		$result = $testee->get( 'non_existent_key' );
+
+		$this->assertSame( '', $result );
+	}
+
 }
