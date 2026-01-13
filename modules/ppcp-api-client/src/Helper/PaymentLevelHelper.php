050796c818a9737e4e62c79792e92629a1551a8b
Narek Zakarian <narekzakarian@gmail.com>
2026-01-13 17:08:40 +0400

    test: fix tests

--- modules/ppcp-api-client/src/Helper/PaymentLevelHelper.php
+++ modules/ppcp-api-client/src/Helper/PaymentLevelHelper.php
@@ -381,7 +381,7 @@ class PaymentLevelHelper {
 				$item->discount(),
 				$item
 			);
-			if ( $discount instanceof Money ) {
+			if ( $discount ) {
 				$line_item['discount_amount'] = array(
 					'currency_code' => $discount->currency_code(),
 					'value'         => $discount->value_str(),
