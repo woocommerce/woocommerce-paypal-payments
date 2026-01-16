6883a8885e079d9c7ce04f9bd67edecd30e82fa2
Philipp Stracker <p.stracker@syde.com>
2026-01-16 17:21:29 +0100

    🎨 Reorder constructor arguments

--- modules/ppcp-settings/src/Data/OnboardingProfile.php
+++ modules/ppcp-settings/src/Data/OnboardingProfile.php
@@ -39,23 +39,23 @@ class OnboardingProfile extends AbstractDataModel {
 	/**
 	 * Constructor.
 	 *
+	 * @param callable $can_use_fastlane  Callable to check whether it can use Fastlane or not.
 	 * @param bool     $can_use_casual_selling Whether casual selling is enabled in the store's country.
 	 * @param bool     $can_use_vaulting       Whether vaulting is enabled in the store's country.
 	 * @param bool     $can_use_card_payments  Whether credit card payments are possible.
 	 * @param bool     $can_use_subscriptions  Whether WC Subscriptions plugin is active.
 	 * @param bool     $should_skip_payment_methods  Whether it should skip payment methods screen.
-	 * @param callable $can_use_fastlane  Callable to check whether it can use Fastlane or not.
 	 * @param bool     $can_use_pay_later  Whether it can use Pay Later or not.
 	 *
 	 * @throws RuntimeException If the OPTION_KEY is not defined in the child class.
 	 */
 	public function __construct(
+		callable $can_use_fastlane,
 		bool $can_use_casual_selling = false,
 		bool $can_use_vaulting = false,
 		bool $can_use_card_payments = false,
 		bool $can_use_subscriptions = false,
 		bool $should_skip_payment_methods = false,
-		callable $can_use_fastlane,
 		bool $can_use_pay_later = false
 	) {
 		parent::__construct();
