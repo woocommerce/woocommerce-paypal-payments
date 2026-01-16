368bd75b4b026c76ff67d3e8960cb9cb2af019d6
Narek Zakarian <narekzakarian@gmail.com>
2026-01-16 15:02:46 +0400

    refactor: remove SettingsMapHelper to unblock compat module migration
    Removes SettingsMapHelper from legacy Settings class, enabling removal
    of its service registration from compat module. This allows compat
    module to migrate to SettingsProvider without this dependency.

--- modules/ppcp-wc-gateway/services.php
+++ modules/ppcp-wc-gateway/services.php
@@ -330,8 +330,7 @@ return array(
 				$container->get( 'wcgateway.button.default-locations' ),
 				$container->get( 'wcgateway.settings.dcc-gateway-title.default' ),
 				$container->get( 'wcgateway.settings.pay-later.default-button-locations' ),
-				$container->get( 'wcgateway.settings.pay-later.default-messaging-locations' ),
-				$container->get( 'compat.settings.settings_map_helper' )
+				$container->get( 'wcgateway.settings.pay-later.default-messaging-locations' )
 			);
 		}
 	),
