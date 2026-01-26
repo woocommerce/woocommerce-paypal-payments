c9502e6379a7afea5fd8364d10305c6a81e7970b
Emili Castells Guasch <e.castells@inpsyde.com>
2026-01-26 17:48:35 +0100

    Set vaulted_paypal_email to empty string before returning early

--- modules/ppcp-wc-subscriptions/src/WcSubscriptionsModule.php
+++ modules/ppcp-wc-subscriptions/src/WcSubscriptionsModule.php
@@ -396,8 +396,10 @@ class WcSubscriptionsModule implements ServiceModule, ExtendingModule, Executabl
 		add_filter(
 			'woocommerce_paypal_payments_localized_script_data',
 			function ( array $localized_script_data ) use ( $c ) {
-				// Return early if save payment methods (Vault v3) is enabled.
+				// When Vault v3 is enabled, set empty vaulted_paypal_email to prevent
+				// undefined JS property from affecting button visibility logic.
 				if ( $c->has( 'save-payment-methods.eligible' ) && $c->get( 'save-payment-methods.eligible' ) ) {
+					$localized_script_data['vaulted_paypal_email'] = '';
 					return $localized_script_data;
 				}
 
