3eb980a4c04847bcf8c2ecdb60259ca78dd34817
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-01-26 08:59:18 +0000

    [BOT] Add build artifact from dev/qa/fix-for-playwright-utils-3.0.0 -> qa/fix-for-playwright-utils-3.0.0.

---
+++ assets/ppcp-button-js-button.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '83040d3ed6f613a5e7df');
+}
--- modules/ppcp-api-client/src/Entity/PhoneWithType.php
+++ modules/ppcp-api-client/src/Entity/PhoneWithType.php
@@ -1,79 +1,67 @@
 <?php
+
 /**
  * The PhoneWithType object
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Entity
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Entity;
 
 /**
  * Class PhoneWithType
  */
-class PhoneWithType {
-
-	const VALID_TYPES = array(
-		'FAX',
-		'HOME',
-		'MOBILE',
-		'OTHER',
-		'PAGER',
-	);
-
-	/**
-	 * The type.
-	 *
-	 * @var string
-	 */
-	private $type;
-
-	/**
-	 * The phone.
-	 *
-	 * @var Phone
-	 */
-	private $phone;
-
-	/**
-	 * PhoneWithType constructor.
-	 *
-	 * @param string $type The type.
-	 * @param Phone  $phone The phone.
-	 */
-	public function __construct( string $type, Phone $phone ) {
-		$this->type  = in_array( $type, self::VALID_TYPES, true ) ? $type : 'OTHER';
-		$this->phone = $phone;
-	}
-
-	/**
-	 * Returns the type.
-	 *
-	 * @return string
-	 */
-	public function type(): string {
-		return $this->type;
-	}
-
-	/**
-	 * Returns the phone.
-	 *
-	 * @return Phone
-	 */
-	public function phone(): Phone {
-		return $this->phone;
-	}
-
-	/**
-	 * Returns the object as array.
-	 *
-	 * @return array
-	 */
-	public function to_array(): array {
-		return array(
-			'phone_type'   => $this->type(),
-			'phone_number' => $this->phone()->to_array(),
-		);
-	}
+class PhoneWithType
+{
+    const VALID_TYPES = array('FAX', 'HOME', 'MOBILE', 'OTHER', 'PAGER');
+    /**
+     * The type.
+     *
+     * @var string
+     */
+    private $type;
+    /**
+     * The phone.
+     *
+     * @var Phone
+     */
+    private $phone;
+    /**
+     * PhoneWithType constructor.
+     *
+     * @param string $type The type.
+     * @param Phone  $phone The phone.
+     */
+    public function __construct(string $type, \WooCommerce\PayPalCommerce\ApiClient\Entity\Phone $phone)
+    {
+        $this->type = in_array($type, self::VALID_TYPES, \true) ? $type : 'OTHER';
+        $this->phone = $phone;
+    }
+    /**
+     * Returns the type.
+     *
+     * @return string
+     */
+    public function type(): string
+    {
+        return $this->type;
+    }
+    /**
+     * Returns the phone.
+     *
+     * @return Phone
+     */
+    public function phone(): \WooCommerce\PayPalCommerce\ApiClient\Entity\Phone
+    {
+        return $this->phone;
+    }
+    /**
+     * Returns the object as array.
+     *
+     * @return array
+     */
+    public function to_array(): array
+    {
+        return array('phone_type' => $this->type(), 'phone_number' => $this->phone()->to_array());
+    }
 }
