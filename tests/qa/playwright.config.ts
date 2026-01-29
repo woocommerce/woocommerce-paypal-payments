5f57c66b9826e9429d7ba2a304b04f58bb9aa7d5
Misha Utkin <m.utkin@inpsyde.com>
2026-01-29 13:29:17 +0100

    Update test.setTimeout format

--- tests/qa/playwright.config.ts
+++ tests/qa/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig< BaseExtend >( {
 	expect: {
 		timeout: 20 * 1000,
 	},
-	timeout: 2 * 60 * 1000,
+	timeout: 2 * 60_000,
 	/* Run tests in files in parallel */
 	fullyParallel: false,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -97,7 +97,7 @@ export default defineConfig< BaseExtend >( {
 			: {
 					mode: 'retain-on-failure', //'on',//
 					size: viewportSize,
-			},
+			  },
 
 		recordVideoOptions: {
 			mode: 'retain-on-failure',
