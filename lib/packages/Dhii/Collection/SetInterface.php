f5ec6fc0a7dadfc76e787c2e69b19d0c0c792a52
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-02-06 13:29:11 +0000

    [BOT] Add build artifact from dev/PCP-5744-check-recaptcha-enabled-before-rendering-v2-container -> PCP-5744-check-recaptcha-enabled-before-rendering-v2-container.

--- lib/packages/Dhii/Collection/SetInterface.php
+++ lib/packages/Dhii/Collection/SetInterface.php
@@ -1,21 +1,14 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Vendor\Dhii\Collection;
 
 use Traversable;
-
 /**
  * A list that can be checked for a key.
  *
  * @since 0.2
  */
-interface SetInterface extends
-    /* @since 0.2 */
-    Traversable,
-    /* @since 0.2 */
-    HasItemCapableInterface
+interface SetInterface extends Traversable, \WooCommerce\PayPalCommerce\Vendor\Dhii\Collection\HasItemCapableInterface
 {
-
 }
--- modules/ppcp-button/src/Endpoint/EndpointInterface.php
+++ modules/ppcp-button/src/Endpoint/EndpointInterface.php
@@ -1,30 +1,28 @@
 <?php
+
 /**
  * The Endpoint interface.
  *
  * @package WooCommerce\PayPalCommerce\Button\Endpoint
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Button\Endpoint;
 
 /**
  * Interface EndpointInterface
  */
-interface EndpointInterface {
-
-	/**
-	 * Returns the nonce for an endpoint.
-	 *
-	 * @return string
-	 */
-	public static function nonce(): string;
-
-	/**
-	 * Handles the request for an endpoint.
-	 *
-	 * @return bool
-	 */
-	public function handle_request(): bool;
+interface EndpointInterface
+{
+    /**
+     * Returns the nonce for an endpoint.
+     *
+     * @return string
+     */
+    public static function nonce(): string;
+    /**
+     * Handles the request for an endpoint.
+     *
+     * @return bool
+     */
+    public function handle_request(): bool;
 }
