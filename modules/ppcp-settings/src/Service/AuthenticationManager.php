ba1ffe591f06dc2d3ef1d2be387fe07f6e7ac65f
Emili Castells Guasch <e.castells@inpsyde.com>
2026-02-09 11:52:00 +0100

    Fix client_secret error message and add client_id pattern validation

--- modules/ppcp-settings/src/Service/AuthenticationManager.php
+++ modules/ppcp-settings/src/Service/AuthenticationManager.php
@@ -180,12 +180,18 @@ class AuthenticationManager {
 			throw new RuntimeException( 'No client ID provided.' );
 		}
 
-		if ( false === preg_match( '/^A[\w-]{79}$/', $client_secret ) ) {
+		// Exactly 80 alphanumeric, underscore, or hyphen characters.
+		if ( 1 !== preg_match( '/^[\w-]{80}$/', $client_id ) ) {
 			throw new RuntimeException( 'Invalid client ID provided.' );
 		}
 
 		if ( empty( $client_secret ) ) {
 			throw new RuntimeException( 'No client secret provided.' );
+		}
+
+		// Starts with "A", followed by 79 alphanumeric, underscore, or hyphen characters.
+		if ( 1 !== preg_match( '/^A[\w-]{79}$/', $client_secret ) ) {
+			throw new RuntimeException( 'Invalid client secret provided.' );
 		}
 	}
 
