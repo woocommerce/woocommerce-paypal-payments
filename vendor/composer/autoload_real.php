85fc2aca68d92604ca4bd0e931217d27203c1649
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-02-20 08:57:28 +0000

    [BOT] Add build artifact from dev/PCP-5897-branded-only-acdc-visible-on-block-chekout -> PCP-5897-branded-only-acdc-visible-on-block-chekout.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit8f1360c3598043abaa942bfe9de529b6
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit8f1360c3598043abaa942bfe9de529b6::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_7bfa48aafcb2d87577d78d9c4d12fc046bca84ad'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_7bfa48aafcb2d87577d78d9c4d12fc046bca84ad'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_a2a8bee0766dcf5e9ded0b80256bce35bd4cfb45'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_a2a8bee0766dcf5e9ded0b80256bce35bd4cfb45'][$fileIdentifier] = true;
 
                 require $file;
             }
