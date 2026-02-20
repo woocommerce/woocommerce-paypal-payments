08dac227ced2937ddf1c6b832357f56afc647bfb
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-02-20 13:59:51 +0000

    [BOT] Add build artifact from dev/PCP-5928-fix-wp-imports -> PCP-5928-fix-wp-imports.

---
+++ assets/ppcp-settings-js-index.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'ed47571487ee841d9851');
+}
--- tests/qa/tests/02-onboarding/_test-data/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './onboarding-learn-more-links.data';
-export * from './onboarding-ui.data';
-export * from './onboarding-badges.data';
