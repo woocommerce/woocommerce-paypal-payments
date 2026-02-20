cba97bb52f2f0236f4a4d5d5f9663403bfd34de4
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-02-20 13:41:05 +0000

    [BOT] Add build artifact from dev/develop -> develop.

--- vendor/scoper-autoload.php
+++ vendor/scoper-autoload.php
@@ -29,7 +29,7 @@ if (!function_exists('humbug_phpscoper_expose_class')) {
         }
     }
 }
-humbug_phpscoper_expose_class('ComposerAutoloaderInit8f1360c3598043abaa942bfe9de529b6', 'WooCommerce\PayPalCommerce\Vendor\ComposerAutoloaderInit8f1360c3598043abaa942bfe9de529b6');
+humbug_phpscoper_expose_class('ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b', 'WooCommerce\PayPalCommerce\Vendor\ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b');
 
 // Function aliases. For more information see:
 // https://github.com/humbug/php-scoper/blob/master/docs/further-reading.md#function-aliases
