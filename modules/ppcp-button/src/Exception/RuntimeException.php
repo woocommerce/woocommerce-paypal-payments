8789a9a056c6f93fd13830ed04bba3d5ae425e8f
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-02-24 13:33:41 +0000

    [BOT] Add build artifact from dev/PCP-5908-fix-pay-later-check -> PCP-5908-fix-pay-later-check.

--- modules/ppcp-button/src/Exception/RuntimeException.php
+++ modules/ppcp-button/src/Exception/RuntimeException.php
@@ -1,18 +1,16 @@
 <?php
+
 /**
  * The modules Runtime Exception.
  *
  * @package WooCommerce\PayPalCommerce\Button\Exception
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Button\Exception;
 
 /**
  * Class RuntimeException
  */
-class RuntimeException extends \RuntimeException {
-
-
+class RuntimeException extends \RuntimeException
+{
 }
