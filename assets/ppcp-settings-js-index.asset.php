80ee283835f53a71315b6edfc7b65caec595b1a7
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-02-24 11:56:49 +0000

    [BOT] Add build artifact from dev/PCP-5413-remove-legacy-ui -> PCP-5413-remove-legacy-ui.

--- assets/ppcp-settings-js-index.asset.php
+++ assets/ppcp-settings-js-index.asset.php
@@ -1,5 +1,5 @@
 <?php
 
 namespace {
-    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'bbe105bbc66cadb5c99a');
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'b8847ff5615a61c1af74');
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf454a4dc4519aa3bafab294be971ae9b::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_2fe533240bfb0dc716ef66384d8ff02fda21ecf1'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_2fe533240bfb0dc716ef66384d8ff02fda21ecf1'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_c35b84ceaa93c3bceec0436de05818f2cc8ba0d9'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_c35b84ceaa93c3bceec0436de05818f2cc8ba0d9'][$fileIdentifier] = true;
 
                 require $file;
             }
