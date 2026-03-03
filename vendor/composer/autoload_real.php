c2f587dedabcb44d2950d4aa8a7cc0e964f55c2e
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-03 09:31:01 +0000

    [BOT] Add build artifact from dev/PCP-5603-add-safety-for-order-id-calls -> PCP-5603-add-safety-for-order-id-calls.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf454a4dc4519aa3bafab294be971ae9b::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_593858aa54cda4b5966c82277dea120e7d240f54'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_593858aa54cda4b5966c82277dea120e7d240f54'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_815270eb6e707db7a7978deab7ca2628ebac59fb'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_815270eb6e707db7a7978deab7ca2628ebac59fb'][$fileIdentifier] = true;
 
                 require $file;
             }
