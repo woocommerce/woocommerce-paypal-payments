7de8c86602d451c2386e9ac5bc07a00138565604
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-03 12:24:16 +0000

    [BOT] Add build artifact from dev/PCP-5658-google-pay-and-apple-pay-gateway-visible-on-classic-checkout-for-countries-that-are-not-eligible-for-acdc -> PCP-5658-google-pay-and-apple-pay-gateway-visible-on-classic-checkout-for-countries-that-are-not-eligible-for-acdc.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf454a4dc4519aa3bafab294be971ae9b::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_1948f4ad44e230e517ab87c844b9c64ed6f1313a'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_1948f4ad44e230e517ab87c844b9c64ed6f1313a'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_db4707119a15cea1b7d2b96abea0106566f61b72'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_db4707119a15cea1b7d2b96abea0106566f61b72'][$fileIdentifier] = true;
 
                 require $file;
             }
