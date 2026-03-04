37861161817a352ad02e0af242ccfd09d1f56b2c
Emili Castells Guasch <e.castells@inpsyde.com>
2026-03-04 16:04:38 +0100

    Ensure no wc subscriptions code is executed when the plugin is not active

--- modules/ppcp-wc-subscriptions/src/WcSubscriptionsModule.php
+++ modules/ppcp-wc-subscriptions/src/WcSubscriptionsModule.php
@@ -73,6 +73,12 @@ class WcSubscriptionsModule implements ServiceModule, ExtendingModule, Executabl
 	 * {@inheritDoc}
 	 */
 	public function run( ContainerInterface $c ): bool {
+		$subscriptions_helper = $c->get( 'wc-subscriptions.helper' );
+		assert( $subscriptions_helper instanceof SubscriptionHelper );
+		if ( ! $subscriptions_helper->plugin_is_active() ) {
+			return true;
+		}
+
 		$this->add_gateways_support( $c );
 
 		add_action(
@@ -477,12 +483,6 @@ class WcSubscriptionsModule implements ServiceModule, ExtendingModule, Executabl
 	 * @return void
 	 */
 	private function add_gateways_support( ContainerInterface $c ): void {
-		$subscriptions_helper = $c->get( 'wc-subscriptions.helper' );
-		assert( $subscriptions_helper instanceof SubscriptionHelper );
-		if ( ! $subscriptions_helper->plugin_is_active() ) {
-			return;
-		}
-
 		add_filter(
 			'woocommerce_paypal_payments_paypal_gateway_supports',
 			function ( array $supports ) use ( $c ): array {
