b260a9c41c54f8f7bbada6394178e6eafee9058d
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-04 06:49:38 +0000

    [BOT] Add build artifact from dev/PCP-5926-playground-demo-workflow-is-failing-due-to-outdated-dependency -> PCP-5926-playground-demo-workflow-is-failing-due-to-outdated-dependency.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf454a4dc4519aa3bafab294be971ae9b::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_e9735d493d5ab9552dc17480f63cb87466089596'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_e9735d493d5ab9552dc17480f63cb87466089596'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_8be1a22ca0d4d71b6b92c9cb39a86e3683e01372'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_8be1a22ca0d4d71b6b92c9cb39a86e3683e01372'][$fileIdentifier] = true;
 
                 require $file;
             }
