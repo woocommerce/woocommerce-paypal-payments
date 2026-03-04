42c4495c1d5bf55f3c1b407593ce5040de3b4bdb
Narek Zakarian <narekzakarian@gmail.com>
2026-03-04 18:00:24 +0400

    feat(axo): guard watermark rendering in block checkout Watermark component

--- modules/ppcp-axo-block/resources/js/components/Watermark/Watermark.js
+++ modules/ppcp-axo-block/resources/js/components/Watermark/Watermark.js
@@ -17,13 +17,14 @@ const Watermark = ( {
 } ) => {
 	const containerRef = useRef( null );
 	const watermarkRef = useRef( null );
+	const axoConfig = window.wc_ppcp_axo;
 
 	useEffect( () => {
 		/**
 		 * Renders the Fastlane watermark.
 		 */
 		const renderWatermark = async () => {
-			if ( ! containerRef.current ) {
+			if ( ! containerRef.current || axoConfig.show_watermark !== '1' ) {
 				return;
 			}
 
