304dabb8c2abf64a97c22cdef041a1261c2f869e
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-05 11:19:21 +0000

    [BOT] Add build artifact from dev/PCP-5942-add-pui-to-initial-onboarding -> PCP-5942-add-pui-to-initial-onboarding.

--- modules/ppcp-api-client/src/Factory/PatchCollectionFactory.php
+++ modules/ppcp-api-client/src/Factory/PatchCollectionFactory.php
@@ -1,91 +1,70 @@
 <?php
+
 /**
  * The PatchCollection factory.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Factory
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Factory;
 
 use WooCommerce\PayPalCommerce\ApiClient\Entity\Order;
 use WooCommerce\PayPalCommerce\ApiClient\Entity\Patch;
 use WooCommerce\PayPalCommerce\ApiClient\Entity\PatchCollection;
 use WooCommerce\PayPalCommerce\ApiClient\Entity\PurchaseUnit;
-
 /**
  * Class PatchCollectionFactory
  */
-class PatchCollectionFactory {
-
-
-	/**
-	 * Creates a Patch Collection by comparing two orders.
-	 *
-	 * @param Order $from The initial order.
-	 * @param Order $to The target order.
-	 *
-	 * @return PatchCollection
-	 */
-	public function from_orders( Order $from, Order $to ): PatchCollection {
-		$all_patches  = array();
-		$all_patches += $this->purchase_units( $from->purchase_units(), $to->purchase_units() );
-
-		return new PatchCollection( ...$all_patches );
-	}
-
-	/**
-	 * Returns patches from purchase units diffs.
-	 *
-	 * @param PurchaseUnit[] $from The Purchase Units to start with.
-	 * @param PurchaseUnit[] $to The Purchase Units to end with after patches where applied.
-	 *
-	 * @return Patch[]
-	 */
-	private function purchase_units( array $from, array $to ): array {
-		$patches = array();
-
-		$path = '/purchase_units';
-		foreach ( $to as $purchase_unit_to ) {
-			$needs_update = ! count(
-				array_filter(
-					$from,
-					static function ( PurchaseUnit $unit ) use ( $purchase_unit_to ): bool {
-						// Loose comparison needed to compare two objects.
-						// phpcs:ignore Universal.Operators.StrictComparisons.LooseEqual
-						return $unit == $purchase_unit_to;
-					}
-				)
-			);
-			if ( ! $needs_update ) {
-				continue;
-			}
-			$purchase_unit_from = current(
-				array_filter(
-					$from,
-					static function ( PurchaseUnit $unit ) use ( $purchase_unit_to ): bool {
-						return $purchase_unit_to->reference_id() === $unit->reference_id();
-					}
-				)
-			);
-			$operation          = $purchase_unit_from ? 'replace' : 'add';
-			$value              = $purchase_unit_to->to_array();
-
-			if ( ! isset( $value['shipping'] ) ) {
-				$shipping = $purchase_unit_from && null !== $purchase_unit_from->shipping() ? $purchase_unit_from->shipping() : null;
-				if ( $shipping ) {
-					$value['shipping'] = $shipping->to_array();
-				}
-			}
-
-			$patches[] = new Patch(
-				$operation,
-				$path . "/@reference_id=='" . $purchase_unit_to->reference_id() . "'",
-				$value
-			);
-		}
-
-		return $patches;
-	}
+class PatchCollectionFactory
+{
+    /**
+     * Creates a Patch Collection by comparing two orders.
+     *
+     * @param Order $from The initial order.
+     * @param Order $to The target order.
+     *
+     * @return PatchCollection
+     */
+    public function from_orders(Order $from, Order $to): PatchCollection
+    {
+        $all_patches = array();
+        $all_patches += $this->purchase_units($from->purchase_units(), $to->purchase_units());
+        return new PatchCollection(...$all_patches);
+    }
+    /**
+     * Returns patches from purchase units diffs.
+     *
+     * @param PurchaseUnit[] $from The Purchase Units to start with.
+     * @param PurchaseUnit[] $to The Purchase Units to end with after patches where applied.
+     *
+     * @return Patch[]
+     */
+    private function purchase_units(array $from, array $to): array
+    {
+        $patches = array();
+        $path = '/purchase_units';
+        foreach ($to as $purchase_unit_to) {
+            $needs_update = !count(array_filter($from, static function (PurchaseUnit $unit) use ($purchase_unit_to): bool {
+                // Loose comparison needed to compare two objects.
+                // phpcs:ignore Universal.Operators.StrictComparisons.LooseEqual
+                return $unit == $purchase_unit_to;
+            }));
+            if (!$needs_update) {
+                continue;
+            }
+            $purchase_unit_from = current(array_filter($from, static function (PurchaseUnit $unit) use ($purchase_unit_to): bool {
+                return $purchase_unit_to->reference_id() === $unit->reference_id();
+            }));
+            $operation = $purchase_unit_from ? 'replace' : 'add';
+            $value = $purchase_unit_to->to_array();
+            if (!isset($value['shipping'])) {
+                $shipping = $purchase_unit_from && null !== $purchase_unit_from->shipping() ? $purchase_unit_from->shipping() : null;
+                if ($shipping) {
+                    $value['shipping'] = $shipping->to_array();
+                }
+            }
+            $patches[] = new Patch($operation, $path . "/@reference_id=='" . $purchase_unit_to->reference_id() . "'", $value);
+        }
+        return $patches;
+    }
 }
--- modules/ppcp-compat/src/PPEC/PPECHelper.php
+++ modules/ppcp-compat/src/PPEC/PPECHelper.php
@@ -1,118 +1,93 @@
 <?php
+
 /**
  * PayPal Express Checkout helper class.
  *
  * @package WooCommerce\PayPalCommerce\Compat\PPEC
  */
-
 namespace WooCommerce\PayPalCommerce\Compat\PPEC;
 
 use Automattic\WooCommerce\Utilities\OrderUtil;
-
 /**
  * Helper class with various constants associated to the PayPal Express Checkout plugin, as well as methods for figuring
  * out the status of the gateway.
  */
-class PPECHelper {
-
-	/**
-	 * The PayPal Express Checkout gateway ID.
-	 */
-	const PPEC_GATEWAY_ID = 'ppec_paypal';
-
-	/**
-	 * The path to the PayPal Express Checkout main plugin file.
-	 */
-	const PPEC_PLUGIN_FILE = 'woocommerce-gateway-paypal-express-checkout/woocommerce-gateway-paypal-express-checkout.php';
-
-	/**
-	 * Option name for PayPal Express Checkout settings.
-	 */
-	const PPEC_SETTINGS_OPTION_NAME = 'woocommerce_ppec_paypal_settings';
-
-
-	/**
-	 * Checks if the PayPal Express Checkout plugin was configured previously.
-	 *
-	 * @return bool
-	 */
-	public static function is_plugin_configured() {
-		return is_array( get_option( self::PPEC_SETTINGS_OPTION_NAME ) );
-	}
-
-	/**
-	 * Checks if the PayPal Express Checkout plugin is active.
-	 */
-	public static function is_plugin_active(): bool {
-		return is_callable( 'wc_gateway_ppec' ); // @phpstan-ignore function.impossibleType
-	}
-
-	/**
-	 * Checks whether the PayPal Express Checkout plugin is available (plugin active and gateway configured).
-	 *
-	 * @return bool
-	 */
-	public static function is_gateway_available() {
-		if ( ! self::is_plugin_active() || ! is_callable( 'wc_gateway_ppec' ) ) { // @phpstan-ignore function.impossibleType
-			return false;
-		}
-
-		$ppec = wc_gateway_ppec(); // @phpstan-ignore function.notFound
-		return is_object( $ppec ) && $ppec->settings && $ppec->settings->get_active_api_credentials();
-	}
-
-	/**
-	 * Checks whether the site has subscriptions handled through PayPal Express Checkout.
-	 *
-	 * @return bool
-	 */
-	public static function site_has_ppec_subscriptions() {
-		$has_ppec_subscriptions = get_transient( 'ppcp_has_ppec_subscriptions' );
-		if ( $has_ppec_subscriptions !== false ) {
-			return $has_ppec_subscriptions === 'true';
-		}
-
-		global $wpdb;
-		if ( class_exists( OrderUtil::class ) && OrderUtil::custom_orders_table_usage_is_enabled() ) {
-			$result = $wpdb->get_var(
-				$wpdb->prepare(
-					"SELECT 1 FROM {$wpdb->prefix}wc_orders WHERE payment_method = %s",
-					self::PPEC_GATEWAY_ID
-				)
-			);
-		} else {
-			$result = $wpdb->get_var(
-				$wpdb->prepare(
-					"SELECT 1 FROM {$wpdb->posts} p JOIN {$wpdb->postmeta} pm ON pm.post_id = p.ID
-				WHERE p.post_type = %s AND p.post_status != %s AND pm.meta_key = %s AND pm.meta_value = %s LIMIT 1",
-					'shop_subscription',
-					'trash',
-					'_payment_method',
-					self::PPEC_GATEWAY_ID
-				)
-			);
-		}
-
-		set_transient(
-			'ppcp_has_ppec_subscriptions',
-			! empty( $result ) ? 'true' : 'false',
-			MONTH_IN_SECONDS
-		);
-
-		return ! empty( $result );
-	}
-
-	/**
-	 * Checks whether the compatibility layer for PPEC Subscriptions should be initialized.
-	 *
-	 * @return bool
-	 */
-	public static function use_ppec_compat_layer_for_subscriptions() {
-		/**
-		 * The filter returning whether the compatibility layer for PPEC Subscriptions should be initialized.
-		 */
-		return ( ! self::is_gateway_available() )
-			&& self::site_has_ppec_subscriptions()
-			&& apply_filters( 'woocommerce_paypal_payments_process_legacy_subscriptions', true );
-	}
+class PPECHelper
+{
+    /**
+     * The PayPal Express Checkout gateway ID.
+     */
+    const PPEC_GATEWAY_ID = 'ppec_paypal';
+    /**
+     * The path to the PayPal Express Checkout main plugin file.
+     */
+    const PPEC_PLUGIN_FILE = 'woocommerce-gateway-paypal-express-checkout/woocommerce-gateway-paypal-express-checkout.php';
+    /**
+     * Option name for PayPal Express Checkout settings.
+     */
+    const PPEC_SETTINGS_OPTION_NAME = 'woocommerce_ppec_paypal_settings';
+    /**
+     * Checks if the PayPal Express Checkout plugin was configured previously.
+     *
+     * @return bool
+     */
+    public static function is_plugin_configured()
+    {
+        return is_array(get_option(self::PPEC_SETTINGS_OPTION_NAME));
+    }
+    /**
+     * Checks if the PayPal Express Checkout plugin is active.
+     */
+    public static function is_plugin_active(): bool
+    {
+        return is_callable('wc_gateway_ppec');
+        // @phpstan-ignore function.impossibleType
+    }
+    /**
+     * Checks whether the PayPal Express Checkout plugin is available (plugin active and gateway configured).
+     *
+     * @return bool
+     */
+    public static function is_gateway_available()
+    {
+        if (!self::is_plugin_active() || !is_callable('wc_gateway_ppec')) {
+            // @phpstan-ignore function.impossibleType
+            return \false;
+        }
+        $ppec = wc_gateway_ppec();
+        // @phpstan-ignore function.notFound
+        return is_object($ppec) && $ppec->settings && $ppec->settings->get_active_api_credentials();
+    }
+    /**
+     * Checks whether the site has subscriptions handled through PayPal Express Checkout.
+     *
+     * @return bool
+     */
+    public static function site_has_ppec_subscriptions()
+    {
+        $has_ppec_subscriptions = get_transient('ppcp_has_ppec_subscriptions');
+        if ($has_ppec_subscriptions !== \false) {
+            return $has_ppec_subscriptions === 'true';
+        }
+        global $wpdb;
+        if (class_exists(OrderUtil::class) && OrderUtil::custom_orders_table_usage_is_enabled()) {
+            $result = $wpdb->get_var($wpdb->prepare("SELECT 1 FROM {$wpdb->prefix}wc_orders WHERE payment_method = %s", self::PPEC_GATEWAY_ID));
+        } else {
+            $result = $wpdb->get_var($wpdb->prepare("SELECT 1 FROM {$wpdb->posts} p JOIN {$wpdb->postmeta} pm ON pm.post_id = p.ID\n\t\t\t\tWHERE p.post_type = %s AND p.post_status != %s AND pm.meta_key = %s AND pm.meta_value = %s LIMIT 1", 'shop_subscription', 'trash', '_payment_method', self::PPEC_GATEWAY_ID));
+        }
+        set_transient('ppcp_has_ppec_subscriptions', !empty($result) ? 'true' : 'false', MONTH_IN_SECONDS);
+        return !empty($result);
+    }
+    /**
+     * Checks whether the compatibility layer for PPEC Subscriptions should be initialized.
+     *
+     * @return bool
+     */
+    public static function use_ppec_compat_layer_for_subscriptions()
+    {
+        /**
+         * The filter returning whether the compatibility layer for PPEC Subscriptions should be initialized.
+         */
+        return !self::is_gateway_available() && self::site_has_ppec_subscriptions() && apply_filters('woocommerce_paypal_payments_process_legacy_subscriptions', \true);
+    }
 }
