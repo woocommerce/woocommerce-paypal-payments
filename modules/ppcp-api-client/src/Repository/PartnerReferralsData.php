fc8c61ebabd864948d0c54470d99284946171365
Narek Zakarian <narekzakarian@gmail.com>
2026-03-06 12:44:33 +0400

    refactor(onboarding): use `FeaturesDefinition::is_feature_eligible()` for PUI check

--- modules/ppcp-api-client/src/Repository/PartnerReferralsData.php
+++ modules/ppcp-api-client/src/Repository/PartnerReferralsData.php
@@ -10,6 +10,7 @@ declare( strict_types = 1 );
 namespace WooCommerce\PayPalCommerce\ApiClient\Repository;
 
 use WooCommerce\PayPalCommerce\ApiClient\Helper\DccApplies;
+use WooCommerce\PayPalCommerce\Settings\Data\Definition\FeaturesDefinition;
 
 class PartnerReferralsData {
 	/**
@@ -22,11 +23,11 @@ class PartnerReferralsData {
 	 * @var DccApplies
 	 */
 	private DccApplies $dcc_applies;
-	protected bool $is_pui_eligible;
+	protected FeaturesDefinition $features_definition;
 
-	public function __construct( DccApplies $dcc_applies, bool $is_pui_eligible ) {
+	public function __construct( DccApplies $dcc_applies, FeaturesDefinition $features_definition ) {
 		$this->dcc_applies     = $dcc_applies; // @phpstan-ignore property.deprecated
-		$this->is_pui_eligible = $is_pui_eligible;
+		$this->features_definition = $features_definition;
 	}
 
 	/**
@@ -100,7 +101,7 @@ class PartnerReferralsData {
 			$first_party_features[] = 'FUTURE_PAYMENT';
 		}
 
-		if ( $this->is_pui_eligible ) {
+		if ( $this->features_definition->is_feature_eligible( FeaturesDefinition::FEATURE_PAY_UPON_INVOICE ) ) {
 			$products[]     = 'PAYMENT_METHODS';
 			$capabilities[] = 'PAY_UPON_INVOICE';
 		}
