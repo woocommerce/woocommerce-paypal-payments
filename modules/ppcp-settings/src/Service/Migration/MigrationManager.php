2e01f068eddd63f7346f0a957de223395581f6e9
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-06 12:53:25 +0000

    [BOT] Add build artifact from dev/PCP-5994-merchant-not-onboarded-when-upgrading-from-previous-version -> PCP-5994-merchant-not-onboarded-when-upgrading-from-previous-version.

--- modules/ppcp-settings/src/Service/Migration/MigrationManager.php
+++ modules/ppcp-settings/src/Service/Migration/MigrationManager.php
@@ -1,85 +1,57 @@
 <?php
+
 /**
  * Provides functionality for settings migration management.
  *
  * @package WooCommerce\PayPalCommerce\Settings\Service\Migration
  */
-
-declare( strict_types = 1 );
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Settings\Service\Migration;
 
 use Exception;
-use Psr\Log\LoggerInterface;
+use WooCommerce\PayPalCommerce\Vendor\Psr\Log\LoggerInterface;
 use WooCommerce\PayPalCommerce\Settings\Data\OnboardingProfile;
-
 /**
  * Class MigrationManager
  *
  * Manages migration operations for plugin settings.
  */
-class MigrationManager implements SettingsMigrationInterface {
-
-	public const OPTION_NAME_MIGRATION_IS_DONE = 'woocommerce_ppcp-settings-migration-is-done';
-
-	protected SettingsMigration $general_settings_migration;
-	protected SettingsTabMigration $settings_tab_migration;
-	protected StylingSettingsMigration $styling_settings_migration;
-	protected PaymentSettingsMigration $payment_settings_migration;
-	protected FastlaneSettingsMigration $fastlane_settings_migration;
-	protected OnboardingProfile $onboarding_profile;
-	protected LoggerInterface $logger;
-
-	public function __construct(
-		SettingsMigration $general_settings_migration,
-		SettingsTabMigration $settings_tab_migration,
-		StylingSettingsMigration $styling_settings_migration,
-		PaymentSettingsMigration $payment_settings_migration,
-		FastlaneSettingsMigration $fastlane_settings_migration,
-		OnboardingProfile $onboarding_profile,
-		LoggerInterface $logger
-	) {
-		$this->general_settings_migration  = $general_settings_migration;
-		$this->settings_tab_migration      = $settings_tab_migration;
-		$this->styling_settings_migration  = $styling_settings_migration;
-		$this->payment_settings_migration  = $payment_settings_migration;
-		$this->fastlane_settings_migration = $fastlane_settings_migration;
-		$this->onboarding_profile          = $onboarding_profile;
-		$this->logger                      = $logger;
-	}
-
-	public function migrate(): void {
-		delete_option( 'woocommerce_ppcp-settings-should-use-old-ui' );
-		delete_option( 'woocommerce-ppcp-is-new-merchant' );
-
-		$this->onboarding_profile->set_completed( true );
-		$this->onboarding_profile->set_gateways_refreshed( true );
-		$this->onboarding_profile->set_gateways_synced( true );
-		$this->onboarding_profile->save();
-
-		$migrations = array(
-			'general_settings' => $this->general_settings_migration,
-			'settings_tab'     => $this->settings_tab_migration,
-			'styling'          => $this->styling_settings_migration,
-			'payment'          => $this->payment_settings_migration,
-			'fastlane'         => $this->fastlane_settings_migration,
-		);
-
-		foreach ( $migrations as $name => $migration ) {
-			try {
-				$migration->migrate();
-			} catch ( Exception $error ) {
-				$this->logger->warning(
-					"Settings migration failed for '{$name}' during transition to new UI",
-					array(
-						'error_message' => $error->getMessage(),
-						'error_code'    => $error->getCode(),
-						'trace'         => $error->getTraceAsString(),
-					)
-				);
-			}
-		}
-
-		update_option( self::OPTION_NAME_MIGRATION_IS_DONE, true );
-	}
+class MigrationManager implements \WooCommerce\PayPalCommerce\Settings\Service\Migration\SettingsMigrationInterface
+{
+    public const OPTION_NAME_MIGRATION_IS_DONE = 'woocommerce_ppcp-settings-migration-is-done';
+    protected \WooCommerce\PayPalCommerce\Settings\Service\Migration\SettingsMigration $general_settings_migration;
+    protected \WooCommerce\PayPalCommerce\Settings\Service\Migration\SettingsTabMigration $settings_tab_migration;
+    protected \WooCommerce\PayPalCommerce\Settings\Service\Migration\StylingSettingsMigration $styling_settings_migration;
+    protected \WooCommerce\PayPalCommerce\Settings\Service\Migration\PaymentSettingsMigration $payment_settings_migration;
+    protected \WooCommerce\PayPalCommerce\Settings\Service\Migration\FastlaneSettingsMigration $fastlane_settings_migration;
+    protected OnboardingProfile $onboarding_profile;
+    protected LoggerInterface $logger;
+    public function __construct(\WooCommerce\PayPalCommerce\Settings\Service\Migration\SettingsMigration $general_settings_migration, \WooCommerce\PayPalCommerce\Settings\Service\Migration\SettingsTabMigration $settings_tab_migration, \WooCommerce\PayPalCommerce\Settings\Service\Migration\StylingSettingsMigration $styling_settings_migration, \WooCommerce\PayPalCommerce\Settings\Service\Migration\PaymentSettingsMigration $payment_settings_migration, \WooCommerce\PayPalCommerce\Settings\Service\Migration\FastlaneSettingsMigration $fastlane_settings_migration, OnboardingProfile $onboarding_profile, LoggerInterface $logger)
+    {
+        $this->general_settings_migration = $general_settings_migration;
+        $this->settings_tab_migration = $settings_tab_migration;
+        $this->styling_settings_migration = $styling_settings_migration;
+        $this->payment_settings_migration = $payment_settings_migration;
+        $this->fastlane_settings_migration = $fastlane_settings_migration;
+        $this->onboarding_profile = $onboarding_profile;
+        $this->logger = $logger;
+    }
+    public function migrate(): void
+    {
+        delete_option('woocommerce_ppcp-settings-should-use-old-ui');
+        delete_option('woocommerce-ppcp-is-new-merchant');
+        $this->onboarding_profile->set_completed(\true);
+        $this->onboarding_profile->set_gateways_refreshed(\true);
+        $this->onboarding_profile->set_gateways_synced(\true);
+        $this->onboarding_profile->save();
+        $migrations = array('general_settings' => $this->general_settings_migration, 'settings_tab' => $this->settings_tab_migration, 'styling' => $this->styling_settings_migration, 'payment' => $this->payment_settings_migration, 'fastlane' => $this->fastlane_settings_migration);
+        foreach ($migrations as $name => $migration) {
+            try {
+                $migration->migrate();
+            } catch (Exception $error) {
+                $this->logger->warning("Settings migration failed for '{$name}' during transition to new UI", array('error_message' => $error->getMessage(), 'error_code' => $error->getCode(), 'trace' => $error->getTraceAsString()));
+            }
+        }
+        update_option(self::OPTION_NAME_MIGRATION_IS_DONE, \true);
+    }
 }
