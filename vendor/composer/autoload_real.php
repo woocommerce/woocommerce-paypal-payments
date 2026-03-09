4f7b26bad0ffd650578eba41fa1f64b72252a011
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-09 11:16:15 +0000

    [BOT] Add build artifact from dev/develop -> develop.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf454a4dc4519aa3bafab294be971ae9b::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_bee6f97d923e2c66d339b10cdcbf4da4ff9af402'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_bee6f97d923e2c66d339b10cdcbf4da4ff9af402'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_dd15721de4399a7c3ce901be7e6561145a34ba2a'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_dd15721de4399a7c3ce901be7e6561145a34ba2a'][$fileIdentifier] = true;
 
                 require $file;
             }
