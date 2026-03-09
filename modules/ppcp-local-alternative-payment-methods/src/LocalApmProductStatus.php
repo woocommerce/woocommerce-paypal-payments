aac4d2d8740a1f67e484278c4a79966a60b76093
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-09 15:40:46 +0000

    [BOT] Add build artifact from dev/PCP-5952-migration-capture-virtual-only-orders-when-enabled-not-preserved-when-we-migrate-to-new-ui -> PCP-5952-migration-capture-virtual-only-orders-when-enabled-not-preserved-when-we-migrate-to-new-ui.

--- modules/ppcp-local-alternative-payment-methods/src/LocalApmProductStatus.php
+++ modules/ppcp-local-alternative-payment-methods/src/LocalApmProductStatus.php
@@ -1,30 +1,28 @@
 <?php
+
 /**
  * Status of local alternative payment methods.
  *
  * @package WooCommerce\PayPalCommerce\LocalAlternativePaymentMethods
  */
-
-declare( strict_types = 1 );
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\LocalAlternativePaymentMethods;
 
 use WooCommerce\PayPalCommerce\ApiClient\Helper\ProductStatus;
 use WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatus;
-
-class LocalApmProductStatus extends ProductStatus {
-	public const KEY = 'products_local_apms_enabled';
-
-	protected function check_api_response( SellerStatus $seller_status ): bool {
-		foreach ( $seller_status->capabilities() as $capability ) {
-			if ( 'ACTIVE' !== $capability->status() ) {
-				continue;
-			}
-			if ( 'PAYPAL_CHECKOUT_ALTERNATIVE_PAYMENT_METHODS' === $capability->name() ) {
-				return true;
-			}
-		}
-
-		return false;
-	}
+class LocalApmProductStatus extends ProductStatus
+{
+    public const KEY = 'products_local_apms_enabled';
+    protected function check_api_response(SellerStatus $seller_status): bool
+    {
+        foreach ($seller_status->capabilities() as $capability) {
+            if ('ACTIVE' !== $capability->status()) {
+                continue;
+            }
+            if ('PAYPAL_CHECKOUT_ALTERNATIVE_PAYMENT_METHODS' === $capability->name()) {
+                return \true;
+            }
+        }
+        return \false;
+    }
 }
