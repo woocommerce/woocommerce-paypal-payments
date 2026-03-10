760c4b5bc20bc171dad9d76d6ff416803a36b50d
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-10 10:36:02 +0000

    [BOT] Add build artifact from dev/PCP-5929-migration-case-when-bcdc-is-part-of-pay-pal-or-as-separated-gateway-pay-later-messaging-tab-is-not-present -> PCP-5929-migration-case-when-bcdc-is-part-of-pay-pal-or-as-separated-gateway-pay-later-messaging-tab-is-not-present.

--- modules/ppcp-api-client/src/Entity/Payee.php
+++ modules/ppcp-api-client/src/Entity/Payee.php
@@ -1,79 +1,73 @@
 <?php
+
 /**
  * The payee object.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Entity
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Entity;
 
 /**
  * Class Payee
  * The entity, which receives the money.
  */
-class Payee {
-
-	/**
-	 * The email address.
-	 *
-	 * @var string
-	 */
-	private $email;
-
-	/**
-	 * The merchant id.
-	 *
-	 * @var string
-	 */
-	private $merchant_id;
-
-	/**
-	 * Payee constructor.
-	 *
-	 * @param string $email The email.
-	 * @param string $merchant_id The merchant id.
-	 */
-	public function __construct(
-		string $email,
-		string $merchant_id
-	) {
-
-		$this->email       = $email;
-		$this->merchant_id = $merchant_id;
-	}
-
-	/**
-	 * Returns the email.
-	 *
-	 * @return string
-	 */
-	public function email(): string {
-		return $this->email;
-	}
-
-	/**
-	 * Returns the merchant id.
-	 *
-	 * @return string
-	 */
-	public function merchant_id(): string {
-		return $this->merchant_id;
-	}
-
-	/**
-	 * Returns the object as array.
-	 *
-	 * @return array
-	 */
-	public function to_array(): array {
-		$data = array();
-		if ( $this->merchant_id ) {
-			$data['merchant_id'] = $this->merchant_id();
-		} else {
-			$data['email_address'] = $this->email();
-		}
-		return $data;
-	}
+class Payee
+{
+    /**
+     * The email address.
+     *
+     * @var string
+     */
+    private $email;
+    /**
+     * The merchant id.
+     *
+     * @var string
+     */
+    private $merchant_id;
+    /**
+     * Payee constructor.
+     *
+     * @param string $email The email.
+     * @param string $merchant_id The merchant id.
+     */
+    public function __construct(string $email, string $merchant_id)
+    {
+        $this->email = $email;
+        $this->merchant_id = $merchant_id;
+    }
+    /**
+     * Returns the email.
+     *
+     * @return string
+     */
+    public function email(): string
+    {
+        return $this->email;
+    }
+    /**
+     * Returns the merchant id.
+     *
+     * @return string
+     */
+    public function merchant_id(): string
+    {
+        return $this->merchant_id;
+    }
+    /**
+     * Returns the object as array.
+     *
+     * @return array
+     */
+    public function to_array(): array
+    {
+        $data = array();
+        if ($this->merchant_id) {
+            $data['merchant_id'] = $this->merchant_id();
+        } else {
+            $data['email_address'] = $this->email();
+        }
+        return $data;
+    }
 }
