a36c294a22b995831bdcc43c4dfcbc43b0ce1f34
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-12 09:19:24 +0000

    [BOT] Add build artifact from dev/PCP-6004-pay-with-crypto-gateway-visible-in-frontend-if-enabled-despite-merchant-not-eligible-setting-not-visible -> PCP-6004-pay-with-crypto-gateway-visible-in-frontend-if-enabled-despite-merchant-not-eligible-setting-not-visible.

--- modules/ppcp-status-report/services.php
+++ modules/ppcp-status-report/services.php
@@ -1,16 +1,13 @@
 <?php
+
 /**
  * The status report module services.
  *
  * @package WooCommerce\PayPalCommerce\StatusReport
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\StatusReport;
 
-return array(
-	'status-report.renderer' => static function (): Renderer {
-		return new Renderer();
-	},
-);
+return array('status-report.renderer' => static function (): \WooCommerce\PayPalCommerce\StatusReport\Renderer {
+    return new \WooCommerce\PayPalCommerce\StatusReport\Renderer();
+});
