e4ea439a3c5b90ce7478982b2348f33e4f80dec1
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-12 15:15:57 +0000

    [BOT] Add build artifact from dev/PCP-5629-acdc-for-unsupported-countries-for-save-payment-method-check-box-toggle-visible-for-acdc -> PCP-5629-acdc-for-unsupported-countries-for-save-payment-method-check-box-toggle-visible-for-acdc.

---
+++ assets/ppcp-paylater-wc-blocks-js-CartPayLaterMessagesBlock-cart-paylater-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5ab36c464e5953e513ab');
+}
