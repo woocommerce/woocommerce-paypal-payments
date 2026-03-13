97fd7b91676d0750f946b3d60f783ab523d6375d
Misha Utkin <m.utkin@inpsyde.com>
2026-03-13 10:29:39 +0100

    Add env reset to global-setup.ts

--- tests/qa-legacy-ui/global-setup.ts
+++ tests/qa-legacy-ui/global-setup.ts
@@ -3,8 +3,31 @@
  */
 import { FullConfig } from '@playwright/test';
 import { restLogin, guestStorageState } from '@inpsyde/playwright-utils/build';
+import { execFileSync  } from 'node:child_process';
 
 async function globalSetup( config: FullConfig ) {
+	// Reset env before the tests run, so the settings in next set of tests can be applied using legacy UI.
+	const SSH_LOGIN = process.env.SSH_LOGIN;
+	const SSH_HOST = process.env.SSH_HOST;
+	const SSH_PORT = process.env.SSH_PORT;
+	const WP_VERSION = process.env.WP_VERSION ?? '6.9';
+	const WP_TYPE = process.env.WP_TYPE ?? 'single';
+	
+	const remoteCmd = `$HOME/bin/reset-wp.sh --wp-version=${ WP_VERSION } --wp-type=${ WP_TYPE }`;
+	const sshArgs = [
+		`${ SSH_LOGIN }@${ SSH_HOST }`,
+		'-p', SSH_PORT,
+		'-o', 'StrictHostKeyChecking=no',
+		remoteCmd,
+	];
+
+	console.log( `Executing: ssh ${ sshArgs.join( ' ' ) }` );
+
+	execFileSync( 'ssh', sshArgs, {
+		stdio: 'inherit',
+		timeout: 60_000,
+	} );
+
 	const projectUse = config.projects[ 0 ].use;
 
 	await restLogin( {
