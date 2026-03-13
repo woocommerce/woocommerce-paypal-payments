d4167aad5fda1360536ffb651d2cc7cf733ef28d
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-13 15:53:31 +0000

    [BOT] Add build artifact from dev/PCP-6042-remove-order-status-requirement-for-pay-pal-payment-processing -> PCP-6042-remove-order-status-requirement-for-pay-pal-payment-processing.

--- modules/ppcp-applepay/src/Assets/AppleProductStatus.php
+++ modules/ppcp-applepay/src/Assets/AppleProductStatus.php
@@ -1,49 +1,43 @@
 <?php
+
 /**
  * Status of the ApplePay merchant connection.
  *
  * @package WooCommerce\PayPalCommerce\Applepay\Assets
  */
-
-declare( strict_types = 1 );
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Applepay\Assets;
 
 use WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatusCapability;
 use WooCommerce\PayPalCommerce\ApiClient\Helper\ProductStatus;
 use WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatus;
-
-class AppleProductStatus extends ProductStatus {
-	public const KEY             = 'products_apple_enabled';
-	public const CAPABILITY_NAME = 'APPLE_PAY';
-
-	public function check_local_state( bool $skip_filters = false ): ?bool {
-		$state = null;
-
-		if ( ! $skip_filters ) {
-			$state = apply_filters( 'woocommerce_paypal_payments_apple_pay_product_status', null );
-		}
-
-		return is_bool( $state ) ? $state : parent::check_local_state();
-	}
-
-	protected function check_api_response( SellerStatus $seller_status ): bool {
-		foreach ( $seller_status->products() as $product ) {
-			if ( $product->name() !== 'PAYMENT_METHODS' ) {
-				continue;
-			}
-
-			if ( in_array( self::CAPABILITY_NAME, $product->capabilities(), true ) ) {
-				return true;
-			}
-		}
-
-		foreach ( $seller_status->capabilities() as $capability ) {
-			if ( $capability->name() === self::CAPABILITY_NAME && $capability->status() === SellerStatusCapability::STATUS_ACTIVE ) {
-				return true;
-			}
-		}
-
-		return false;
-	}
+class AppleProductStatus extends ProductStatus
+{
+    public const KEY = 'products_apple_enabled';
+    public const CAPABILITY_NAME = 'APPLE_PAY';
+    public function check_local_state(bool $skip_filters = \false): ?bool
+    {
+        $state = null;
+        if (!$skip_filters) {
+            $state = apply_filters('woocommerce_paypal_payments_apple_pay_product_status', null);
+        }
+        return is_bool($state) ? $state : parent::check_local_state();
+    }
+    protected function check_api_response(SellerStatus $seller_status): bool
+    {
+        foreach ($seller_status->products() as $product) {
+            if ($product->name() !== 'PAYMENT_METHODS') {
+                continue;
+            }
+            if (in_array(self::CAPABILITY_NAME, $product->capabilities(), \true)) {
+                return \true;
+            }
+        }
+        foreach ($seller_status->capabilities() as $capability) {
+            if ($capability->name() === self::CAPABILITY_NAME && $capability->status() === SellerStatusCapability::STATUS_ACTIVE) {
+                return \true;
+            }
+        }
+        return \false;
+    }
 }
--- modules/ppcp-settings/src/DTO/ConfigurationFlagsDTO.php
+++ modules/ppcp-settings/src/DTO/ConfigurationFlagsDTO.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
  * Data transfer object. Stores flags that are relevant for the default plugin
  * configuration at the end of the onboarding process.
  *
  * @package WooCommerce\PayPalCommerce\Settings\DTO;
  */
-
-declare( strict_types = 1 );
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Settings\DTO;
 
 /**
@@ -15,33 +14,31 @@ namespace WooCommerce\PayPalCommerce\Settings\DTO;
  *
  * Intentionally has no internal logic, sanitation or validation.
  */
-class ConfigurationFlagsDTO {
-	/**
-	 * The merchant's country, which indicates availability of certain features.
-	 *
-	 * @var string
-	 */
-	public string $country_code = '';
-
-	/**
-	 * Whether we configure a business account.
-	 * If false, a personal (casual seller) account is set up.
-	 *
-	 * @var bool
-	 */
-	public bool $is_business_seller = false;
-
-	/**
-	 * Whether credit card payments should be handled by our plugin.
-	 *
-	 * @var bool
-	 */
-	public bool $use_card_payments = false;
-
-	/**
-	 * If the shop needs to process subscription payments.
-	 *
-	 * @var bool
-	 */
-	public bool $use_subscriptions = false;
+class ConfigurationFlagsDTO
+{
+    /**
+     * The merchant's country, which indicates availability of certain features.
+     *
+     * @var string
+     */
+    public string $country_code = '';
+    /**
+     * Whether we configure a business account.
+     * If false, a personal (casual seller) account is set up.
+     *
+     * @var bool
+     */
+    public bool $is_business_seller = \false;
+    /**
+     * Whether credit card payments should be handled by our plugin.
+     *
+     * @var bool
+     */
+    public bool $use_card_payments = \false;
+    /**
+     * If the shop needs to process subscription payments.
+     *
+     * @var bool
+     */
+    public bool $use_subscriptions = \false;
 }
