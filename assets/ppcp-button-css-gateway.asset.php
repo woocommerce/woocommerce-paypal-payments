3de231bb4fdf8f2dc173aeffe34eb2259056923c
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-17 08:36:20 +0000

    [BOT] Add build artifact from dev/PCP-6046-migration-pui-and-oxxo-do-not-persist-after-upgrade-from-2-9-6-to-4-0-0 -> PCP-6046-migration-pui-and-oxxo-do-not-persist-after-upgrade-from-2-9-6-to-4-0-0.

---
+++ assets/ppcp-button-css-gateway.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '7e1a68ce372845115be3');
+}
---
+++ assets/ppcp-settings-js-index.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '371d2d56035078c4ba4b');
+}
