512aaaf1f78f8baca23cc7fe83ee649108181f3c
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-17 13:27:36 +0000

    [BOT] Add build artifact from dev/release/4.0.0 -> release/4.0.0.

---
+++ assets/ppcp-googlepay-js-boot-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => '67357819fcbaea8b4ebf');
+}
--- modules/ppcp-paylater-configurator/src/PayLaterConfiguratorModule.php
+++ modules/ppcp-paylater-configurator/src/PayLaterConfiguratorModule.php
@@ -1,12 +1,11 @@
 <?php
+
 /**
  * The Pay Later configurator module.
  *
  * @package WooCommerce\PayPalCommerce\PayLaterConfigurator
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\PayLaterConfigurator;
 
 use WooCommerce\PayPalCommerce\PayLaterConfigurator\Endpoint\GetConfig;
@@ -17,70 +16,55 @@ use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ServiceModule;
 use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
 use WooCommerce\PayPalCommerce\Settings\Data\SettingsProvider;
 use WooCommerce\PayPalCommerce\Settings\Data\PayLaterMessagingSettings;
-
 /**
  * Class PayLaterConfiguratorModule
  */
-class PayLaterConfiguratorModule implements ServiceModule, ExecutableModule {
-	use ModuleClassNameIdTrait;
-
-	/**
-	 * Returns whether the module should be loaded.
-	 */
-	public static function is_enabled(): bool {
-		return apply_filters(
-		// phpcs:ignore WordPress.NamingConventions.ValidHookName.UseUnderscores
-			'woocommerce.feature-flags.woocommerce_paypal_payments.paylater_configurator_enabled',
-			getenv( 'PCP_PAYLATER_CONFIGURATOR' ) !== '0'
-		);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function services(): array {
-		return require __DIR__ . '/../services.php';
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function run( ContainerInterface $c ): bool {
-
-		add_action(
-			'init',
-			static function () use ( $c ) {
-				$is_available = $c->get( 'paylater-configurator.is-available' );
-				if ( ! $is_available ) {
-					return;
-				}
-
-				$settings_provider = $c->get( 'settings.settings-provider' );
-				assert( $settings_provider instanceof SettingsProvider );
-
-				$paylater_settings = $c->get( 'settings.data.paylater-messaging-settings' );
-				assert( $paylater_settings instanceof PayLaterMessagingSettings );
-
-				add_action(
-					'wc_ajax_' . SaveConfig::ENDPOINT,
-					static function () use ( $c ) {
-						$endpoint = $c->get( 'paylater-configurator.endpoint.save-config' );
-						assert( $endpoint instanceof SaveConfig );
-						$endpoint->handle_request();
-					}
-				);
-
-				add_action(
-					'wc_ajax_' . GetConfig::ENDPOINT,
-					static function () use ( $c ) {
-						$endpoint = $c->get( 'paylater-configurator.endpoint.get-config' );
-						assert( $endpoint instanceof GetConfig );
-						$endpoint->handle_request();
-					}
-				);
-			}
-		);
-
-		return true;
-	}
+class PayLaterConfiguratorModule implements ServiceModule, ExecutableModule
+{
+    use ModuleClassNameIdTrait;
+    /**
+     * Returns whether the module should be loaded.
+     */
+    public static function is_enabled(): bool
+    {
+        return apply_filters(
+            // phpcs:ignore WordPress.NamingConventions.ValidHookName.UseUnderscores
+            'woocommerce.feature-flags.woocommerce_paypal_payments.paylater_configurator_enabled',
+            getenv('PCP_PAYLATER_CONFIGURATOR') !== '0'
+        );
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function services(): array
+    {
+        return require __DIR__ . '/../services.php';
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function run(ContainerInterface $c): bool
+    {
+        add_action('init', static function () use ($c) {
+            $is_available = $c->get('paylater-configurator.is-available');
+            if (!$is_available) {
+                return;
+            }
+            $settings_provider = $c->get('settings.settings-provider');
+            assert($settings_provider instanceof SettingsProvider);
+            $paylater_settings = $c->get('settings.data.paylater-messaging-settings');
+            assert($paylater_settings instanceof PayLaterMessagingSettings);
+            add_action('wc_ajax_' . SaveConfig::ENDPOINT, static function () use ($c) {
+                $endpoint = $c->get('paylater-configurator.endpoint.save-config');
+                assert($endpoint instanceof SaveConfig);
+                $endpoint->handle_request();
+            });
+            add_action('wc_ajax_' . GetConfig::ENDPOINT, static function () use ($c) {
+                $endpoint = $c->get('paylater-configurator.endpoint.get-config');
+                assert($endpoint instanceof GetConfig);
+                $endpoint->handle_request();
+            });
+        });
+        return \true;
+    }
 }
