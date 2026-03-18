ccc91ac7c09e384d0224bd7b8cc12228ce8d20a8
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-18 22:56:21 +0000

    [BOT] Add build artifact from dev/PCP-6059-review-and-apply-crypto -> PCP-6059-review-and-apply-crypto.

---
+++ assets/ppcp-settings-js-index.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'ae516a2bb78802e996c6');
+}
