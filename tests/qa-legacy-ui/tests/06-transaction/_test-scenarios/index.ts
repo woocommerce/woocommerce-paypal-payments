6e2679d19a5e40832d9680bd3aa3c3c405baf0a7
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-19 16:32:59 +0000

    [BOT] Add build artifact from dev/PCP-6063-opcache-invalidation -> PCP-6063-opcache-invalidation.

--- tests/qa-legacy-ui/tests/06-transaction/_test-scenarios/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './cart';
-export * from './checkout';
-export * from './classic-cart';
-export * from './classic-checkout';
-export * from './classic-product';
-export * from './pay-by-link';
-export * from './product';
