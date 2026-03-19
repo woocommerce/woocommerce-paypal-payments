c3695e6e29f860debbb52307645616d4fd521bd7
Misha Utkin <m.utkin@inpsyde.com>
2026-03-19 08:36:09 +0100

    Split frontend-ui spec by preconditions

--- tests/qa-legacy-ui/tests/05-frontend-ui/frontend-ui-acdc.spec.ts
+++ tests/qa-legacy-ui/tests/05-frontend-ui/frontend-ui-acdc.spec.ts
@@ -9,53 +9,61 @@ import {
 	acdc,
 } from '../../resources';
 
-test.beforeAll( async ( { utils, customizer } ) => {
-	test.setTimeout( 4 * 60_000 );
-	await utils.configureStore( storeConfigClassic );
-	await utils.configurePcp( pcpConfigDefault );
-	await utils.advancedCardProcessing.setup( { enableGateway: true } );
-	await customizer.setWooCommerceTermsAndConditions( 'Shop' );
-	await utils.updatePcpPlugin();
-	await new Promise( ( resolve ) => setTimeout( resolve, 60_000 ) );
+test.beforeAll( async ( { utils } ) => {
+	await utils.resetEnvironment();
+	await utils.createStorageStates();
 } );
 
-test( 'PCP-2064 | Frontend UI - Pay by link - ACDC - Unchecked Terms and conditions @Critical', async ( {
-	wooCommerceUtils,
-	wooCommerceApi,
-	classicPayForOrder,
-} ) => {
-	const testOrder = {
-		...orders.default,
-		payment: acdc,
-	};
-	let order = await wooCommerceUtils.createApiOrder( testOrder );
+test.describe( () => {
 
-	await classicPayForOrder.visit( order.id, order.order_key );
-	await classicPayForOrder.ppui.acdcGateway().click();
-	await classicPayForOrder.ppui
-		.cardNumberInput()
-		.fill( testOrder.payment.card.card_number );
-	await classicPayForOrder.ppui.cardExpirationInput().click();
-	await classicPayForOrder.ppui.page.keyboard.type(
-		testOrder.payment.card.expiration_date
-	);
-	await classicPayForOrder.ppui
-		.cardCVVInput()
-		.fill( testOrder.payment.card.card_cvv );
-	await classicPayForOrder.payForOrderButton().click();
+	test.beforeAll( async ( { utils, customizer } ) => {
+		test.setTimeout( 5 * 60_000 );
+		await utils.setupStore();
+		await utils.configureStore( storeConfigClassic );
+		await utils.configurePcp( pcpConfigDefault );
+		await utils.advancedCardProcessing.setup( { enableGateway: true } );
+		await customizer.setWooCommerceTermsAndConditions( 'Shop' );
+		await utils.updatePcpPlugin();
+	} );
 
-	await classicPayForOrder.assertUrl( order.id, order.order_key );
-	await expect(
-		classicPayForOrder.page.getByText(
-			'Please read and accept the terms and conditions to proceed with your order.'
-		)
-	).toBeVisible();
+	test( 'PCP-2064 | Frontend UI - Pay by link - ACDC - Unchecked Terms and conditions @Critical', async ( {
+		wooCommerceUtils,
+		wooCommerceApi,
+		classicPayForOrder,
+	} ) => {
+		const testOrder = {
+			...orders.default,
+			payment: acdc,
+		};
+		let order = await wooCommerceUtils.createApiOrder( testOrder );
 
-	order = await wooCommerceApi.getOrder( order.id );
-	await expect( order.status ).toEqual( 'pending' );
-	await expect( order.transaction_id ).toHaveLength( 0 );
+		await classicPayForOrder.visit( order.id, order.order_key );
+		await classicPayForOrder.ppui.acdcGateway().click();
+		await classicPayForOrder.ppui
+			.cardNumberInput()
+			.fill( testOrder.payment.card.card_number );
+		await classicPayForOrder.ppui.cardExpirationInput().click();
+		await classicPayForOrder.ppui.page.keyboard.type(
+			testOrder.payment.card.expiration_date
+		);
+		await classicPayForOrder.ppui
+			.cardCVVInput()
+			.fill( testOrder.payment.card.card_cvv );
+		await classicPayForOrder.payForOrderButton().click();
+
+		await classicPayForOrder.assertUrl( order.id, order.order_key );
+		await expect(
+			classicPayForOrder.page.getByText(
+				'Please read and accept the terms and conditions to proceed with your order.'
+			)
+		).toBeVisible();
+
+		order = await wooCommerceApi.getOrder( order.id );
+		await expect( order.status ).toEqual( 'pending' );
+		await expect( order.transaction_id ).toHaveLength( 0 );
+	} );
+
+	test.afterAll( async ( { customizer } ) => {
+		await customizer.setWooCommerceTermsAndConditions( 'No page set' );
 } );
-
-test.afterAll( async ( { customizer } ) => {
-	await customizer.setWooCommerceTermsAndConditions( 'No page set' );
 } );
--- tests/qa-legacy-ui/tests/05-frontend-ui/frontend-ui-pay-later.spec.ts
+++ tests/qa-legacy-ui/tests/05-frontend-ui/frontend-ui-pay-later.spec.ts
@@ -8,51 +8,60 @@ import {
 	products,
 } from '../../resources';
 
-test.beforeAll( async ( { utils, payLater } ) => {
-	test.setTimeout( 4 * 60_000 );
-	await utils.configureStore( storeConfigClassic );
-	await utils.configurePcp( pcpConfigDefault );
-	await payLater.visit();
-	const enableGatewayCheckbox = payLater.enableGatewayCheckbox();
-	await expect(
-		enableGatewayCheckbox,
-		'Assert enable Gateway checkbox is visible.'
-	).toBeVisible();
-	await enableGatewayCheckbox.check();
-	await payLater.removeItemsFromSelectBox( 'Pay Later Button Locations', [
-		'Cart',
-		'Classic Cart',
-		'Express Checkout',
-		'Classic Checkout',
-		'Single Product',
-	] );
-	await payLater.saveChanges();
-	await utils.updatePcpPlugin();
-	await new Promise( ( resolve ) => setTimeout( resolve, 60_000 ) );
+test.beforeAll( async ( { utils } ) => {
+	await utils.resetEnvironment();
+	await utils.createStorageStates();
 } );
 
-test( 'PCP-2047 | Frontend UI - Classic cart - Pay Later - Disabled Pay Later button is NOT displayed @Critical', async ( {
-	utils,
-	classicCart,
-	classicCheckout,
-	checkout,
-	cart,
-	product,
-} ) => {
-	await utils.fillVisitorsCart( [ products.simple10 ] );
+test.describe( () => {
 
-	await product.visit( products.simple10.slug );
-	await product.ppui.assertPayLaterButtonVisibility( false );
+	test.beforeAll( async ( { utils, payLater } ) => {
+		test.setTimeout( 5 * 60_000 );
+		await utils.setupStore();
+		await utils.configureStore( storeConfigClassic );
+		await utils.configurePcp( pcpConfigDefault );
+		await payLater.visit();
+		const enableGatewayCheckbox = payLater.enableGatewayCheckbox();
+		await expect(
+			enableGatewayCheckbox,
+			'Assert enable Gateway checkbox is visible.'
+		).toBeVisible();
+		await enableGatewayCheckbox.check();
+		await payLater.removeItemsFromSelectBox( 'Pay Later Button Locations', [
+			'Cart',
+			'Classic Cart',
+			'Express Checkout',
+			'Classic Checkout',
+			'Single Product',
+		] );
+		await payLater.saveChanges();
+		await utils.updatePcpPlugin();
+		// await new Promise( ( resolve ) => setTimeout( resolve, 60_000 ) );
+	} );
 
-	await cart.visit();
-	await cart.ppui.assertPayLaterButtonVisibility( false );
+	test( 'PCP-2047 | Frontend UI - Classic cart - Pay Later - Disabled Pay Later button is NOT displayed @Critical', async ( {
+		utils,
+		classicCart,
+		classicCheckout,
+		checkout,
+		cart,
+		product,
+	} ) => {
+		await utils.fillVisitorsCart( [ products.simple10 ] );
 
-	await classicCart.visit();
-	await classicCart.ppui.assertPayLaterButtonVisibility( false );
+		await product.visit( products.simple10.slug );
+		await product.ppui.assertPayLaterButtonVisibility( false );
 
-	await checkout.visit();
-	await checkout.ppui.assertPayLaterButtonVisibility( false );
+		await cart.visit();
+		await cart.ppui.assertPayLaterButtonVisibility( false );
 
-	await classicCheckout.visit();
-	await classicCheckout.ppui.assertPayLaterButtonVisibility( false );
+		await classicCart.visit();
+		await classicCart.ppui.assertPayLaterButtonVisibility( false );
+
+		await checkout.visit();
+		await checkout.ppui.assertPayLaterButtonVisibility( false );
+
+		await classicCheckout.visit();
+		await classicCheckout.ppui.assertPayLaterButtonVisibility( false );
+	} );
 } );
