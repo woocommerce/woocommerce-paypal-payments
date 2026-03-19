7ecb5dae12f5f39af33a795f027ef4fb3f8fcc3c
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-19 16:17:25 +0000

    [BOT] Add build artifact from dev/PCP-6062-new-ui-on-fresh-site-after-payment-with-pui-orders-stuck-on-status-on-hold-as-webhook-are-not-registered -> PCP-6062-new-ui-on-fresh-site-after-payment-with-pui-orders-stuck-on-status-on-hold-as-webhook-are-not-registered.

--- modules/ppcp-api-client/src/Helper/DccApplies.php
+++ modules/ppcp-api-client/src/Helper/DccApplies.php
@@ -1,140 +1,128 @@
 <?php
+
 /**
  * The DCC Applies helper checks if the current installation can use DCC or not.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Helper
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Helper;
 
 /**
  * Class DccApplies
  */
-class DccApplies {
-
-	/**
-	 * The matrix which countries and currency combinations can be used for DCC.
-	 *
-	 * @var array
-	 */
-	private $allowed_country_currency_matrix;
-
-	/**
-	 * Which countries support which credit cards. Empty credit card arrays mean no restriction on
-	 * currency.
-	 *
-	 * @var array
-	 */
-	private $country_card_matrix;
-
-	/**
-	 * The getter of the 3-letter currency code of the shop.
-	 *
-	 * @var CurrencyGetter
-	 */
-	private CurrencyGetter $currency;
-
-	/**
-	 * 2-letter country code of the shop.
-	 *
-	 * @var string
-	 */
-	private $country;
-
-	/**
-	 * DccApplies constructor.
-	 *
-	 * @param array          $allowed_country_currency_matrix The matrix which countries and currency combinations can be used for DCC.
-	 * @param array          $country_card_matrix Which countries support which credit cards. Empty credit card arrays mean no restriction on
-	 *          currency.
-	 * @param CurrencyGetter $currency The getter of the 3-letter currency code of the shop.
-	 * @param string         $country 2-letter country code of the shop.
-	 */
-	public function __construct(
-		array $allowed_country_currency_matrix,
-		array $country_card_matrix,
-		CurrencyGetter $currency,
-		string $country
-	) {
-		$this->allowed_country_currency_matrix = $allowed_country_currency_matrix;
-		$this->country_card_matrix             = $country_card_matrix;
-		$this->currency                        = $currency;
-		$this->country                         = $country;
-	}
-
-	/**
-	 * Returns whether DCC can be used in the current country and the current currency used.
-	 *
-	 * @return bool
-	 */
-	public function for_country_currency(): bool {
-		if ( ! in_array( $this->country, array_keys( $this->allowed_country_currency_matrix ), true ) ) {
-			return false;
-		}
-		$applies = in_array( $this->currency->get(), $this->allowed_country_currency_matrix[ $this->country ], true );
-		return $applies;
-	}
-
-	/**
-	 * Returns whether WooCommerce Payments plugin is available for the store country.
-	 *
-	 * @return bool
-	 */
-	public function for_wc_payments(): bool {
-		$countries = array_keys( $this->allowed_country_currency_matrix );
-		array_push( $countries, 'AT', 'BE', 'HK', 'IE', 'NL', 'PL', 'PT', 'SG', 'CH' );
-
-		return in_array( $this->country, $countries, true );
-	}
-
-	/**
-	 * Returns credit cards, which can be used.
-	 *
-	 * @return array
-	 */
-	public function valid_cards(): array {
-		$cards = array();
-		if ( ! isset( $this->country_card_matrix[ $this->country ] ) ) {
-			return $cards;
-		}
-
-		$supported_currencies = $this->country_card_matrix[ $this->country ];
-		foreach ( $supported_currencies as $card => $currencies ) {
-			if ( $this->can_process_card( $card ) ) {
-				$cards[] = $card;
-			}
-		}
-		if ( in_array( 'amex', $cards, true ) ) {
-			$cards[] = 'american-express';
-		}
-		if ( in_array( 'mastercard', $cards, true ) ) {
-			$cards[] = 'master-card';
-		}
-		return $cards;
-	}
-
-	/**
-	 * Whether a card can be used or not.
-	 *
-	 * @param string $card The card.
-	 *
-	 * @return bool
-	 */
-	public function can_process_card( string $card ): bool {
-		if ( ! isset( $this->country_card_matrix[ $this->country ] ) ) {
-			return false;
-		}
-		if ( ! isset( $this->country_card_matrix[ $this->country ][ $card ] ) ) {
-			return false;
-		}
-
-		/**
-		 * If the supported currencies array is empty, there are no
-		 * restrictions, which currencies are supported by a card.
-		 */
-		$supported_currencies = $this->country_card_matrix[ $this->country ][ $card ];
-		return empty( $supported_currencies ) || in_array( $this->currency->get(), $supported_currencies, true );
-	}
+class DccApplies
+{
+    /**
+     * The matrix which countries and currency combinations can be used for DCC.
+     *
+     * @var array
+     */
+    private $allowed_country_currency_matrix;
+    /**
+     * Which countries support which credit cards. Empty credit card arrays mean no restriction on
+     * currency.
+     *
+     * @var array
+     */
+    private $country_card_matrix;
+    /**
+     * The getter of the 3-letter currency code of the shop.
+     *
+     * @var CurrencyGetter
+     */
+    private \WooCommerce\PayPalCommerce\ApiClient\Helper\CurrencyGetter $currency;
+    /**
+     * 2-letter country code of the shop.
+     *
+     * @var string
+     */
+    private $country;
+    /**
+     * DccApplies constructor.
+     *
+     * @param array          $allowed_country_currency_matrix The matrix which countries and currency combinations can be used for DCC.
+     * @param array          $country_card_matrix Which countries support which credit cards. Empty credit card arrays mean no restriction on
+     *          currency.
+     * @param CurrencyGetter $currency The getter of the 3-letter currency code of the shop.
+     * @param string         $country 2-letter country code of the shop.
+     */
+    public function __construct(array $allowed_country_currency_matrix, array $country_card_matrix, \WooCommerce\PayPalCommerce\ApiClient\Helper\CurrencyGetter $currency, string $country)
+    {
+        $this->allowed_country_currency_matrix = $allowed_country_currency_matrix;
+        $this->country_card_matrix = $country_card_matrix;
+        $this->currency = $currency;
+        $this->country = $country;
+    }
+    /**
+     * Returns whether DCC can be used in the current country and the current currency used.
+     *
+     * @return bool
+     */
+    public function for_country_currency(): bool
+    {
+        if (!in_array($this->country, array_keys($this->allowed_country_currency_matrix), \true)) {
+            return \false;
+        }
+        $applies = in_array($this->currency->get(), $this->allowed_country_currency_matrix[$this->country], \true);
+        return $applies;
+    }
+    /**
+     * Returns whether WooCommerce Payments plugin is available for the store country.
+     *
+     * @return bool
+     */
+    public function for_wc_payments(): bool
+    {
+        $countries = array_keys($this->allowed_country_currency_matrix);
+        array_push($countries, 'AT', 'BE', 'HK', 'IE', 'NL', 'PL', 'PT', 'SG', 'CH');
+        return in_array($this->country, $countries, \true);
+    }
+    /**
+     * Returns credit cards, which can be used.
+     *
+     * @return array
+     */
+    public function valid_cards(): array
+    {
+        $cards = array();
+        if (!isset($this->country_card_matrix[$this->country])) {
+            return $cards;
+        }
+        $supported_currencies = $this->country_card_matrix[$this->country];
+        foreach ($supported_currencies as $card => $currencies) {
+            if ($this->can_process_card($card)) {
+                $cards[] = $card;
+            }
+        }
+        if (in_array('amex', $cards, \true)) {
+            $cards[] = 'american-express';
+        }
+        if (in_array('mastercard', $cards, \true)) {
+            $cards[] = 'master-card';
+        }
+        return $cards;
+    }
+    /**
+     * Whether a card can be used or not.
+     *
+     * @param string $card The card.
+     *
+     * @return bool
+     */
+    public function can_process_card(string $card): bool
+    {
+        if (!isset($this->country_card_matrix[$this->country])) {
+            return \false;
+        }
+        if (!isset($this->country_card_matrix[$this->country][$card])) {
+            return \false;
+        }
+        /**
+         * If the supported currencies array is empty, there are no
+         * restrictions, which currencies are supported by a card.
+         */
+        $supported_currencies = $this->country_card_matrix[$this->country][$card];
+        return empty($supported_currencies) || in_array($this->currency->get(), $supported_currencies, \true);
+    }
 }
--- modules/ppcp-webhooks/src/WebhookModule.php
+++ modules/ppcp-webhooks/src/WebhookModule.php
@@ -1,12 +1,11 @@
 <?php
+
 /**
  * The webhook module.
  *
  * @package WooCommerce\PayPalCommerce\Webhooks
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Webhooks;
 
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ExecutableModule;
@@ -17,137 +16,96 @@ use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
 use WooCommerce\PayPalCommerce\Webhooks\Endpoint\ResubscribeEndpoint;
 use WooCommerce\PayPalCommerce\Webhooks\Endpoint\SimulateEndpoint;
 use WooCommerce\PayPalCommerce\Webhooks\Endpoint\SimulationStateEndpoint;
-
 /**
  * Class WebhookModule
  */
-class WebhookModule implements ServiceModule, FactoryModule, ExecutableModule {
-	use ModuleClassNameIdTrait;
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function services(): array {
-		return require __DIR__ . '/../services.php';
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function factories(): array {
-		return require __DIR__ . '/../factories.php';
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function run( ContainerInterface $container ): bool {
-
-		add_action(
-			'rest_api_init',
-			static function () use ( $container ) {
-				$endpoint = $container->get( 'webhook.endpoint.controller' );
-				/**
-				 * The Incoming Webhook Endpoint.
-				 *
-				 * @var IncomingWebhookEndpoint $endpoint
-				 */
-				$endpoint->register();
-			}
-		);
-
-		add_action(
-			WebhookRegistrar::EVENT_HOOK,
-			static function () use ( $container ) {
-				$registrar = $container->get( 'webhook.registrar' );
-				assert( $registrar instanceof WebhookRegistrar );
-				$registrar->register();
-			}
-		);
-
-		add_action(
-			'woocommerce_paypal_payments_gateway_deactivate',
-			static function () use ( $container ) {
-				$registrar = $container->get( 'webhook.registrar' );
-				assert( $registrar instanceof WebhookRegistrar );
-				$registrar->unregister();
-			}
-		);
-
-		add_action(
-			'admin_init',
-			static function () use ( $container ) {
-				$is_connected  = $container->get( 'settings.flag.is-connected' );
-				$is_registered = $container->get( 'webhook.is-registered' );
-				if ( ! $is_connected || $is_registered ) {
-					return;
-				}
-
-				$throttle_key = 'ppcp_webhook_auto_register_throttle';
-				if ( get_transient( $throttle_key ) ) {
-					return;
-				}
-				set_transient( $throttle_key, true, 5 * MINUTE_IN_SECONDS );
-
-				$registrar = $container->get( 'webhook.registrar' );
-				assert( $registrar instanceof WebhookRegistrar );
-				$registrar->register();
-			}
-		);
-
-		add_action(
-			'woocommerce_paypal_payments_gateway_migrate',
-			static function ( $installed_plugin_version ) use ( $container ) {
-				// Skip fresh installs.
-				if ( ! $installed_plugin_version ) {
-					return;
-				}
-
-				// Only resubscribe for PUI or OXXO merchants.
-				$pui_settings = get_option( 'woocommerce_ppcp-pay-upon-invoice-gateway_settings', array() );
-				$pui_enabled  = is_array( $pui_settings ) && ( $pui_settings['enabled'] ?? 'no' ) === 'yes';
-
-				$oxxo_settings = get_option( 'woocommerce_ppcp-oxxo-gateway_settings', array() );
-				$oxxo_enabled  = is_array( $oxxo_settings ) && ( $oxxo_settings['enabled'] ?? 'no' ) === 'yes';
-
-				if ( ! $pui_enabled && ! $oxxo_enabled ) {
-					return;
-				}
-
-				// Clear stored webhook to force re-registration via self-healing.
-				delete_option( WebhookRegistrar::KEY );
-			}
-		);
-
-		add_action(
-			'wc_ajax_' . ResubscribeEndpoint::ENDPOINT,
-			static function () use ( $container ) {
-				$endpoint = $container->get( 'webhook.endpoint.resubscribe' );
-				assert( $endpoint instanceof ResubscribeEndpoint );
-
-				$endpoint->handle_request();
-			}
-		);
-
-		add_action(
-			'wc_ajax_' . SimulateEndpoint::ENDPOINT,
-			static function () use ( $container ) {
-				$endpoint = $container->get( 'webhook.endpoint.simulate' );
-				assert( $endpoint instanceof SimulateEndpoint );
-
-				$endpoint->handle_request();
-			}
-		);
-		add_action(
-			'wc_ajax_' . SimulationStateEndpoint::ENDPOINT,
-			static function () use ( $container ) {
-				$endpoint = $container->get( 'webhook.endpoint.simulation-state' );
-				assert( $endpoint instanceof SimulationStateEndpoint );
-
-				$endpoint->handle_request();
-			}
-		);
-
-		return true;
-	}
+class WebhookModule implements ServiceModule, FactoryModule, ExecutableModule
+{
+    use ModuleClassNameIdTrait;
+    /**
+     * {@inheritDoc}
+     */
+    public function services(): array
+    {
+        return require __DIR__ . '/../services.php';
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function factories(): array
+    {
+        return require __DIR__ . '/../factories.php';
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function run(ContainerInterface $container): bool
+    {
+        add_action('rest_api_init', static function () use ($container) {
+            $endpoint = $container->get('webhook.endpoint.controller');
+            /**
+             * The Incoming Webhook Endpoint.
+             *
+             * @var IncomingWebhookEndpoint $endpoint
+             */
+            $endpoint->register();
+        });
+        add_action(\WooCommerce\PayPalCommerce\Webhooks\WebhookRegistrar::EVENT_HOOK, static function () use ($container) {
+            $registrar = $container->get('webhook.registrar');
+            assert($registrar instanceof \WooCommerce\PayPalCommerce\Webhooks\WebhookRegistrar);
+            $registrar->register();
+        });
+        add_action('woocommerce_paypal_payments_gateway_deactivate', static function () use ($container) {
+            $registrar = $container->get('webhook.registrar');
+            assert($registrar instanceof \WooCommerce\PayPalCommerce\Webhooks\WebhookRegistrar);
+            $registrar->unregister();
+        });
+        add_action('admin_init', static function () use ($container) {
+            $is_connected = $container->get('settings.flag.is-connected');
+            $is_registered = $container->get('webhook.is-registered');
+            if (!$is_connected || $is_registered) {
+                return;
+            }
+            $throttle_key = 'ppcp_webhook_auto_register_throttle';
+            if (get_transient($throttle_key)) {
+                return;
+            }
+            set_transient($throttle_key, \true, 5 * MINUTE_IN_SECONDS);
+            $registrar = $container->get('webhook.registrar');
+            assert($registrar instanceof \WooCommerce\PayPalCommerce\Webhooks\WebhookRegistrar);
+            $registrar->register();
+        });
+        add_action('woocommerce_paypal_payments_gateway_migrate', static function ($installed_plugin_version) use ($container) {
+            // Skip fresh installs.
+            if (!$installed_plugin_version) {
+                return;
+            }
+            // Only resubscribe for PUI or OXXO merchants.
+            $pui_settings = get_option('woocommerce_ppcp-pay-upon-invoice-gateway_settings', array());
+            $pui_enabled = is_array($pui_settings) && ($pui_settings['enabled'] ?? 'no') === 'yes';
+            $oxxo_settings = get_option('woocommerce_ppcp-oxxo-gateway_settings', array());
+            $oxxo_enabled = is_array($oxxo_settings) && ($oxxo_settings['enabled'] ?? 'no') === 'yes';
+            if (!$pui_enabled && !$oxxo_enabled) {
+                return;
+            }
+            // Clear stored webhook to force re-registration via self-healing.
+            delete_option(\WooCommerce\PayPalCommerce\Webhooks\WebhookRegistrar::KEY);
+        });
+        add_action('wc_ajax_' . ResubscribeEndpoint::ENDPOINT, static function () use ($container) {
+            $endpoint = $container->get('webhook.endpoint.resubscribe');
+            assert($endpoint instanceof ResubscribeEndpoint);
+            $endpoint->handle_request();
+        });
+        add_action('wc_ajax_' . SimulateEndpoint::ENDPOINT, static function () use ($container) {
+            $endpoint = $container->get('webhook.endpoint.simulate');
+            assert($endpoint instanceof SimulateEndpoint);
+            $endpoint->handle_request();
+        });
+        add_action('wc_ajax_' . SimulationStateEndpoint::ENDPOINT, static function () use ($container) {
+            $endpoint = $container->get('webhook.endpoint.simulation-state');
+            assert($endpoint instanceof SimulationStateEndpoint);
+            $endpoint->handle_request();
+        });
+        return \true;
+    }
 }
