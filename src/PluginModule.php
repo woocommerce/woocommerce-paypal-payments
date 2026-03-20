550d77e9942ad39af6331ec80b1fa1cec25497c0
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-20 15:21:49 +0000

    [BOT] Add build artifact from dev/PCP-6067-remove-the-woocommerce-ppcp-is-new-merchant-flag -> PCP-6067-remove-the-woocommerce-ppcp-is-new-merchant-flag.

--- src/PluginModule.php
+++ src/PluginModule.php
@@ -1,36 +1,35 @@
 <?php
+
 /**
  * The plugin module.
  *
  * @package WooCommerce\PayPalCommerce
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce;
 
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ExecutableModule;
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ModuleClassNameIdTrait;
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ServiceModule;
 use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
-
 /**
  * Class PluginModule
  */
-class PluginModule implements ServiceModule, ExecutableModule {
-	use ModuleClassNameIdTrait;
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function services(): array {
-		return require __DIR__ . '/services.php';
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function run( ContainerInterface $c ): bool {
-		return true;
-	}
+class PluginModule implements ServiceModule, ExecutableModule
+{
+    use ModuleClassNameIdTrait;
+    /**
+     * {@inheritDoc}
+     */
+    public function services(): array
+    {
+        return require __DIR__ . '/services.php';
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function run(ContainerInterface $c): bool
+    {
+        return \true;
+    }
 }
