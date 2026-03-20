63439fee8db7fc42ea0a64c3fc5a2902b7c04105
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-20 11:10:34 +0000

    [BOT] Add build artifact from dev/PCP-6068-migration-pay-pal-title-and-description-not-saved-after-migration -> PCP-6068-migration-pay-pal-title-and-description-not-saved-after-migration.

--- src/Http/WpRedirector.php
+++ src/Http/WpRedirector.php
@@ -1,25 +1,26 @@
 <?php
+
 /**
  * HTTP redirection.
  *
  * @package WooCommerce\PayPalCommerce\Api
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Http;
 
 /**
  * Wrapper for HTTP redirection via wp_safe_redirect.
  */
-class WpRedirector implements RedirectorInterface {
-	/**
-	 * Starts HTTP redirection and shutdowns.
-	 *
-	 * @param string $location The URL to redirect to.
-	 */
-	public function redirect( string $location ): void {
-		wp_safe_redirect( $location, 302 );
-		exit;
-	}
+class WpRedirector implements \WooCommerce\PayPalCommerce\Http\RedirectorInterface
+{
+    /**
+     * Starts HTTP redirection and shutdowns.
+     *
+     * @param string $location The URL to redirect to.
+     */
+    public function redirect(string $location): void
+    {
+        wp_safe_redirect($location, 302);
+        exit;
+    }
 }
