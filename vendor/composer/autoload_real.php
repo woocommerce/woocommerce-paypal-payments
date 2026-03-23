5f66f4d3a72d84962ee30bb69b976e0a12a8b346
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-23 13:48:13 +0000

    [BOT] Add build artifact from dev/fix-broken-migration-checkout-reload -> fix-broken-migration-checkout-reload.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInitf454a4dc4519aa3bafab294be971ae9b
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf454a4dc4519aa3bafab294be971ae9b::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_abab9be499072dcca1de66476d85ad592ca25bb5'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_abab9be499072dcca1de66476d85ad592ca25bb5'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_be04496e0d28c0811e8661088971ae0d41f084fb'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_be04496e0d28c0811e8661088971ae0d41f084fb'][$fileIdentifier] = true;
 
                 require $file;
             }
