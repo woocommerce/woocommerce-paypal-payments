6e05330624e3ad291c676661f71a157e3c003348
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-23 13:24:44 +0000

    [BOT] Add build artifact from dev/fix-broken-migration-checkout-reload -> fix-broken-migration-checkout-reload.

--- modules/ppcp-admin-notices/services.php
+++ modules/ppcp-admin-notices/services.php
@@ -1,12 +1,11 @@
 <?php
+
 /**
  * The services of the admin notice module.
  *
  * @package WooCommerce\PayPalCommerce\AdminNotices
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\AdminNotices;
 
 use WooCommerce\PayPalCommerce\Assets\AssetGetter;
@@ -17,27 +16,14 @@ use WooCommerce\PayPalCommerce\AdminNotices\Renderer\RendererInterface;
 use WooCommerce\PayPalCommerce\AdminNotices\Repository\Repository;
 use WooCommerce\PayPalCommerce\AdminNotices\Repository\RepositoryInterface;
 use WooCommerce\PayPalCommerce\AdminNotices\Endpoint\MuteMessageEndpoint;
-
-return array(
-	'admin-notices.asset_getter'          => static function ( ContainerInterface $container ): AssetGetter {
-		$factory = $container->get( 'assets.asset_getter_factory' );
-		assert( $factory instanceof AssetGetterFactory );
-
-		return $factory->for_module( 'ppcp-admin-notices' );
-	},
-	'admin-notices.renderer'              => static function ( ContainerInterface $container ): RendererInterface {
-		return new Renderer(
-			$container->get( 'admin-notices.repository' ),
-			$container->get( 'admin-notices.asset_getter' ),
-			$container->get( 'ppcp.asset-version' )
-		);
-	},
-	'admin-notices.repository'            => static function ( ContainerInterface $container ): RepositoryInterface {
-		return new Repository();
-	},
-	'admin-notices.mute-message-endpoint' => static function ( ContainerInterface $container ): MuteMessageEndpoint {
-		return new MuteMessageEndpoint(
-			$container->get( 'button.request-data' )
-		);
-	},
-);
+return array('admin-notices.asset_getter' => static function (ContainerInterface $container): AssetGetter {
+    $factory = $container->get('assets.asset_getter_factory');
+    assert($factory instanceof AssetGetterFactory);
+    return $factory->for_module('ppcp-admin-notices');
+}, 'admin-notices.renderer' => static function (ContainerInterface $container): RendererInterface {
+    return new Renderer($container->get('admin-notices.repository'), $container->get('admin-notices.asset_getter'), $container->get('ppcp.asset-version'));
+}, 'admin-notices.repository' => static function (ContainerInterface $container): RepositoryInterface {
+    return new Repository();
+}, 'admin-notices.mute-message-endpoint' => static function (ContainerInterface $container): MuteMessageEndpoint {
+    return new MuteMessageEndpoint($container->get('button.request-data'));
+});
