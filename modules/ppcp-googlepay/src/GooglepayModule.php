99dd62e8924b9d55905368a16c2d3a2524fea1af
Narek Zakarian <narekzakarian@gmail.com>
2026-03-26 18:18:00 +0400

    fix: Skip Google Pay gateway registration when disabled for current location
    Added a context-aware check to the woocommerce_payment_gateways filter
    using button_styling() methods, so the Google Pay gateway is not registered
    as a WC payment method when it is disabled for the current page in the
    styling settings.

--- modules/ppcp-googlepay/src/GooglepayModule.php
+++ modules/ppcp-googlepay/src/GooglepayModule.php
@@ -14,6 +14,7 @@ use WC_Payment_Gateway;
 use WooCommerce\PayPalCommerce\ApiClient\Factory\ExperienceContextBuilder;
 use WooCommerce\PayPalCommerce\Button\Assets\ButtonInterface;
 use WooCommerce\PayPalCommerce\Button\Assets\SmartButtonInterface;
+use WooCommerce\PayPalCommerce\Button\Helper\Context;
 use WooCommerce\PayPalCommerce\Googlepay\Endpoint\UpdatePaymentDataEndpoint;
 use WooCommerce\PayPalCommerce\Googlepay\Helper\GoogleProductStatus;
 use WooCommerce\PayPalCommerce\Googlepay\Helper\AvailabilityNotice;
@@ -187,12 +188,23 @@ class GooglepayModule implements ServiceModule, ExecutableModule {
 				$settings = $c->get( 'settings.settings-provider' );
 				assert( $settings instanceof SettingsProvider );
 
-				if ( $settings->googlepay_enabled() ) {
-					$googlepay_gateway = $c->get( 'googlepay.wc-gateway' );
-					assert( $googlepay_gateway instanceof WC_Payment_Gateway );
-
-					$methods[] = $googlepay_gateway;
+				if ( ! $settings->googlepay_enabled() ) {
+					return $methods;
 				}
+
+				$context = $c->get( 'button.helper.context' );
+				assert( $context instanceof Context );
+
+				$page_methods = $settings->button_styling( $context->context() )->methods;
+
+				if ( ! in_array( 'ppcp-googlepay', $page_methods, true ) ) {
+					return $methods;
+				}
+
+				$googlepay_gateway = $c->get( 'googlepay.wc-gateway' );
+				assert( $googlepay_gateway instanceof WC_Payment_Gateway );
+
+				$methods[] = $googlepay_gateway;
 
 				return $methods;
 			}
