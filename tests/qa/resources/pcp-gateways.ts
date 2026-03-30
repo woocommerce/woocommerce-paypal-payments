3a3d4f98f65b3a2e7c15db1979f4d3765fe93185
Misha Utkin <m.utkin@inpsyde.com>
2026-03-30 15:52:06 +0200

    Fix ACDC title for CI

--- tests/qa/resources/pcp-gateways.ts
+++ tests/qa/resources/pcp-gateways.ts
@@ -55,7 +55,7 @@ const acdc: Pcp.Gateway = {
 	description: '',
 	title: 'Debit & Credit Cards',
 	titleInPcpSettings: 'Advanced Credit and Debit Card Payments',
-	titleInWcSettings: 'Debit & Credit Cards',
+	titleInWcSettings: 'Advanced Card Processing',
 	hasSettingsButton: true,
 	enabled: false,
 
