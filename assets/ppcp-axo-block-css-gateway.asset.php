a535fe816b708f3471bc650e376b8248c8781fa5
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-30 14:24:48 +0000

    [BOT] Add build artifact from dev/fix/locations-after-upgrading -> fix/locations-after-upgrading.

---
+++ assets/ppcp-axo-block-css-gateway.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '4d63ebe9c544ee9a1b09');
+}
---
+++ assets/ppcp-axo-block-js-index.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'caa3406cbd867379806c');
+}
