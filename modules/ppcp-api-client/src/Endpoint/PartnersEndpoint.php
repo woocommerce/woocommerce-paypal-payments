3022ad5af56aef559802dd3f72d13125f9201c93
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-03-31 07:54:06 +0000

    [BOT] Add build artifact from dev/PCP-6096-no-migration-for-new-merchant -> PCP-6096-no-migration-for-new-merchant.

--- modules/ppcp-api-client/src/Endpoint/PartnersEndpoint.php
+++ modules/ppcp-api-client/src/Endpoint/PartnersEndpoint.php
@@ -1,166 +1,117 @@
 <?php
+
 /**
  * The Partners Endpoint.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Endpoint
  */
-
-declare( strict_types=1 );
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Endpoint;
 
-use Psr\Log\LoggerInterface;
+use WooCommerce\PayPalCommerce\Vendor\Psr\Log\LoggerInterface;
 use WooCommerce\PayPalCommerce\ApiClient\Authentication\Bearer;
 use WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatus;
 use WooCommerce\PayPalCommerce\ApiClient\Exception\PayPalApiException;
 use WooCommerce\PayPalCommerce\ApiClient\Exception\RuntimeException;
 use WooCommerce\PayPalCommerce\ApiClient\Factory\SellerStatusFactory;
 use WooCommerce\PayPalCommerce\ApiClient\Helper\FailureRegistry;
-
 /**
  * Class PartnersEndpoint
  */
-class PartnersEndpoint {
-
-	use RequestTrait;
-
-	/**
-	 * The Host URL.
-	 *
-	 * @var string
-	 */
-	private $host;
-
-	/**
-	 * The bearer.
-	 *
-	 * @var Bearer
-	 */
-	private $bearer;
-
-	/**
-	 * The logger.
-	 *
-	 * @var LoggerInterface
-	 */
-	private $logger;
-
-	/**
-	 * The seller status factory.
-	 *
-	 * @var SellerStatusFactory
-	 */
-	private $seller_status_factory;
-
-	/**
-	 * The partner ID.
-	 *
-	 * @var string
-	 */
-	private $partner_id;
-
-	/**
-	 * The merchant ID.
-	 *
-	 * @var string
-	 */
-	private $merchant_id;
-
-	/**
-	 * The failure registry.
-	 *
-	 * @var FailureRegistry
-	 */
-	private $failure_registry;
-
-	/**
-	 * PartnersEndpoint constructor.
-	 *
-	 * @param string              $host The host.
-	 * @param Bearer              $bearer The bearer.
-	 * @param LoggerInterface     $logger The logger.
-	 * @param SellerStatusFactory $seller_status_factory The seller status factory.
-	 * @param string              $partner_id The partner ID.
-	 * @param string              $merchant_id The merchant ID.
-	 * @param FailureRegistry     $failure_registry The API failure registry.
-	 */
-	public function __construct(
-		string $host,
-		Bearer $bearer,
-		LoggerInterface $logger,
-		SellerStatusFactory $seller_status_factory,
-		string $partner_id,
-		string $merchant_id,
-		FailureRegistry $failure_registry
-	) {
-		$this->host                  = $host;
-		$this->bearer                = $bearer;
-		$this->logger                = $logger;
-		$this->seller_status_factory = $seller_status_factory;
-		$this->partner_id            = $partner_id;
-		$this->merchant_id           = $merchant_id;
-		$this->failure_registry      = $failure_registry;
-	}
-
-	/**
-	 * Returns the current seller status.
-	 *
-	 * @return SellerStatus
-	 * @throws RuntimeException When request could not be fulfilled.
-	 */
-	public function seller_status(): SellerStatus {
-		$url      = trailingslashit( $this->host ) . 'v1/customer/partners/' . $this->partner_id . '/merchant-integrations/' . $this->merchant_id;
-		$bearer   = $this->bearer->bearer();
-		$args     = array(
-			'method'  => 'GET',
-			'headers' => array(
-				'Authorization' => 'Bearer ' . $bearer->token(),
-				'Content-Type'  => 'application/json',
-			),
-		);
-		$response = $this->request( $url, $args );
-		if ( is_wp_error( $response ) ) {
-
-			$error = new RuntimeException(
-				__(
-					'Could not fetch sellers status.',
-					'woocommerce-paypal-payments'
-				)
-			);
-
-			$this->logger->log(
-				'warning',
-				$error->getMessage(),
-				array(
-					'args'     => $args,
-					'response' => $response,
-				)
-			);
-			throw $error;
-		}
-
-		$json        = json_decode( wp_remote_retrieve_body( $response ) );
-		$status_code = (int) wp_remote_retrieve_response_code( $response );
-		if ( 200 !== $status_code ) {
-			$error = new PayPalApiException( $json, $status_code );
-			$this->logger->log(
-				'warning',
-				$error->getMessage(),
-				array(
-					'args'     => $args,
-					'response' => $response,
-				)
-			);
-
-			// Register the failure on api failure registry.
-			$this->failure_registry->add_failure( FailureRegistry::SELLER_STATUS_KEY );
-
-			throw $error;
-		}
-
-		$this->failure_registry->clear_failures( FailureRegistry::SELLER_STATUS_KEY );
-
-		$status = $this->seller_status_factory->from_paypal_response( $json );
-		return $status;
-	}
+class PartnersEndpoint
+{
+    use \WooCommerce\PayPalCommerce\ApiClient\Endpoint\RequestTrait;
+    /**
+     * The Host URL.
+     *
+     * @var string
+     */
+    private $host;
+    /**
+     * The bearer.
+     *
+     * @var Bearer
+     */
+    private $bearer;
+    /**
+     * The logger.
+     *
+     * @var LoggerInterface
+     */
+    private $logger;
+    /**
+     * The seller status factory.
+     *
+     * @var SellerStatusFactory
+     */
+    private $seller_status_factory;
+    /**
+     * The partner ID.
+     *
+     * @var string
+     */
+    private $partner_id;
+    /**
+     * The merchant ID.
+     *
+     * @var string
+     */
+    private $merchant_id;
+    /**
+     * The failure registry.
+     *
+     * @var FailureRegistry
+     */
+    private $failure_registry;
+    /**
+     * PartnersEndpoint constructor.
+     *
+     * @param string              $host The host.
+     * @param Bearer              $bearer The bearer.
+     * @param LoggerInterface     $logger The logger.
+     * @param SellerStatusFactory $seller_status_factory The seller status factory.
+     * @param string              $partner_id The partner ID.
+     * @param string              $merchant_id The merchant ID.
+     * @param FailureRegistry     $failure_registry The API failure registry.
+     */
+    public function __construct(string $host, Bearer $bearer, LoggerInterface $logger, SellerStatusFactory $seller_status_factory, string $partner_id, string $merchant_id, FailureRegistry $failure_registry)
+    {
+        $this->host = $host;
+        $this->bearer = $bearer;
+        $this->logger = $logger;
+        $this->seller_status_factory = $seller_status_factory;
+        $this->partner_id = $partner_id;
+        $this->merchant_id = $merchant_id;
+        $this->failure_registry = $failure_registry;
+    }
+    /**
+     * Returns the current seller status.
+     *
+     * @return SellerStatus
+     * @throws RuntimeException When request could not be fulfilled.
+     */
+    public function seller_status(): SellerStatus
+    {
+        $url = trailingslashit($this->host) . 'v1/customer/partners/' . $this->partner_id . '/merchant-integrations/' . $this->merchant_id;
+        $bearer = $this->bearer->bearer();
+        $args = array('method' => 'GET', 'headers' => array('Authorization' => 'Bearer ' . $bearer->token(), 'Content-Type' => 'application/json'));
+        $response = $this->request($url, $args);
+        if (is_wp_error($response)) {
+            $error = new RuntimeException(__('Could not fetch sellers status.', 'woocommerce-paypal-payments'));
+            $this->logger->log('warning', $error->getMessage(), array('args' => $args, 'response' => $response));
+            throw $error;
+        }
+        $json = json_decode(wp_remote_retrieve_body($response));
+        $status_code = (int) wp_remote_retrieve_response_code($response);
+        if (200 !== $status_code) {
+            $error = new PayPalApiException($json, $status_code);
+            $this->logger->log('warning', $error->getMessage(), array('args' => $args, 'response' => $response));
+            // Register the failure on api failure registry.
+            $this->failure_registry->add_failure(FailureRegistry::SELLER_STATUS_KEY);
+            throw $error;
+        }
+        $this->failure_registry->clear_failures(FailureRegistry::SELLER_STATUS_KEY);
+        $status = $this->seller_status_factory->from_paypal_response($json);
+        return $status;
+    }
 }
--- modules/ppcp-applepay/src/Assets/BlocksPaymentMethod.php
+++ modules/ppcp-applepay/src/Assets/BlocksPaymentMethod.php
@@ -1,12 +1,11 @@
 <?php
+
 /**
  * The applepay blocks module.
  *
  * @package WooCommerce\PayPalCommerce\Applepay
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Applepay\Assets;
 
 use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType;
@@ -16,74 +15,58 @@ use WooCommerce\PayPalCommerce\Assets\AssetGetter;
 use WooCommerce\PayPalCommerce\Button\Assets\ButtonInterface;
 use WooCommerce\PayPalCommerce\Button\Helper\Context;
 use WooCommerce\PayPalCommerce\Settings\Data\SettingsProvider;
-
-class BlocksPaymentMethod extends AbstractPaymentMethodType {
-	private AssetGetter $asset_getter;
-	private string $version;
-	private ButtonInterface $button;
-	private PaymentMethodTypeInterface $paypal_payment_method;
-	private Context $context;
-	private SettingsProvider $settings_provider;
-
-	public function __construct(
-		string $name,
-		AssetGetter $asset_getter,
-		string $version,
-		ButtonInterface $button,
-		PaymentMethodTypeInterface $paypal_payment_method,
-		Context $context,
-		SettingsProvider $settings_provider
-	) {
-		$this->name                  = $name;
-		$this->asset_getter          = $asset_getter;
-		$this->version               = $version;
-		$this->button                = $button;
-		$this->paypal_payment_method = $paypal_payment_method;
-		$this->context               = $context;
-		$this->settings_provider     = $settings_provider;
-	}
-
-	public function initialize() {  }
-
-	public function is_active(): bool {
-		$methods = $this->settings_provider->button_styling( $this->context->context() )->methods;
-
-		if ( ! in_array( ApplePayGateway::ID, $methods, true ) ) {
-			return false;
-		}
-
-		return $this->paypal_payment_method->is_active();
-	}
-
-	public function get_payment_method_script_handles() {
-		$handle = $this->name . '-block';
-
-		wp_register_script(
-			$handle,
-			$this->asset_getter->get_asset_url( 'boot-block.js' ),
-			array(),
-			$this->version,
-			true
-		);
-
-		return array( $handle );
-	}
-
-	public function get_payment_method_data() {
-		$paypal_data = $this->paypal_payment_method->get_payment_method_data();
-
-		if ( is_admin() ) {
-			$script_data = $this->button->script_data_for_admin(); // @phpstan-ignore method.notFound
-		} else {
-			$script_data = $this->button->script_data();
-		}
-
-		return array(
-			'id'          => $this->name,
-			'title'       => $paypal_data['title'], // TODO : see if we should use another.
-			'description' => $paypal_data['description'], // TODO : see if we should use another.
-			'enabled'     => $this->is_active(),
-			'scriptData'  => $script_data,
-		);
-	}
+class BlocksPaymentMethod extends AbstractPaymentMethodType
+{
+    private AssetGetter $asset_getter;
+    private string $version;
+    private ButtonInterface $button;
+    private PaymentMethodTypeInterface $paypal_payment_method;
+    private Context $context;
+    private SettingsProvider $settings_provider;
+    public function __construct(string $name, AssetGetter $asset_getter, string $version, ButtonInterface $button, PaymentMethodTypeInterface $paypal_payment_method, Context $context, SettingsProvider $settings_provider)
+    {
+        $this->name = $name;
+        $this->asset_getter = $asset_getter;
+        $this->version = $version;
+        $this->button = $button;
+        $this->paypal_payment_method = $paypal_payment_method;
+        $this->context = $context;
+        $this->settings_provider = $settings_provider;
+    }
+    public function initialize()
+    {
+    }
+    public function is_active(): bool
+    {
+        $methods = $this->settings_provider->button_styling($this->context->context())->methods;
+        if (!in_array(ApplePayGateway::ID, $methods, \true)) {
+            return \false;
+        }
+        return $this->paypal_payment_method->is_active();
+    }
+    public function get_payment_method_script_handles()
+    {
+        $handle = $this->name . '-block';
+        wp_register_script($handle, $this->asset_getter->get_asset_url('boot-block.js'), array(), $this->version, \true);
+        return array($handle);
+    }
+    public function get_payment_method_data()
+    {
+        $paypal_data = $this->paypal_payment_method->get_payment_method_data();
+        if (is_admin()) {
+            $script_data = $this->button->script_data_for_admin();
+            // @phpstan-ignore method.notFound
+        } else {
+            $script_data = $this->button->script_data();
+        }
+        return array(
+            'id' => $this->name,
+            'title' => $paypal_data['title'],
+            // TODO : see if we should use another.
+            'description' => $paypal_data['description'],
+            // TODO : see if we should use another.
+            'enabled' => $this->is_active(),
+            'scriptData' => $script_data,
+        );
+    }
 }
