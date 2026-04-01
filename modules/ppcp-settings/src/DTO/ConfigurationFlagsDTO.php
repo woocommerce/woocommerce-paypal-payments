da1caab690062ed7e25826aee7357b67f30e6366
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-04-01 13:39:45 +0000

    [BOT] Add build artifact from dev/PCP-6099-add-bn-code-to-confirm-payment-source-requests -> PCP-6099-add-bn-code-to-confirm-payment-source-requests.

--- modules/ppcp-settings/src/DTO/ConfigurationFlagsDTO.php
+++ modules/ppcp-settings/src/DTO/ConfigurationFlagsDTO.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
  * Data transfer object. Stores flags that are relevant for the default plugin
  * configuration at the end of the onboarding process.
  *
  * @package WooCommerce\PayPalCommerce\Settings\DTO;
  */
-
-declare( strict_types = 1 );
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Settings\DTO;
 
 /**
@@ -15,40 +14,37 @@ namespace WooCommerce\PayPalCommerce\Settings\DTO;
  *
  * Intentionally has no internal logic, sanitation or validation.
  */
-class ConfigurationFlagsDTO {
-	/**
-	 * The merchant's country, which indicates availability of certain features.
-	 *
-	 * @var string
-	 */
-	public string $country_code = '';
-
-	/**
-	 * Whether we configure a business account.
-	 * If false, a personal (casual seller) account is set up.
-	 *
-	 * @var bool
-	 */
-	public bool $is_business_seller = false;
-
-	/**
-	 * Whether credit card payments should be handled by our plugin.
-	 *
-	 * @var bool
-	 */
-	public bool $use_card_payments = false;
-
-	/**
-	 * Whether digital wallets (Apple Pay/Google Pay) should be enabled.
-	 *
-	 * @var bool
-	 */
-	public bool $use_digital_wallets = false;
-
-	/**
-	 * If the shop needs to process subscription payments.
-	 *
-	 * @var bool
-	 */
-	public bool $use_subscriptions = false;
+class ConfigurationFlagsDTO
+{
+    /**
+     * The merchant's country, which indicates availability of certain features.
+     *
+     * @var string
+     */
+    public string $country_code = '';
+    /**
+     * Whether we configure a business account.
+     * If false, a personal (casual seller) account is set up.
+     *
+     * @var bool
+     */
+    public bool $is_business_seller = \false;
+    /**
+     * Whether credit card payments should be handled by our plugin.
+     *
+     * @var bool
+     */
+    public bool $use_card_payments = \false;
+    /**
+     * Whether digital wallets (Apple Pay/Google Pay) should be enabled.
+     *
+     * @var bool
+     */
+    public bool $use_digital_wallets = \false;
+    /**
+     * If the shop needs to process subscription payments.
+     *
+     * @var bool
+     */
+    public bool $use_subscriptions = \false;
 }
