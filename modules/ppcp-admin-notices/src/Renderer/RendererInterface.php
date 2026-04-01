ea7fd25c844b51eb99099bf22a179d3263404aae
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-04-01 13:08:09 +0000

    [BOT] Add build artifact from dev/release/4.0.2 -> release/4.0.2.

--- modules/ppcp-admin-notices/src/Renderer/RendererInterface.php
+++ modules/ppcp-admin-notices/src/Renderer/RendererInterface.php
@@ -1,30 +1,28 @@
 <?php
+
 /**
  * The renderer interface.
  *
  * @package WooCommerce\PayPalCommerce\AdminNotices\Renderer
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\AdminNotices\Renderer;
 
 /**
  * Interface RendererInterface
  */
-interface RendererInterface {
-
-	/**
-	 * Renders the messages.
-	 *
-	 * @return bool
-	 */
-	public function render(): bool;
-
-	/**
-	 * Enqueues common assets required for the admin notice behavior.
-	 *
-	 * @return void
-	 */
-	public function enqueue_admin(): void;
+interface RendererInterface
+{
+    /**
+     * Renders the messages.
+     *
+     * @return bool
+     */
+    public function render(): bool;
+    /**
+     * Enqueues common assets required for the admin notice behavior.
+     *
+     * @return void
+     */
+    public function enqueue_admin(): void;
 }
