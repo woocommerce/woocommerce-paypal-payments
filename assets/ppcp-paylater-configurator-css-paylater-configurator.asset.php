1c1eb82e5504abc8a22e59e01720afe72d0302d2
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-04-01 12:32:00 +0000

    [BOT] Add build artifact from dev/PCP-6076-restore-firefox-onboarding-notice -> PCP-6076-restore-firefox-onboarding-notice.

---
+++ assets/ppcp-paylater-configurator-css-paylater-configurator.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => 'dc254801bdc6bb05d7fe');
+}
--- modules/ppcp-wc-subscriptions/module.php
+++ modules/ppcp-wc-subscriptions/module.php
@@ -1,14 +1,13 @@
 <?php
+
 /**
  * The module.
  *
  * @package WooCommerce\PayPalCommerce\WcSubscriptions
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\WcSubscriptions;
 
-return static function (): WcSubscriptionsModule {
-	return new WcSubscriptionsModule();
+return static function (): \WooCommerce\PayPalCommerce\WcSubscriptions\WcSubscriptionsModule {
+    return new \WooCommerce\PayPalCommerce\WcSubscriptions\WcSubscriptionsModule();
 };
