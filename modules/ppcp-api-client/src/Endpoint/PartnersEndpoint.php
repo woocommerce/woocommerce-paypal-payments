9ce4c6bff1332d8aa6f8799bda8a7e9ab35396de
Philipp Stracker <p.stracker@syde.com>
2026-04-01 16:04:43 +0200

    ♻️ Add type hints and explicit visibility

--- modules/ppcp-api-client/src/Endpoint/PartnersEndpoint.php
+++ modules/ppcp-api-client/src/Endpoint/PartnersEndpoint.php
@@ -79,17 +79,17 @@ class PartnersEndpoint {
 	 *
 	 * @var Cache
 	 */
-	private $cache;
+	private Cache $cache;
 
 	/**
 	 * Cache lifetime for seller status responses, in seconds.
 	 */
-	const SELLER_STATUS_CACHE_TTL = 600; // 10 minutes.
+	public const SELLER_STATUS_CACHE_TTL = 600; // 10 minutes.
 
 	/**
 	 * Cache key for the seller status response.
 	 */
-	const SELLER_STATUS_CACHE_KEY = 'seller_status';
+	public const SELLER_STATUS_CACHE_KEY = 'seller_status';
 
 	/**
 	 * PartnersEndpoint constructor.
