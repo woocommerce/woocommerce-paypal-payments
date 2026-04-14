e691d56f37401131428834a2c65d989b7d9afc23
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-04-14 08:27:15 +0000

    [BOT] Add build artifact from dev/PCP-6189-duplicate-woo-commerce-orders-created-when-buyer-switches-payment-method-at-checkout-i-deal-→-credit-card -> PCP-6189-duplicate-woo-commerce-orders-created-when-buyer-switches-payment-method-at-checkout-i-deal-→-credit-card.

---
+++ assets/ppcp-axo-block-js-plugins-PayPalInsightsLoader.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wp-data', 'wp-element', 'wp-plugins'), 'version' => '01292c2cc0f3c1cbb4c2');
+}
--- modules/ppcp-api-client/src/Factory/ProductFactory.php
+++ modules/ppcp-api-client/src/Factory/ProductFactory.php
@@ -1,43 +1,37 @@
 <?php
+
 /**
  * The Product factory.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Factory
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Factory;
 
 use stdClass;
 use WooCommerce\PayPalCommerce\ApiClient\Entity\Product;
 use WooCommerce\PayPalCommerce\ApiClient\Exception\RuntimeException;
-
 /**
  * Class ProductFactory
  */
-class ProductFactory {
-
-	/**
-	 * Creates a Product based off a PayPal response.
-	 *
-	 * @param stdClass $data The JSON object.
-	 *
-	 * @return Product
-	 * @throws RuntimeException When JSON object is malformed.
-	 */
-	public function from_paypal_response( stdClass $data ): Product {
-		if ( ! isset( $data->id ) ) {
-			throw new RuntimeException( 'No id for product given' );
-		}
-		if ( ! isset( $data->name ) ) {
-			throw new RuntimeException( 'No name for product given' );
-		}
-
-		return new Product(
-			$data->id,
-			$data->name,
-			$data->description ?? ''
-		);
-	}
+class ProductFactory
+{
+    /**
+     * Creates a Product based off a PayPal response.
+     *
+     * @param stdClass $data The JSON object.
+     *
+     * @return Product
+     * @throws RuntimeException When JSON object is malformed.
+     */
+    public function from_paypal_response(stdClass $data): Product
+    {
+        if (!isset($data->id)) {
+            throw new RuntimeException('No id for product given');
+        }
+        if (!isset($data->name)) {
+            throw new RuntimeException('No name for product given');
+        }
+        return new Product($data->id, $data->name, $data->description ?? '');
+    }
 }
