dfb33dd9a8ce616cb0129d53bd7f408ef846b359
Philipp Stracker <p.stracker@syde.com>
2026-04-30 14:00:17 +0200

    ♻️ Clean up redundant tests

--- tests/PHPUnit/StoreSync/Config/StoreCurrencyValueTest.php
+++ tests/PHPUnit/StoreSync/Config/StoreCurrencyValueTest.php
@@ -10,29 +10,6 @@ use function Brain\Monkey\Functions\when;
  * @covers \WooCommerce\PayPalCommerce\StoreSync\Config\StoreCurrencyValue
  */
 class StoreCurrencyValueTest extends TestCase {
-
-	/**
-	 * GIVEN the StoreCurrencyValue class with no required constructor arguments
-	 * WHEN the class is instantiated
-	 * THEN an instance is created successfully
-	 */
-	public function test_can_be_instantiated(): void {
-		$testee = new StoreCurrencyValue();
-
-		$this->assertInstanceOf( StoreCurrencyValue::class, $testee );
-	}
-
-	/**
-	 * GIVEN a StoreCurrencyValue instance
-	 * WHEN the value() method is inspected
-	 * THEN it exists and is callable on the public API
-	 */
-	public function test_value_method_is_callable(): void {
-		$testee = new StoreCurrencyValue();
-
-		$this->assertTrue( is_callable( array( $testee, 'value' ) ) );
-	}
-
 	/**
 	 * GIVEN the WooCommerce store is configured with USD as the active currency
 	 * WHEN value() is called
