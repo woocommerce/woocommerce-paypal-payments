555a5961f999ebec74189d8997f437b6c03f768f
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-04 20:50:30 +0000

    [BOT] Add build artifact from dev/PCP-5241-create-pay-pal-settings-importer-class -> PCP-5241-create-pay-pal-settings-importer-class.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInita39f67516f6df923b6f79368c7def9e4
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInita39f67516f6df923b6f79368c7def9e4::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_3fe9b90c8f3187e464473eed6ee1984d130d11f6'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_3fe9b90c8f3187e464473eed6ee1984d130d11f6'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_be69ceb8a6585eae926d92fc8bbc7e0240168921'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_be69ceb8a6585eae926d92fc8bbc7e0240168921'][$fileIdentifier] = true;
 
                 require $file;
             }
