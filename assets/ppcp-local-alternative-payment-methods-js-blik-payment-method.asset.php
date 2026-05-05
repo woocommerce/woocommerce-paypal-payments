1b03e1c333989713dd63541c4f131b46182ccab1
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-05 07:27:47 +0000

    [BOT] Add build artifact from dev/PCP-5937-vault-management-component-fi -> PCP-5937-vault-management-component-fi.

---
+++ assets/ppcp-local-alternative-payment-methods-js-blik-payment-method.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wc-blocks-registry'), 'version' => '13159b565b2f6ea67082');
+}
---
+++ assets/ppcp-paylater-wc-blocks-js-CheckoutPayLaterMessagesBlock-checkout-paylater-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '01f7077b4888278662cb');
+}
