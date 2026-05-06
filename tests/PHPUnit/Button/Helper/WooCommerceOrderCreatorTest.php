e19076c5d6f88dac2e372d8019b4ed5a2176a36d
Philipp Stracker <p.stracker@syde.com>
2026-05-06 13:54:42 +0200

    🎨 Format unit test code

--- tests/PHPUnit/Button/Helper/WooCommerceOrderCreatorTest.php
+++ tests/PHPUnit/Button/Helper/WooCommerceOrderCreatorTest.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare( strict_types = 1 );
 
 namespace WooCommerce\PayPalCommerce\Button\Helper;
 
@@ -16,24 +16,22 @@ use WooCommerce\PayPalCommerce\WcGateway\FundingSource\FundingSourceRenderer;
 use WooCommerce\PayPalCommerce\WcSubscriptions\Helper\SubscriptionHelper;
 use function Brain\Monkey\Functions\expect;
 
-class WooCommerceOrderCreatorTest extends TestCase
-{
+class WooCommerceOrderCreatorTest extends TestCase {
 	/**
 	 * GIVEN a PayPal order with no purchase units (shipping = null)
 	 * AND Brain\Monkey expects apply_filters called once with the filter name, null, $order, $paypal_data
 	 * WHEN get_shipping() is invoked via reflection
 	 * THEN the return value equals $filter_shipping (from the filter)
 	 */
-	public function test_filter_can_override_return_value_of_get_shipping(): void
-	{
-		$order = Mockery::mock(Order::class);
-		$order->shouldReceive('purchase_units')->andReturn([]);
+	public function test_filter_can_override_return_value_of_get_shipping(): void {
+		$order = Mockery::mock( Order::class );
+		$order->shouldReceive( 'purchase_units' )->andReturn( [] );
 
-		$filter_shipping = Mockery::mock(Shipping::class);
+		$filter_shipping = Mockery::mock( Shipping::class );
 
-		$paypal_data = ['key' => 'value'];
+		$paypal_data = [ 'key' => 'value' ];
 
-		expect('apply_filters')
+		expect( 'apply_filters' )
 			->once()
 			->with(
 				'woocommerce_paypal_payments_order_creator_get_shipping',
@@ -41,23 +39,23 @@ class WooCommerceOrderCreatorTest extends TestCase
 				$order,
 				$paypal_data
 			)
-			->andReturn($filter_shipping);
+			->andReturn( $filter_shipping );
 
 		$sut = new WooCommerceOrderCreator(
-			Mockery::mock(FundingSourceRenderer::class),
-			Mockery::mock(SessionHandler::class),
-			Mockery::mock(SubscriptionHelper::class),
-			Mockery::mock(CartDataFactory::class),
-			Mockery::mock(ShippingFactory::class),
-			Mockery::mock(PayerFactory::class)
+			Mockery::mock( FundingSourceRenderer::class ),
+			Mockery::mock( SessionHandler::class ),
+			Mockery::mock( SubscriptionHelper::class ),
+			Mockery::mock( CartDataFactory::class ),
+			Mockery::mock( ShippingFactory::class ),
+			Mockery::mock( PayerFactory::class )
 		);
 
 		// Testing a private method, as we want to confirm the presence and ability of a WP filter.
-		$method = new ReflectionMethod(WooCommerceOrderCreator::class, 'get_shipping');
-		$method->setAccessible(true);
+		$method = new ReflectionMethod( WooCommerceOrderCreator::class, 'get_shipping' );
+		$method->setAccessible( true );
 
-		$result = $method->invoke($sut, $order, $paypal_data);
+		$result = $method->invoke( $sut, $order, $paypal_data );
 
-		$this->assertSame($filter_shipping, $result);
+		$this->assertSame( $filter_shipping, $result );
 	}
 }
