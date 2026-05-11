aded859e02a46b0434dc28d34ea14c46b80b30dc
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-11 15:32:12 +0000

    [BOT] Add build artifact from dev/qa/move-playwright-to-root -> qa/move-playwright-to-root.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInita39f67516f6df923b6f79368c7def9e4
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInita39f67516f6df923b6f79368c7def9e4::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_5399a396ea38be0cba75595a8fd8b08737c149e0'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_5399a396ea38be0cba75595a8fd8b08737c149e0'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_a4f700f6920ede8e662cbbab37b66a31b21fa20d'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_a4f700f6920ede8e662cbbab37b66a31b21fa20d'][$fileIdentifier] = true;
 
                 require $file;
             }
