2715570efbc7abdf0567e233fb7ce4dcdc02e409
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-13 10:32:40 +0000

    [BOT] Add build artifact from dev/PCP-6398-undefined-di-service -> PCP-6398-undefined-di-service.

---
+++ assets/ppcp-axo-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => 'dea15bacac1bdab0d4c6');
+}
---
+++ assets/ppcp-save-payment-methods-js-add-payment-method.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '262bfa4ccde03da1f4b8');
+}
