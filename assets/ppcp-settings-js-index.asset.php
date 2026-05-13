b6be3075b9ae9f9fca62ee312b6169de7d978418
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-13 17:08:03 +0000

    [BOT] Add build artifact from dev/PCP-6405-remove-test-code -> PCP-6405-remove-test-code.

---
+++ assets/ppcp-settings-js-index.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'ad3d6acb5fc70538dc7e');
+}
