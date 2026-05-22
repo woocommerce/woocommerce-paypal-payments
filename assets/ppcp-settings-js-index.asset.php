f2fb0cac90653fc7bfd36fc545204848f824d943
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-22 11:32:07 +0000

    [BOT] Add build artifact from dev/qa/fix-googlepay-test-for-ci -> qa/fix-googlepay-test-for-ci.

---
+++ assets/ppcp-settings-js-index.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '1ac5f52409f533ef75d6');
+}
--- modules/ppcp-blocks/extensions.php
+++ modules/ppcp-blocks/extensions.php
@@ -1,43 +1,26 @@
 <?php
+
 /**
  * The blocks module extensions.
  *
  * @package WooCommerce\PayPalCommerce\Blocks
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Blocks;
 
 use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
-
-return array(
-	'wcgateway.button.locations'                       => function ( array $locations, ContainerInterface $container ): array {
-		return array_merge(
-			$locations,
-			array(
-				'checkout-block-express' => did_action( 'init' ) ? _x( 'Express Checkout', 'Name of Buttons Location', 'woocommerce-paypal-payments' ) : 'Express Checkout',
-				'cart-block'             => did_action( 'init' ) ? _x( 'Cart', 'Name of Buttons Location', 'woocommerce-paypal-payments' ) : 'Cart',
-			)
-		);
-	},
-	'wcgateway.settings.pay-later.messaging-locations' => function ( array $locations, ContainerInterface $container ): array {
-		unset( $locations['checkout-block-express'] );
-		unset( $locations['cart-block'] );
-
-		return $locations;
-	},
-
-	'button.pay-now-contexts'                          => function ( array $contexts, ContainerInterface $container ): array {
-		if ( ! $container->get( 'blocks.settings.final_review_enabled' ) ) {
-			$contexts[] = 'checkout-block';
-			$contexts[] = 'cart-block';
-		}
-
-		return $contexts;
-	},
-
-	'button.handle-shipping-in-paypal'                 => function ( bool $previous, ContainerInterface $container ): bool {
-		return ! $container->get( 'blocks.settings.final_review_enabled' );
-	},
-);
+return array('wcgateway.button.locations' => function (array $locations, ContainerInterface $container): array {
+    return array_merge($locations, array('checkout-block-express' => did_action('init') ? _x('Express Checkout', 'Name of Buttons Location', 'woocommerce-paypal-payments') : 'Express Checkout', 'cart-block' => did_action('init') ? _x('Cart', 'Name of Buttons Location', 'woocommerce-paypal-payments') : 'Cart'));
+}, 'wcgateway.settings.pay-later.messaging-locations' => function (array $locations, ContainerInterface $container): array {
+    unset($locations['checkout-block-express']);
+    unset($locations['cart-block']);
+    return $locations;
+}, 'button.pay-now-contexts' => function (array $contexts, ContainerInterface $container): array {
+    if (!$container->get('blocks.settings.final_review_enabled')) {
+        $contexts[] = 'checkout-block';
+        $contexts[] = 'cart-block';
+    }
+    return $contexts;
+}, 'button.handle-shipping-in-paypal' => function (bool $previous, ContainerInterface $container): bool {
+    return !$container->get('blocks.settings.final_review_enabled');
+});
