ad0294b4b97371409cfc2f9f85a10c46255abf90
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-25 07:57:55 +0000

    [BOT] Add build artifact from dev/qa/fix-googlepay-test-for-ci -> qa/fix-googlepay-test-for-ci.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_e693f8f519f1aca0607cd568ad9870e97ae7b031'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_e693f8f519f1aca0607cd568ad9870e97ae7b031'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_25cdad9936041013dddb60744d124c17227ee60f'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_25cdad9936041013dddb60744d124c17227ee60f'][$fileIdentifier] = true;
 
                 require $file;
             }
