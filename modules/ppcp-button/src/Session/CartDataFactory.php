2cabe0a019f89d8bf613fb8e0ad5ec2c296903bd
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-05-29 10:28:06 +0000

    [BOT] Add build artifact from dev/PCP-6463-negative-fee-on-manually-created-order-influence-on-wrong-total-amount-on-pay-pal-side -> PCP-6463-negative-fee-on-manually-created-order-influence-on-wrong-total-amount-on-pay-pal-side.

--- modules/ppcp-button/src/Session/CartDataFactory.php
+++ modules/ppcp-button/src/Session/CartDataFactory.php
@@ -1,35 +1,28 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Button\Session;
 
 use Exception;
 use WC_Cart;
-
 /**
  * Creates CartData.
  */
-class CartDataFactory {
-	/**
-	 * Creates CartData from the WC cart.
-	 *
-	 * @throws Exception If WC cart is missing.
-	 */
-	public function from_current_cart( ?WC_Cart $cart = null ): CartData {
-		if ( ! $cart ) {
-			$cart = WC()->cart;
-			if ( ! $cart instanceof WC_Cart ) {
-				throw new Exception( 'Cart not found.' );
-			}
-		}
-
-		return new CartData(
-			$cart->get_cart_for_session(),
-			$cart->get_applied_coupons(),
-			$cart->needs_shipping(),
-			get_current_user_id(),
-			$cart->get_cart_hash()
-		);
-	}
+class CartDataFactory
+{
+    /**
+     * Creates CartData from the WC cart.
+     *
+     * @throws Exception If WC cart is missing.
+     */
+    public function from_current_cart(?WC_Cart $cart = null): \WooCommerce\PayPalCommerce\Button\Session\CartData
+    {
+        if (!$cart) {
+            $cart = WC()->cart;
+            if (!$cart instanceof WC_Cart) {
+                throw new Exception('Cart not found.');
+            }
+        }
+        return new \WooCommerce\PayPalCommerce\Button\Session\CartData($cart->get_cart_for_session(), $cart->get_applied_coupons(), $cart->needs_shipping(), get_current_user_id(), $cart->get_cart_hash());
+    }
 }
