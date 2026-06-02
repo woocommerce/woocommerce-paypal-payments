043a067b31fcda5c2b489fdcaf34b5c4d09f9fd7
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-02 08:05:14 +0000

    [BOT] Add build artifact from dev/PCP-6487-apm-s-transaction-link-open-paypal-com-instead-of-sandbox-paypal-com -> PCP-6487-apm-s-transaction-link-open-paypal-com-instead-of-sandbox-paypal-com.

---
+++ assets/ppcp-axo-css-styles.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '01dd772292f5c158d499');
+}
--- modules/ppcp-webhooks/module.php
+++ modules/ppcp-webhooks/module.php
@@ -1,14 +1,13 @@
 <?php
+
 /**
  * The webhook module.
  *
  * @package WooCommerce\PayPalCommerce\Webhooks
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Webhooks;
 
-return static function (): WebhookModule {
-	return new WebhookModule();
+return static function (): \WooCommerce\PayPalCommerce\Webhooks\WebhookModule {
+    return new \WooCommerce\PayPalCommerce\Webhooks\WebhookModule();
 };
