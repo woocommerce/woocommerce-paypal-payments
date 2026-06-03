631feb29f62c6fb964bb77f2209eccd3aa577d62
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-03 10:25:26 +0000

    [BOT] Add build artifact from dev/PCP-5965-feature-flag-and-vault-component-module-setup -> PCP-5965-feature-flag-and-vault-component-module-setup.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_460a112dc6d4aa97cc5ee7ef2a185404786d5f61'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_460a112dc6d4aa97cc5ee7ef2a185404786d5f61'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_5fe93665517a6f0f8a60093b27639aa2263e4fd5'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_5fe93665517a6f0f8a60093b27639aa2263e4fd5'][$fileIdentifier] = true;
 
                 require $file;
             }
