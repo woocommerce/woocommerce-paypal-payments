226a855d65456358136d98765f092dca686b57f3
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-05 13:18:18 +0000

    [BOT] Add build artifact from dev/PCP-6517-mismatch-line-item-not-delivered-to-pay-pal-overview-transaction-page -> PCP-6517-mismatch-line-item-not-delivered-to-pay-pal-overview-transaction-page.

--- modules/ppcp-wc-gateway/src/Endpoint/CaptureCardPayment.php
+++ modules/ppcp-wc-gateway/src/Endpoint/CaptureCardPayment.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\WcGateway\Endpoint;
 
-use Psr\Log\LoggerInterface;
+use WooCommerce\PayPalCommerce\Vendor\Psr\Log\LoggerInterface;
 use RuntimeException;
 use WC_Order;
 use WooCommerce\PayPalCommerce\ApiClient\Authentication\Bearer;
@@ -15,112 +14,69 @@ use WooCommerce\PayPalCommerce\ApiClient\Factory\OrderFactory;
 use WooCommerce\PayPalCommerce\ApiClient\Factory\PurchaseUnitFactory;
 use WooCommerce\PayPalCommerce\Settings\Data\SettingsProvider;
 use WP_Error;
-
-class CaptureCardPayment {
-
-	use RequestTrait;
-
-	/**
-	 * The host.
-	 *
-	 * @var string
-	 */
-	private $host;
-
-	/**
-	 * The bearer.
-	 *
-	 * @var Bearer
-	 */
-	private $bearer;
-
-	/**
-	 * The order factory.
-	 *
-	 * @var OrderFactory
-	 */
-	private $order_factory;
-
-	/**
-	 * The purchase unit factory.
-	 *
-	 * @var PurchaseUnitFactory
-	 */
-	private $purchase_unit_factory;
-
-	private SettingsProvider $settings_provider;
-
-	/**
-	 * The logger.
-	 *
-	 * @var LoggerInterface
-	 */
-	private $logger;
-
-	public function __construct(
-		string $host,
-		Bearer $bearer,
-		OrderFactory $order_factory,
-		PurchaseUnitFactory $purchase_unit_factory,
-		SettingsProvider $settings_provider,
-		LoggerInterface $logger
-	) {
-		$this->host                  = $host;
-		$this->bearer                = $bearer;
-		$this->order_factory         = $order_factory;
-		$this->purchase_unit_factory = $purchase_unit_factory;
-		$this->settings_provider     = $settings_provider;
-		$this->logger                = $logger;
-	}
-
-	/**
-	 * Creates PayPal order from the given card vault id.
-	 *
-	 * @throws RuntimeException When request fails.
-	 */
-	public function create_order( string $vault_id, WC_Order $wc_order ): Order {
-		$intent = strtoupper( $this->settings_provider->payment_intent() ) === 'AUTHORIZE' ? 'AUTHORIZE' : 'CAPTURE';
-		$items  = array( $this->purchase_unit_factory->from_wc_order( $wc_order ) );
-
-		$data = array(
-			'intent'         => $intent,
-			'purchase_units' => array_map(
-				static function ( PurchaseUnit $item ): array {
-					return $item->to_array();
-				},
-				$items
-			),
-			'payment_source' => array(
-				'card' => array(
-					'vault_id'          => $vault_id,
-					'stored_credential' => array(
-						'payment_initiator' => 'CUSTOMER',
-						'payment_type'      => 'UNSCHEDULED',
-						'usage'             => 'SUBSEQUENT',
-					),
-				),
-			),
-		);
-
-		$bearer = $this->bearer->bearer();
-		$url    = trailingslashit( $this->host ) . 'v2/checkout/orders';
-		$args   = array(
-			'method'  => 'POST',
-			'headers' => array(
-				'Authorization'     => 'Bearer ' . $bearer->token(),
-				'Content-Type'      => 'application/json',
-				'PayPal-Request-Id' => uniqid( 'ppcp-', true ),
-			),
-			'body'    => wp_json_encode( $data ),
-		);
-
-		$response = $this->request( $url, $args );
-		if ( $response instanceof WP_Error ) {
-			throw new RuntimeException( $response->get_error_message() );
-		}
-
-		$decoded_response = json_decode( $response['body'] );
-
-		return $this->order_factory->from_paypal_response( $decoded_response );
-	}
+class CaptureCardPayment
+{
+    use RequestTrait;
+    /**
+     * The host.
+     *
+     * @var string
+     */
+    private $host;
+    /**
+     * The bearer.
+     *
+     * @var Bearer
+     */
+    private $bearer;
+    /**
+     * The order factory.
+     *
+     * @var OrderFactory
+     */
+    private $order_factory;
+    /**
+     * The purchase unit factory.
+     *
+     * @var PurchaseUnitFactory
+     */
+    private $purchase_unit_factory;
+    private SettingsProvider $settings_provider;
+    /**
+     * The logger.
+     *
+     * @var LoggerInterface
+     */
+    private $logger;
+    public function __construct(string $host, Bearer $bearer, OrderFactory $order_factory, PurchaseUnitFactory $purchase_unit_factory, SettingsProvider $settings_provider, LoggerInterface $logger)
+    {
+        $this->host = $host;
+        $this->bearer = $bearer;
+        $this->order_factory = $order_factory;
+        $this->purchase_unit_factory = $purchase_unit_factory;
+        $this->settings_provider = $settings_provider;
+        $this->logger = $logger;
+    }
+    /**
+     * Creates PayPal order from the given card vault id.
+     *
+     * @throws RuntimeException When request fails.
+     */
+    public function create_order(string $vault_id, WC_Order $wc_order): Order
+    {
+        $intent = strtoupper($this->settings_provider->payment_intent()) === 'AUTHORIZE' ? 'AUTHORIZE' : 'CAPTURE';
+        $items = array($this->purchase_unit_factory->from_wc_order($wc_order));
+        $data = array('intent' => $intent, 'purchase_units' => array_map(static function (PurchaseUnit $item): array {
+            return $item->to_array();
+        }, $items), 'payment_source' => array('card' => array('vault_id' => $vault_id, 'stored_credential' => array('payment_initiator' => 'CUSTOMER', 'payment_type' => 'UNSCHEDULED', 'usage' => 'SUBSEQUENT'))));
+        $bearer = $this->bearer->bearer();
+        $url = trailingslashit($this->host) . 'v2/checkout/orders';
+        $args = array('method' => 'POST', 'headers' => array('Authorization' => 'Bearer ' . $bearer->token(), 'Content-Type' => 'application/json', 'PayPal-Request-Id' => uniqid('ppcp-', \true)), 'body' => wp_json_encode($data));
+        $response = $this->request($url, $args);
+        if ($response instanceof WP_Error) {
+            throw new RuntimeException($response->get_error_message());
+        }
+        $decoded_response = json_decode($response['body']);
+        return $this->order_factory->from_paypal_response($decoded_response);
+    }
 }
