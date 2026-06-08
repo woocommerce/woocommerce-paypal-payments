b1dca397528221a28185cd3d6107ea63df4965a5
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-08 13:28:11 +0000

    [BOT] Add build artifact from dev/PCP-4672-investigate-options-for-user-facing-notifications-for-onboarding-errors -> PCP-4672-investigate-options-for-user-facing-notifications-for-onboarding-errors.

--- assets/ppcp-settings-js-index.asset.php
+++ assets/ppcp-settings-js-index.asset.php
@@ -1,5 +1,5 @@
 <?php
 
 namespace {
-    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '6b623d4ec80e28db233d');
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'd6a1c7e44a8a52403e1e');
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_4a68024875da317843a2521a032b82819ec2f9b0'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_4a68024875da317843a2521a032b82819ec2f9b0'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_8d4a45afae9bc42fd9af48da815a4222ab3cf63b'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_8d4a45afae9bc42fd9af48da815a4222ab3cf63b'][$fileIdentifier] = true;
 
                 require $file;
             }
