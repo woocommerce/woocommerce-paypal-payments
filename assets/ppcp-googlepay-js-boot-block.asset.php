fab4df8c9f6219c010540726411160f1abc2b08a
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-09 15:22:07 +0000

    [BOT] Add build artifact from dev/PCP-6447-gift-cards-ignored-on-paypal-payment-page -> PCP-6447-gift-cards-ignored-on-paypal-payment-page.

--- assets/ppcp-googlepay-js-boot-block.asset.php
+++ assets/ppcp-googlepay-js-boot-block.asset.php
@@ -1,5 +1,5 @@
 <?php
 
 namespace {
-    return array('dependencies' => array('wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => '920219be5ad7e6ac67a2');
+    return array('dependencies' => array('wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => '085edd5f792d92f9a7c3');
 }
--- assets/ppcp-googlepay-js-boot.asset.php
+++ assets/ppcp-googlepay-js-boot.asset.php
@@ -1,5 +1,5 @@
 <?php
 
 namespace {
-    return array('dependencies' => array(), 'version' => 'e9f96d9ff334236a27ae');
+    return array('dependencies' => array(), 'version' => 'f6d8aa881aad47974565');
 }
