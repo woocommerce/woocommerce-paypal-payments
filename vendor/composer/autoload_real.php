d96a14233b19b5c8634429a771f7ecb2b3bb2c52
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-09 10:54:18 +0000

    [BOT] Add build artifact from dev/PCP-5937-vault-management-component-fi -> PCP-5937-vault-management-component-fi.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_5b77ac9b15e6fb5a5e0e66bc748500bbc7b17655'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_5b77ac9b15e6fb5a5e0e66bc748500bbc7b17655'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_c19a35169b49b6e254e557fd956172842ade5e45'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_c19a35169b49b6e254e557fd956172842ade5e45'][$fileIdentifier] = true;
 
                 require $file;
             }
