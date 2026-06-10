52727364d263662e20d8e6e4603a42474a811485
Emili Castells Guasch <e.castells@inpsyde.com>
2026-06-10 09:24:27 +0200

    Remove unused classes

--- modules/ppcp-vault-component/services.php
+++ modules/ppcp-vault-component/services.php
@@ -5,9 +5,6 @@ namespace WooCommerce\PayPalCommerce\VaultComponent;
 
 use Psr\Log\LoggerInterface;
 use WooCommerce\PayPalCommerce\ApiClient\Authentication\ClientCredentials;
-use WooCommerce\PayPalCommerce\ApiClient\Endpoint\OrderEndpoint;
-use WooCommerce\PayPalCommerce\ApiClient\Factory\PurchaseUnitFactory;
-use WooCommerce\PayPalCommerce\ApiClient\Factory\ShippingPreferenceFactory;
 use WooCommerce\PayPalCommerce\ApiClient\Helper\Cache;
 use WooCommerce\PayPalCommerce\ApiClient\Helper\ReferenceTransactionStatus;
 use WooCommerce\PayPalCommerce\VaultComponent\Authentication\VaultClientToken;
