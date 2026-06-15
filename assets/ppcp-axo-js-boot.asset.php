0b5f61c2a3ba361b2dd882d2ba0a39fe84650b00
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-15 10:33:43 +0000

    [BOT] Add build artifact from dev/qa/remove-gpay-test -> qa/remove-gpay-test.

---
+++ assets/ppcp-axo-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '466e9ae5d65223f9d525');
+}
---
+++ assets/ppcp-button-js-button.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => 'bfb19eae15fa082390c9');
+}
