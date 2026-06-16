7cc2b3327d738c3e2adc4cd64a3377daa044b07b
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-16 17:36:35 +0000

    [BOT] Add build artifact from dev/PCP-6558-fix-continuation-mode -> PCP-6558-fix-continuation-mode.

--- modules/ppcp-api-client/src/Entity/AuthorizationStatus.php
+++ modules/ppcp-api-client/src/Entity/AuthorizationStatus.php
@@ -1,111 +1,92 @@
 <?php
+
 /**
  * The AuthorizationStatus object.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Entity
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Entity;
 
 use WooCommerce\PayPalCommerce\ApiClient\Exception\RuntimeException;
-
 /**
  * Class AuthorizationStatus
  */
-class AuthorizationStatus {
-
-	const INTERNAL           = 'INTERNAL';
-	const CREATED            = 'CREATED';
-	const CAPTURED           = 'CAPTURED';
-	const COMPLETED          = 'COMPLETED';
-	const DENIED             = 'DENIED';
-	const EXPIRED            = 'EXPIRED';
-	const PARTIALLY_CAPTURED = 'PARTIALLY_CAPTURED';
-	const VOIDED             = 'VOIDED';
-	const PENDING            = 'PENDING';
-	const VALID_STATUS       = array(
-		self::INTERNAL,
-		self::CREATED,
-		self::CAPTURED,
-		self::COMPLETED,
-		self::DENIED,
-		self::EXPIRED,
-		self::PARTIALLY_CAPTURED,
-		self::VOIDED,
-		self::PENDING,
-	);
-
-	/**
-	 * The status.
-	 *
-	 * @var string
-	 */
-	private $status;
-
-	/**
-	 * The details.
-	 *
-	 * @var AuthorizationStatusDetails|null
-	 */
-	private $details;
-
-	/**
-	 * AuthorizationStatus constructor.
-	 *
-	 * @param string                          $status The status.
-	 * @param AuthorizationStatusDetails|null $details The details.
-	 * @throws RuntimeException When the status is not valid.
-	 */
-	public function __construct( string $status, ?AuthorizationStatusDetails $details = null ) {
-		if ( ! in_array( $status, self::VALID_STATUS, true ) ) {
-			throw new RuntimeException(
-				sprintf(
-					'%s is not a valid status',
-					$status
-				)
-			);
-		}
-		$this->status  = $status;
-		$this->details = $details;
-	}
-
-	/**
-	 * Returns an AuthorizationStatus as Internal.
-	 *
-	 * @return AuthorizationStatus
-	 */
-	public static function as_internal(): AuthorizationStatus {
-		return new self( self::INTERNAL );
-	}
-
-	/**
-	 * Compares the current status with a given one.
-	 *
-	 * @param string $status The status to compare with.
-	 *
-	 * @return bool
-	 */
-	public function is( string $status ): bool {
-		return $this->status === $status;
-	}
-
-	/**
-	 * Returns the status.
-	 *
-	 * @return string
-	 */
-	public function name(): string {
-		return $this->status;
-	}
-
-	/**
-	 * Returns the details.
-	 *
-	 * @return AuthorizationStatusDetails|null
-	 */
-	public function details(): ?AuthorizationStatusDetails {
-		return $this->details;
-	}
+class AuthorizationStatus
+{
+    const INTERNAL = 'INTERNAL';
+    const CREATED = 'CREATED';
+    const CAPTURED = 'CAPTURED';
+    const COMPLETED = 'COMPLETED';
+    const DENIED = 'DENIED';
+    const EXPIRED = 'EXPIRED';
+    const PARTIALLY_CAPTURED = 'PARTIALLY_CAPTURED';
+    const VOIDED = 'VOIDED';
+    const PENDING = 'PENDING';
+    const VALID_STATUS = array(self::INTERNAL, self::CREATED, self::CAPTURED, self::COMPLETED, self::DENIED, self::EXPIRED, self::PARTIALLY_CAPTURED, self::VOIDED, self::PENDING);
+    /**
+     * The status.
+     *
+     * @var string
+     */
+    private $status;
+    /**
+     * The details.
+     *
+     * @var AuthorizationStatusDetails|null
+     */
+    private $details;
+    /**
+     * AuthorizationStatus constructor.
+     *
+     * @param string                          $status The status.
+     * @param AuthorizationStatusDetails|null $details The details.
+     * @throws RuntimeException When the status is not valid.
+     */
+    public function __construct(string $status, ?\WooCommerce\PayPalCommerce\ApiClient\Entity\AuthorizationStatusDetails $details = null)
+    {
+        if (!in_array($status, self::VALID_STATUS, \true)) {
+            throw new RuntimeException(sprintf('%s is not a valid status', $status));
+        }
+        $this->status = $status;
+        $this->details = $details;
+    }
+    /**
+     * Returns an AuthorizationStatus as Internal.
+     *
+     * @return AuthorizationStatus
+     */
+    public static function as_internal(): \WooCommerce\PayPalCommerce\ApiClient\Entity\AuthorizationStatus
+    {
+        return new self(self::INTERNAL);
+    }
+    /**
+     * Compares the current status with a given one.
+     *
+     * @param string $status The status to compare with.
+     *
+     * @return bool
+     */
+    public function is(string $status): bool
+    {
+        return $this->status === $status;
+    }
+    /**
+     * Returns the status.
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->status;
+    }
+    /**
+     * Returns the details.
+     *
+     * @return AuthorizationStatusDetails|null
+     */
+    public function details(): ?\WooCommerce\PayPalCommerce\ApiClient\Entity\AuthorizationStatusDetails
+    {
+        return $this->details;
+    }
 }
--- src/services.php
+++ src/services.php
@@ -1,12 +1,11 @@
 <?php
+
 /**
  * The plugin module services.
  *
  * @package WooCommerce\PayPalCommerce
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce;
 
 use WooCommerce\PayPalCommerce\Assets\AssetGetterFactory;
@@ -16,48 +15,29 @@ use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Package;
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Properties\PluginProperties;
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Properties\Properties;
 use WooCommerce\PayPalCommerce\Vendor\Psr\Container\ContainerInterface;
-
-return array(
-	'ppcp.asset-version'            => function ( ContainerInterface $container ): string {
-		return $container->get( 'ppcp.plugin-version' );
-	},
-
-	'assets.asset_getter_factory'   => function ( ContainerInterface $container ): AssetGetterFactory {
-		$properties = $container->get( Package::PROPERTIES );
-		assert( $properties instanceof Properties );
-
-		return new AssetGetterFactory(
-			(string) $properties->baseUrl(),
-			$properties->basePath()
-		);
-	},
-
-	'http.redirector'               => function ( ContainerInterface $container ): RedirectorInterface {
-		return new WpRedirector();
-	},
-	'ppcp.plugin-version'           => function ( ContainerInterface $container ): string {
-		/** @var Properties $properties */
-		$properties = $container->get( Package::PROPERTIES );
-
-		return $properties->version();
-	},
-	'ppcp.base-name'                => function ( ContainerInterface $container ): string {
-		/** @var Properties $properties */
-		$properties = $container->get( Package::PROPERTIES );
-
-		return $properties->baseName();
-	},
-	'ppcp.path-to-plugin-folder'    => function ( ContainerInterface $container ): string {
-		/** @var Properties $properties */
-		$properties = $container->get( Package::PROPERTIES );
-
-		return $properties->basePath();
-	},
-	'ppcp.path-to-plugin-main-file' => function ( ContainerInterface $container ): string {
-		/** @var PluginProperties $properties */
-		$properties = $container->get( Package::PROPERTIES );
-
-		/** @psalm-suppress UndefinedInterfaceMethod */
-		return $properties->pluginMainFile();
-	},
-);
+return array('ppcp.asset-version' => function (ContainerInterface $container): string {
+    return $container->get('ppcp.plugin-version');
+}, 'assets.asset_getter_factory' => function (ContainerInterface $container): AssetGetterFactory {
+    $properties = $container->get(Package::PROPERTIES);
+    assert($properties instanceof Properties);
+    return new AssetGetterFactory((string) $properties->baseUrl(), $properties->basePath());
+}, 'http.redirector' => function (ContainerInterface $container): RedirectorInterface {
+    return new WpRedirector();
+}, 'ppcp.plugin-version' => function (ContainerInterface $container): string {
+    /** @var Properties $properties */
+    $properties = $container->get(Package::PROPERTIES);
+    return $properties->version();
+}, 'ppcp.base-name' => function (ContainerInterface $container): string {
+    /** @var Properties $properties */
+    $properties = $container->get(Package::PROPERTIES);
+    return $properties->baseName();
+}, 'ppcp.path-to-plugin-folder' => function (ContainerInterface $container): string {
+    /** @var Properties $properties */
+    $properties = $container->get(Package::PROPERTIES);
+    return $properties->basePath();
+}, 'ppcp.path-to-plugin-main-file' => function (ContainerInterface $container): string {
+    /** @var PluginProperties $properties */
+    $properties = $container->get(Package::PROPERTIES);
+    /** @psalm-suppress UndefinedInterfaceMethod */
+    return $properties->pluginMainFile();
+});
