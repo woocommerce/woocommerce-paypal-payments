3fa072216940fbcee4e51adcbfa96f0fd6dedd94
Misha Utkin <m.utkin@inpsyde.com>
2026-06-18 13:32:33 +0200

    Add order data with negative fees

--- tests/qa/resources/orders.ts
+++ tests/qa/resources/orders.ts
@@ -11,6 +11,24 @@ const country = process.env.WC_DEFAULT_COUNTRY || 'usa';
 const currency = process.env.WC_DEFAULT_CURRENCY || 'USD';
 const merchant = merchants[ country ];
 
+orders.negative12PercentFee = {
+	...orders.default,
+	fees: [ {
+		name: 'negative12PercentFee',
+		total: '-12.00',
+		type: 'percent',
+	} ],
+};
+
+orders.negative15FixedFee = {
+	...orders.default,
+	fees: [ {
+		name: 'negative15FixedFee',
+		total: '-15.00',
+		type: 'fixed',
+	} ],
+};
+
 for ( const order in orders ) {
 	orders[ order ].merchant = merchant;
 	orders[ order ].currency = currency;
