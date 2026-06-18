5532f429d981fb4d88d64d51c6f285eb8fde378d
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-18 14:53:25 +0000

    [BOT] Add build artifact from dev/PCP-6294-mexico-enable-installments-not-visible-on-overview-tab-in-section-feautres -> PCP-6294-mexico-enable-installments-not-visible-on-overview-tab-in-section-feautres.

--- modules/ppcp-api-client/src/Helper/OrderHelper.php
+++ modules/ppcp-api-client/src/Helper/OrderHelper.php
@@ -1,34 +1,32 @@
 <?php
+
 /**
  * PayPal order helper.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Helper
  */
-
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Helper;
 
 use WooCommerce\PayPalCommerce\ApiClient\Entity\Order;
-
 /**
  * Class OrderHelper
  */
-class OrderHelper {
-
-	/**
-	 * Checks if order contains physical goods.
-	 *
-	 * @param Order $order PayPal order.
-	 * @return bool
-	 */
-	public function contains_physical_goods( Order $order ): bool {
-		foreach ( $order->purchase_units() as $unit ) {
-			if ( $unit->contains_physical_goods() ) {
-				return true;
-			}
-		}
-
-		return false;
-	}
+class OrderHelper
+{
+    /**
+     * Checks if order contains physical goods.
+     *
+     * @param Order $order PayPal order.
+     * @return bool
+     */
+    public function contains_physical_goods(Order $order): bool
+    {
+        foreach ($order->purchase_units() as $unit) {
+            if ($unit->contains_physical_goods()) {
+                return \true;
+            }
+        }
+        return \false;
+    }
 }
