22b7229dbb5cb0db09b503103891840c8e21a18e
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-25 11:41:48 +0000

    [BOT] Add build artifact from dev/qa/new-autotests-pcp-6459-pcp-6513 -> qa/new-autotests-pcp-6459-pcp-6513.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_65372f8ba15901fb8fd0b93e37672ed4df9208f2'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_65372f8ba15901fb8fd0b93e37672ed4df9208f2'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_0f300e93c414cb73dab77efe917611204dfd671d'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_0f300e93c414cb73dab77efe917611204dfd671d'][$fileIdentifier] = true;
 
                 require $file;
             }
