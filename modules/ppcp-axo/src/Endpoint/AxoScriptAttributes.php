40f9768987071d592c8907e2e1ec263435b3e1be
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-26 10:18:26 +0000

    [BOT] Add build artifact from dev/PCP-6429-acdc-edit-order-page-processor-response-0000-approved-is-showing-on-edit-order-page -> PCP-6429-acdc-edit-order-page-processor-response-0000-approved-is-showing-on-edit-order-page.

--- modules/ppcp-axo/src/Endpoint/AxoScriptAttributes.php
+++ modules/ppcp-axo/src/Endpoint/AxoScriptAttributes.php
@@ -2,71 +2,52 @@
 
 namespace WooCommerce\PayPalCommerce\Axo\Endpoint;
 
-use Psr\Log\LoggerInterface;
+use WooCommerce\PayPalCommerce\Vendor\Psr\Log\LoggerInterface;
 use WooCommerce\PayPalCommerce\ApiClient\Authentication\SdkClientToken;
 use WooCommerce\PayPalCommerce\ApiClient\Exception\PayPalApiException;
 use WooCommerce\PayPalCommerce\Button\Endpoint\EndpointInterface;
 use WooCommerce\PayPalCommerce\Button\Endpoint\RequestData;
 use WooCommerce\PayPalCommerce\Button\Exception\NonceValidationException;
 use WooCommerce\PayPalCommerce\Button\Helper\Context;
-
 /**
  * Handles the request for the PayPal Axo script attributes.
  */
-class AxoScriptAttributes implements EndpointInterface {
-
-	const ENDPOINT = 'ppc-axo-script-attributes';
-	protected Context $context;
-
-	private RequestData $request_data;
-	private LoggerInterface $logger;
-	private SdkClientToken $sdk_client_token;
-	private bool $axo_eligible;
-
-	public function __construct(
-		RequestData $request_data,
-		LoggerInterface $logger,
-		SdkClientToken $sdk_client_token,
-		bool $axo_eligible,
-		Context $context
-	) {
-		$this->request_data     = $request_data;
-		$this->logger           = $logger;
-		$this->sdk_client_token = $sdk_client_token;
-		$this->axo_eligible     = $axo_eligible;
-		$this->context          = $context;
-	}
-
-	public static function nonce(): string {
-		return self::ENDPOINT;
-	}
-
-	public function handle_request(): void {
-		try {
-			$this->request_data->read_request( $this->nonce() );
-		} catch ( NonceValidationException $error ) {
-			wp_send_json_error( array( 'message' => $error->getMessage() ), 400 );
-		}
-
-		if (
-			! $this->axo_eligible
-			|| is_user_logged_in()
-			|| $this->context->is_paypal_continuation()
-		) {
-			wp_send_json_error( 'Failed to load axo script attributes.' );
-		}
-
-		try {
-			$token = $this->sdk_client_token->sdk_client_token();
-		} catch ( PayPalApiException $exception ) {
-			$this->logger->error( $exception->getMessage() );
-			wp_send_json_error( $exception->getMessage() );
-		}
-
-		wp_send_json_success(
-			array(
-				'sdk_client_token' => $token,
-			)
-		);
-	}
+class AxoScriptAttributes implements EndpointInterface
+{
+    const ENDPOINT = 'ppc-axo-script-attributes';
+    protected Context $context;
+    private RequestData $request_data;
+    private LoggerInterface $logger;
+    private SdkClientToken $sdk_client_token;
+    private bool $axo_eligible;
+    public function __construct(RequestData $request_data, LoggerInterface $logger, SdkClientToken $sdk_client_token, bool $axo_eligible, Context $context)
+    {
+        $this->request_data = $request_data;
+        $this->logger = $logger;
+        $this->sdk_client_token = $sdk_client_token;
+        $this->axo_eligible = $axo_eligible;
+        $this->context = $context;
+    }
+    public static function nonce(): string
+    {
+        return self::ENDPOINT;
+    }
+    public function handle_request(): void
+    {
+        try {
+            $this->request_data->read_request($this->nonce());
+        } catch (NonceValidationException $error) {
+            wp_send_json_error(array('message' => $error->getMessage()), 400);
+        }
+        if (!$this->axo_eligible || is_user_logged_in() || $this->context->is_paypal_continuation()) {
+            wp_send_json_error('Failed to load axo script attributes.');
+        }
+        try {
+            $token = $this->sdk_client_token->sdk_client_token();
+        } catch (PayPalApiException $exception) {
+            $this->logger->error($exception->getMessage());
+            wp_send_json_error($exception->getMessage());
+        }
+        wp_send_json_success(array('sdk_client_token' => $token));
+    }
 }
--- modules/ppcp-button/src/Session/CartDataFactory.php
+++ modules/ppcp-button/src/Session/CartDataFactory.php
@@ -1,41 +1,32 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\Button\Session;
 
 use Exception;
 use WC_Cart;
-
 /**
  * Creates CartData.
  */
-class CartDataFactory {
-	/**
-	 * Creates CartData from the WC cart.
-	 *
-	 * @throws Exception If WC cart is missing.
-	 */
-	public function from_current_cart( ?WC_Cart $cart = null ): CartData {
-		if ( ! $cart ) {
-			$cart = WC()->cart;
-			if ( ! $cart instanceof WC_Cart ) {
-				throw new Exception( 'Cart not found.' );
-			}
-		}
-
-		$cart_data = new CartData(
-			$cart->get_cart_for_session(),
-			$cart->get_applied_coupons(),
-			$cart->needs_shipping(),
-			get_current_user_id(),
-			$cart->get_cart_hash()
-		);
-
-		if ( WC()->session ) {
-			$cart_data->set_session_customer_id( (string) WC()->session->get_customer_id() );
-		}
-
-		return $cart_data;
-	}
+class CartDataFactory
+{
+    /**
+     * Creates CartData from the WC cart.
+     *
+     * @throws Exception If WC cart is missing.
+     */
+    public function from_current_cart(?WC_Cart $cart = null): \WooCommerce\PayPalCommerce\Button\Session\CartData
+    {
+        if (!$cart) {
+            $cart = WC()->cart;
+            if (!$cart instanceof WC_Cart) {
+                throw new Exception('Cart not found.');
+            }
+        }
+        $cart_data = new \WooCommerce\PayPalCommerce\Button\Session\CartData($cart->get_cart_for_session(), $cart->get_applied_coupons(), $cart->needs_shipping(), get_current_user_id(), $cart->get_cart_hash());
+        if (WC()->session) {
+            $cart_data->set_session_customer_id((string) WC()->session->get_customer_id());
+        }
+        return $cart_data;
+    }
 }
