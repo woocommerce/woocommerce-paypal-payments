834560e0aea0fb243f32ebe80ff4ff255f9ef464
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-26 12:44:08 +0000

    [BOT] Add build artifact from dev/PCP-6393-pay-pal-order-amount-tampering-via-missing-ownership-check-issue-16 -> PCP-6393-pay-pal-order-amount-tampering-via-missing-ownership-check-issue-16.

---
+++ assets/ppcp-applepay-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '747d178cb1327bb97e93');
+}
---
+++ assets/ppcp-paylater-wc-blocks-js-CheckoutPayLaterMessagesBlock-checkout-paylater-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2f5756d2c0ee6d0a598b');
+}
