fedd52d5099c666c1f1a2e610954a1eab0584e84
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-06-28 16:44:57 +0000

    [BOT] Add build artifact from dev/PCP-6368-advanced-card-processing-orders-fail-with-order-does-not-contain-an-id-error-user-report -> PCP-6368-advanced-card-processing-orders-fail-with-order-does-not-contain-an-id-error-user-report.

---
+++ assets/ppcp-blocks-js-checkout-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => '51f4338745f1eb00ec65');
+}
---
+++ assets/ppcp-paylater-block-js-paylater-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '115f8b4af9c2fea23c13');
+}
