95a5a56bf38600604e9eda99d063a7c74cd52174
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-07-01 14:06:00 +0000

    [BOT] Add build artifact from dev/PCP-6143-PCP-6220-express-checkout-pay-now-continuation-fix -> PCP-6143-PCP-6220-express-checkout-pay-now-continuation-fix.

---
+++ assets/ppcp-googlepay-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '11e551ef86eda35c357b');
+}
---
+++ assets/ppcp-local-alternative-payment-methods-css-gateway.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '0a07020596bf318b37e6');
+}
