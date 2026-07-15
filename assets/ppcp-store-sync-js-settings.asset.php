bf2b7fcde4fcf4ff0f0c8bbc4d8ed22ae8b24ffa
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-07-15 11:49:08 +0000

    [BOT] Add build artifact from dev/qa/fix-oxxo-tests-pcp-6638 -> qa/fix-oxxo-tests-pcp-6638.

---
+++ assets/ppcp-store-sync-js-settings.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1e4ec403a972b6aacc21');
+}
