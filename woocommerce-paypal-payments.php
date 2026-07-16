33d9fd5d594fe873f28734c868cd9a77fff2f4f8
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-07-16 07:34:37 +0000

    [BOT] Add build artifact from dev/qa/fix-oxxo-tests-pcp-6638 -> qa/fix-oxxo-tests-pcp-6638.

--- woocommerce-paypal-payments.php
+++ woocommerce-paypal-payments.php
@@ -1,22 +1,23 @@
 <?php
 
 /**
- * Plugin Name: WooCommerce PayPal Payments
- * Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
- * Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
- * Version:     4.1.1
- * Author:      PayPal
- * Author URI:  https://paypal.com/
- * License:     GPL-2.0
- * Requires PHP: 7.4
- * Requires Plugins: woocommerce
- * Requires at least: 6.5
- * WC requires at least: 9.6
- * WC tested up to: 10.9
- * Text Domain: woocommerce-paypal-payments
- *
- * @package WooCommerce\PayPalCommerce
- */
+* Plugin Name: WooCommerce PayPal Payments
+* Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
+* Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
+* Version: 0.0.0+qa-fix-oxxo-tests-pcp-6638.46d2693
+SHA: 46d2693fe24f0976480362260962b8db25826756
+* Author:      PayPal
+* Author URI:  https://paypal.com/
+* License:     GPL-2.0
+* Requires PHP: 7.4
+* Requires Plugins: woocommerce
+* Requires at least: 6.5
+* WC requires at least: 9.6
+* WC tested up to: 10.9
+* Text Domain: woocommerce-paypal-payments
+*
+* @package WooCommerce\PayPalCommerce
+*/
 declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce;
 
