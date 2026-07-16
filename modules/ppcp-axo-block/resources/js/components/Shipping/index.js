00bec3b85b03b2718eeaf2fb328d8a2e979ef3c5
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-07-16 12:15:15 +0000

    [BOT] Add build artifact from dev/PCP-6681-saved-pay-pal-token-in-subscription-change-payment-flow-moves-active-subscription-to-pending -> PCP-6681-saved-pay-pal-token-in-subscription-change-payment-flow-moves-active-subscription-to-pending.

--- modules/ppcp-axo-block/resources/js/components/Shipping/index.js
+++
@@ -1,6 +0,0 @@
-export { default as ShippingChangeButton } from './ShippingChangeButton';
-export { default as ShippingChangeButtonManager } from './ShippingChangeButtonManager';
-export {
-	injectShippingChangeButton,
-	removeShippingChangeButton,
-} from './utils';
--- modules/ppcp-vault-component/module.php
+++ modules/ppcp-vault-component/module.php
@@ -1,8 +1,8 @@
 <?php
-declare(strict_types=1);
 
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\VaultComponent;
 
-return static function (): VaultComponentModule {
-	return new VaultComponentModule();
+return static function (): \WooCommerce\PayPalCommerce\VaultComponent\VaultComponentModule {
+    return new \WooCommerce\PayPalCommerce\VaultComponent\VaultComponentModule();
 };
