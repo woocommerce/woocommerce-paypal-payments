1691122f9ee3864117823ca64bae1d79545e5ada
Narek Zakarian <narekzakarian@gmail.com>
2026-07-16 16:16:16 +0400

    style(wc-gateway): remove redundant @param descriptions

--- modules/ppcp-wc-gateway/src/Gateway/PayPalGateway.php
+++ modules/ppcp-wc-gateway/src/Gateway/PayPalGateway.php
@@ -716,8 +716,8 @@ class PayPalGateway extends \WC_Payment_Gateway {
 	/**
 	 * Check whether customer is changing subscription payment.
 	 *
-	 * @param SubscriptionHelper $subscription_helper Subscription helper.
-	 * @param WC_Order           $wc_order WC order.
+	 * @param SubscriptionHelper $subscription_helper
+	 * @param WC_Order           $wc_order
 	 * @return bool
 	 */
 	private function is_customer_changing_subscription_payment( SubscriptionHelper $subscription_helper, WC_Order $wc_order ): bool {
@@ -728,10 +728,10 @@ class PayPalGateway extends \WC_Payment_Gateway {
 	/**
 	 * Adds the given WC payment token into the given WC Order.
 	 *
-	 * @param WC_Order       $wc_order WC order.
-	 * @param int            $wc_payment_token_id WC payment token ID.
-	 * @param string         $return_url Return url.
-	 * @param SessionHandler $session_handler Session handler.
+	 * @param WC_Order       $wc_order
+	 * @param int            $wc_payment_token_id
+	 * @param string         $return_url
+	 * @param SessionHandler $session_handler
 	 * @return array{result: string, redirect: string, errorMessage?: string}
 	 */
 	private function add_payment_token_to_order(
