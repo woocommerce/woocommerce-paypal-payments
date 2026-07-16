b4620f9249ddb97f6c7fb79dcaff731d49b96848
Daniel Dudzic <d.dudzic@syde.com>
2026-07-16 18:26:48 +0200

    refactor: 🚚 Register ppc-update-shipping unconditionally in ppcp-order-endpoints

--- modules/ppcp-order-endpoints/src/OrderEndpointsModule.php
+++ modules/ppcp-order-endpoints/src/OrderEndpointsModule.php
@@ -15,6 +15,7 @@ namespace WooCommerce\PayPalCommerce\OrderEndpoints;
 use WooCommerce\PayPalCommerce\OrderEndpoints\Endpoint\ApproveOrderEndpoint;
 use WooCommerce\PayPalCommerce\OrderEndpoints\Endpoint\ChangeCartEndpoint;
 use WooCommerce\PayPalCommerce\OrderEndpoints\Endpoint\CreateOrderEndpoint;
+use WooCommerce\PayPalCommerce\OrderEndpoints\Endpoint\UpdateShippingEndpoint;
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ExecutableModule;
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ModuleClassNameIdTrait;
 use WooCommerce\PayPalCommerce\Vendor\Inpsyde\Modularity\Module\ServiceModule;
@@ -72,6 +73,16 @@ class OrderEndpointsModule implements ServiceModule, ExecutableModule {
 				 *
 				 * @var CreateOrderEndpoint $endpoint
 				 */
+				$endpoint->handle_request();
+			}
+		);
+
+		add_action(
+			'wc_ajax_' . UpdateShippingEndpoint::ENDPOINT,
+			static function () use ( $c ) {
+				$endpoint = $c->get( 'order-endpoints.endpoint.update-shipping' );
+				assert( $endpoint instanceof UpdateShippingEndpoint );
+
 				$endpoint->handle_request();
 			}
 		);
