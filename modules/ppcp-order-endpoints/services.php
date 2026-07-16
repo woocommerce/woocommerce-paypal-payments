84db390503e99abac7d7e0314781e9daf471503e
Daniel Dudzic <d.dudzic@syde.com>
2026-07-16 16:26:02 +0200

    feat: 🎉 Scaffold neutral ppcp-order-endpoints module

---
+++ modules/ppcp-order-endpoints/services.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The order endpoints module services.
+ *
+ * @package WooCommerce\PayPalCommerce\OrderEndpoints
+ */
+
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\OrderEndpoints;
+
+return array();
