862a2804c9786ac3b091392a48f7120fcb9762cf
Kyrylo <k.braslavskyi@syde.com>
2026-07-21 18:24:51 +0300

    Add product customization detector

---
+++ modules/ppcp-compat/src/Exception/RuntimeException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The modules Runtime Exception.
+ *
+ * @package WooCommerce\PayPalCommerce\Compat\Exception
+ */
+
+declare(strict_types=1);
+
+namespace WooCommerce\PayPalCommerce\Compat\Exception;
+
+/**
+ * Class RuntimeException
+ */
+class RuntimeException extends \RuntimeException {
+
+
+}
