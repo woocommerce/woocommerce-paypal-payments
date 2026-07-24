a2e1a45ea58484055b1e41ce8b9f5b9e153d1496
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-07-24 09:17:42 +0000

    [BOT] Add build artifact from dev/PCP-6714-unsuccessfully-sandbox-onboarding-via-wizard-connecting-under-button-for-live-account -> PCP-6714-unsuccessfully-sandbox-onboarding-via-wizard-connecting-under-button-for-live-account.

---
+++ assets/ppcp-settings-js-index.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '7cd38590741ca8051531');
+}
