d5223a5dd3d1fd9be5d0eb60eb5a21624f6304c0
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-07-24 17:08:06 +0000

    [BOT] Add build artifact from dev/PCP-6719-onboarding-api-error -> PCP-6719-onboarding-api-error.

--- woocommerce-paypal-payments.php
+++ woocommerce-paypal-payments.php
@@ -4,7 +4,7 @@
 * Plugin Name: WooCommerce PayPal Payments
 * Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
 * Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
-* Version: 4.1.1-alpha20260721+pcp-6719-onboarding-api-error.f582f14
+* Version: 4.1.2+PCP-6719
 SHA: f582f1434ff49714a5fa556d63bd06c1dc2a253d
 * Author:      PayPal
 * Author URI:  https://paypal.com/
