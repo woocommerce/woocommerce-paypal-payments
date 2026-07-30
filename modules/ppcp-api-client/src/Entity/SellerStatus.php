2994b14d85c93c7cc3c833aa9b06cd9f620267bb
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-07-30 09:19:31 +0000

    [BOT] Add build artifact from dev/PCP-6731-plugin-calls-vault-api-with-empty-customer-id -> PCP-6731-plugin-calls-vault-api-with-empty-customer-id.

--- modules/ppcp-api-client/src/Entity/SellerStatus.php
+++ modules/ppcp-api-client/src/Entity/SellerStatus.php
@@ -1,119 +1,103 @@
 <?php
+
 /**
  * The seller status entity.
  *
  * @package WooCommerce\PayPalCommerce\ApiClient\Entity
  */
-
-declare( strict_types=1 );
-
+declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce\ApiClient\Entity;
 
 /**
  * Class SellerStatus
  */
-class SellerStatus {
-
-	/**
-	 * The products.
-	 *
-	 * @var SellerStatusProduct[]
-	 */
-	private $products;
-
-	/**
-	 * The capabilities.
-	 *
-	 * @var SellerStatusCapability[]
-	 */
-	private $capabilities;
-
-	/**
-	 * Merchant country on PayPal.
-	 *
-	 * @var string
-	 */
-	private string $country;
-
-	/**
-	 * SellerStatus constructor.
-	 *
-	 * @param SellerStatusProduct[]    $products The products.
-	 * @param SellerStatusCapability[] $capabilities The capabilities.
-	 * @param string                   $country Merchant country on PayPal.
-	 *
-	 * @psalm-suppress RedundantConditionGivenDocblockType
-	 */
-	public function __construct( array $products, array $capabilities, string $country = '' ) {
-		foreach ( $products as $key => $product ) {
-			if ( $product instanceof SellerStatusProduct ) {
-				continue;
-			}
-			unset( $products[ $key ] );
-		}
-		foreach ( $capabilities as $key => $capability ) {
-			if ( $capability instanceof SellerStatusCapability ) {
-				continue;
-			}
-			unset( $capabilities[ $key ] );
-		}
-
-		$this->products     = $products;
-		$this->capabilities = $capabilities;
-		$this->country      = $country;
-	}
-
-	/**
-	 * Returns the products.
-	 *
-	 * @return SellerStatusProduct[]
-	 */
-	public function products(): array {
-		return $this->products;
-	}
-
-	/**
-	 * Returns the capabilities.
-	 *
-	 * @return SellerStatusCapability[]
-	 */
-	public function capabilities(): array {
-		return $this->capabilities;
-	}
-
-	/**
-	 * Returns merchant's country on PayPal.
-	 *
-	 * @return string
-	 */
-	public function country(): string {
-		return $this->country;
-	}
-
-	/**
-	 * Returns the entity as array.
-	 *
-	 * @return array
-	 */
-	public function to_array(): array {
-		$products = array_map(
-			function ( SellerStatusProduct $product ): array {
-				return $product->to_array();
-			},
-			$this->products()
-		);
-
-		$capabilities = array_map(
-			function ( SellerStatusCapability $capability ): array {
-				return $capability->to_array();
-			},
-			$this->capabilities()
-		);
-
-		return array(
-			'products'     => $products,
-			'capabilities' => $capabilities,
-			'country'      => $this->country,
-		);
-	}
+class SellerStatus
+{
+    /**
+     * The products.
+     *
+     * @var SellerStatusProduct[]
+     */
+    private $products;
+    /**
+     * The capabilities.
+     *
+     * @var SellerStatusCapability[]
+     */
+    private $capabilities;
+    /**
+     * Merchant country on PayPal.
+     *
+     * @var string
+     */
+    private string $country;
+    /**
+     * SellerStatus constructor.
+     *
+     * @param SellerStatusProduct[]    $products The products.
+     * @param SellerStatusCapability[] $capabilities The capabilities.
+     * @param string                   $country Merchant country on PayPal.
+     *
+     * @psalm-suppress RedundantConditionGivenDocblockType
+     */
+    public function __construct(array $products, array $capabilities, string $country = '')
+    {
+        foreach ($products as $key => $product) {
+            if ($product instanceof \WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatusProduct) {
+                continue;
+            }
+            unset($products[$key]);
+        }
+        foreach ($capabilities as $key => $capability) {
+            if ($capability instanceof \WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatusCapability) {
+                continue;
+            }
+            unset($capabilities[$key]);
+        }
+        $this->products = $products;
+        $this->capabilities = $capabilities;
+        $this->country = $country;
+    }
+    /**
+     * Returns the products.
+     *
+     * @return SellerStatusProduct[]
+     */
+    public function products(): array
+    {
+        return $this->products;
+    }
+    /**
+     * Returns the capabilities.
+     *
+     * @return SellerStatusCapability[]
+     */
+    public function capabilities(): array
+    {
+        return $this->capabilities;
+    }
+    /**
+     * Returns merchant's country on PayPal.
+     *
+     * @return string
+     */
+    public function country(): string
+    {
+        return $this->country;
+    }
+    /**
+     * Returns the entity as array.
+     *
+     * @return array
+     */
+    public function to_array(): array
+    {
+        $products = array_map(function (\WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatusProduct $product): array {
+            return $product->to_array();
+        }, $this->products());
+        $capabilities = array_map(function (\WooCommerce\PayPalCommerce\ApiClient\Entity\SellerStatusCapability $capability): array {
+            return $capability->to_array();
+        }, $this->capabilities());
+        return array('products' => $products, 'capabilities' => $capabilities, 'country' => $this->country);
+    }
 }
