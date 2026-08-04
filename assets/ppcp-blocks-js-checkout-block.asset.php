7178d8aae2ac8f897af2ba3a0d9f373feb7f5bcd
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-04 10:46:43 +0000

    [BOT] Add build artifact from dev/PCP-6433-free-trial-vaulting-subscription-block-checkout-pay-pal-gateway-does-not-appear -> PCP-6433-free-trial-vaulting-subscription-block-checkout-pay-pal-gateway-does-not-appear.

---
+++ assets/ppcp-blocks-js-checkout-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => 'd9e49f8af46d19ac3059');
+}
---
+++ assets/ppcp-paylater-wc-blocks-js-CheckoutPayLaterMessagesBlock-checkout-paylater-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fe88695f1692977a9b8e');
+}
