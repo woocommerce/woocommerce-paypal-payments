d0d8e1277fa47733d3383fc1b338ccca564251d5
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-13 10:58:03 +0000

    [BOT] Add build artifact from dev/qa/ngrok-for-parallel-tests -> qa/ngrok-for-parallel-tests.

---
+++ assets/ppcp-paylater-wc-blocks-js-CheckoutPayLaterMessagesBlock-checkout-paylater-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6c2cbb06b4d4f9be82fd');
+}
