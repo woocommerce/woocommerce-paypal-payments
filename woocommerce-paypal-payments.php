14d17ba60fbeb7526864381ea873f1f2ee752cf9
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-14 09:23:29 +0000

    [BOT] Add build artifact from dev/develop -> develop.

--- woocommerce-paypal-payments.php
+++ woocommerce-paypal-payments.php
@@ -4,7 +4,8 @@
  * Plugin Name: WooCommerce PayPal Payments
  * Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
  * Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
- * Version:     4.1.2
+ * Version: 4.1.2-alpha20260811+develop.1f005fa
+ * SHA: 1f005fa00e97b979a12a4c873e4e4cd5865f8a7c
  * Author:      PayPal
  * Author URI:  https://paypal.com/
  * License:     GPL-2.0
