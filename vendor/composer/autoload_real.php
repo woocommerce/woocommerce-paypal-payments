7b0f768de726a8a46f31572e4d6bb7a2ccb06bbc
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-14 09:49:58 +0000

    [BOT] Add build artifact from dev/qa/cloudflare-for-parallel-tests -> qa/cloudflare-for-parallel-tests.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_4fbfcd9de83cc25a3f1e88d67a550a214cf24276'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_4fbfcd9de83cc25a3f1e88d67a550a214cf24276'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_7b5af5d19e7dcd41f660eca5fb2abb9d570190f0'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_7b5af5d19e7dcd41f660eca5fb2abb9d570190f0'][$fileIdentifier] = true;
 
                 require $file;
             }
