5ad922ea1aeffd95dbb6ea93c227d4686291d0bd
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-17 18:34:00 +0000

    [BOT] Add build artifact from dev/qa/ngrok-for-parallel-tests -> qa/ngrok-for-parallel-tests.

--- woocommerce-paypal-payments.php
+++ woocommerce-paypal-payments.php
@@ -1,23 +1,22 @@
 <?php
 
 /**
-* Plugin Name: WooCommerce PayPal Payments
-* Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
-* Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
-* Version: 4.1.2-alpha20260811+qa-ngrok-for-parallel-tests.b6b2ff4
-SHA: b6b2ff4d2f3ef549be7c007ebc57475f08da0a95
-* Author:      PayPal
-* Author URI:  https://paypal.com/
-* License:     GPL-2.0
-* Requires PHP: 7.4
-* Requires Plugins: woocommerce
-* Requires at least: 6.5
-* WC requires at least: 9.6
-* WC tested up to: 10.9
-* Text Domain: woocommerce-paypal-payments
-*
-* @package WooCommerce\PayPalCommerce
-*/
+ * Plugin Name: WooCommerce PayPal Payments
+ * Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
+ * Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
+ * Version:     4.1.2
+ * Author:      PayPal
+ * Author URI:  https://paypal.com/
+ * License:     GPL-2.0
+ * Requires PHP: 7.4
+ * Requires Plugins: woocommerce
+ * Requires at least: 6.5
+ * WC requires at least: 9.6
+ * WC tested up to: 10.9
+ * Text Domain: woocommerce-paypal-payments
+ *
+ * @package WooCommerce\PayPalCommerce
+ */
 declare (strict_types=1);
 namespace WooCommerce\PayPalCommerce;
 
