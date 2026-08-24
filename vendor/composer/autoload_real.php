72d8219b1633b00ab669bd4653342b37e6a0a76e
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-24 09:56:56 +0000

    [BOT] Add build artifact from dev/PCP-5788-wc-subscriptions-integration -> PCP-5788-wc-subscriptions-integration.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_bc05eaf3734478b1342700fe2fba80baae2ce621'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_bc05eaf3734478b1342700fe2fba80baae2ce621'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_4b80cbb675085ba5e7585eee085d2d53ca21355a'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_4b80cbb675085ba5e7585eee085d2d53ca21355a'][$fileIdentifier] = true;
 
                 require $file;
             }
