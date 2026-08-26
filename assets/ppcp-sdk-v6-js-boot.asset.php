ed8ffa8ab6c947a944dc021dab1bbdc96c6b8065
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-26 10:45:38 +0000

    [BOT] Add build artifact from dev/PCP-6828-enable-sdk-v-6-feature-flag-for-new-store-setups-non-upgrades -> PCP-6828-enable-sdk-v-6-feature-flag-for-new-store-setups-non-upgrades.

---
+++ assets/ppcp-sdk-v6-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '097f86747ee07a1e79ff');
+}
--- tests/qa/tests/05-transactions/_test-data/pui/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './pui-classic-checkout.data';
-export * from './pui-checkout.data';
-export * from './pui-pay-by-link.data';
