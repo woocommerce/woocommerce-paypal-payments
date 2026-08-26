ec7d94100f35db29c602e09d36a69c6689c920bb
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-26 18:01:27 +0000

    [BOT] Add build artifact from dev/PCP-6784-v6-ui-adjustments -> PCP-6784-v6-ui-adjustments.

---
+++ assets/ppcp-axo-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '563d06c1c93d30f37b12');
+}
---
+++ assets/ppcp-sdk-v6-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => 'd8f522528ac1f3e13745');
+}
