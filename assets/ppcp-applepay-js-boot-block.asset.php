888df6a507d6555ba16382a22ebd319e1807d002
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-27 11:26:23 +0000

    [BOT] Add build artifact from dev/PCP-6780-mobile-pay-pal-payment-fails-to-show-order-confirmation -> PCP-6780-mobile-pay-pal-payment-fails-to-show-order-confirmation.

--- assets/ppcp-applepay-js-boot-block.asset.php
+++ assets/ppcp-applepay-js-boot-block.asset.php
@@ -1,5 +1,5 @@
 <?php
 
 namespace {
-    return array('dependencies' => array('wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => 'bc99e91122d2cfb17b45');
+    return array('dependencies' => array('wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => '07311c7f26a12ad44f75');
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_6922cc1a9e1d397dce2a5337f593c7cc80cf9af3'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_6922cc1a9e1d397dce2a5337f593c7cc80cf9af3'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_5f6df3c377b0cfcf34601c3d6920da69c28f9a23'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_5f6df3c377b0cfcf34601c3d6920da69c28f9a23'][$fileIdentifier] = true;
 
                 require $file;
             }
