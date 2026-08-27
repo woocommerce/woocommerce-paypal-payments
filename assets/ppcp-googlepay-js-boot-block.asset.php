6989ac9ecc552043ad0bea74ed2d9f9303f67164
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-27 11:23:58 +0000

    [BOT] Add build artifact from dev/PCP-6780-mobile-pay-pal-payment-fails-to-show-order-confirmation -> PCP-6780-mobile-pay-pal-payment-fails-to-show-order-confirmation.

---
+++ assets/ppcp-googlepay-js-boot-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => '519422f8674fe0628d7e');
+}
---
+++ assets/ppcp-googlepay-js-boot.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => '22060367bab8ac710aa3');
+}
