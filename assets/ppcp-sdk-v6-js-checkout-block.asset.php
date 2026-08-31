f4507e959fbab8be59da3d7b06e4d2d4477316c2
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-31 14:28:44 +0000

    [BOT] Add build artifact from dev/develop -> develop.

--- assets/ppcp-sdk-v6-js-checkout-block.asset.php
+++ assets/ppcp-sdk-v6-js-checkout-block.asset.php
@@ -1,5 +1,5 @@
 <?php
 
 namespace {
-    return array('dependencies' => array('wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e086dbe6b07c60a8c9b5');
+    return array('dependencies' => array('wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '117cb04b19f652da847a');
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_36d62c625fcdd0041060e7d603388f2f23599e12'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_36d62c625fcdd0041060e7d603388f2f23599e12'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_3a43bd02ff19dc20f35735821a9e8c31f389edb0'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_3a43bd02ff19dc20f35735821a9e8c31f389edb0'][$fileIdentifier] = true;
 
                 require $file;
             }
