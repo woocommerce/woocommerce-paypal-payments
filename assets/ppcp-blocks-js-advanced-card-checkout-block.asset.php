036203729f05cbb7a68fe9cd040db42dde983224
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-08-31 13:29:28 +0000

    [BOT] Add build artifact from dev/PCP-6844-pay-pal-gateway-not-visible-on-block-checkout-when-wc-subscription-auto-renewal-are-enabled -> PCP-6844-pay-pal-gateway-not-visible-on-block-checkout-when-wc-subscription-auto-renewal-are-enabled.

---
+++ assets/ppcp-blocks-js-advanced-card-checkout-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('react', 'wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => 'b3a6149dbfa9568ff295');
+}
---
+++ assets/ppcp-paypal-subscriptions-js-paypal-subscription.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array(), 'version' => 'f0cd222028531935dd8b');
+}
