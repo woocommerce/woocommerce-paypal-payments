f27d063d2642e785705938dd6e99030e52e52fe6
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-03 14:53:27 +0000

    [BOT] Add build artifact from dev/release/4.1.3 -> release/4.1.3.

--- assets/ppcp-sdk-v6-js-checkout-block.asset.php
+++ assets/ppcp-sdk-v6-js-checkout-block.asset.php
@@ -1,5 +1,5 @@
 <?php
 
 namespace {
-    return array('dependencies' => array('wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '80dadd3267d635b40cf7');
+    return array('dependencies' => array('wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '94efe62a934860c9f4e9');
 }
