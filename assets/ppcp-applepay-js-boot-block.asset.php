1a1bc5e099bb1175222da245ac9567e4d44c4fd4
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-04 08:43:38 +0000

    [BOT] Add build artifact from dev/PCP-6886-pay-pal-subscriptions-pay-now-experience-purchase-from-cart-classic-cart-always-fails-with-cart-changed-after-subscription-approval -> PCP-6886-pay-pal-subscriptions-pay-now-experience-purchase-from-cart-classic-cart-always-fails-with-cart-changed-after-subscription-approval.

---
+++ assets/ppcp-applepay-js-boot-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wc-blocks-registry', 'wp-element', 'wp-i18n'), 'version' => 'fdfbac7d6f2a74eb7189');
+}
---
+++ assets/ppcp-sdk-v6-js-checkout-block.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace {
+    return array('dependencies' => array('wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '94efe62a934860c9f4e9');
+}
