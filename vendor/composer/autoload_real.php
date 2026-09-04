1d1a58f09a88e0a5108b133c30503ceace2e3ebb
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-04 15:40:03 +0000

    [BOT] Add build artifact from dev/PCP-6840-bcdc-doesnt-appear-for-mexican-store-vith-sdk-v-6 -> PCP-6840-bcdc-doesnt-appear-for-mexican-store-vith-sdk-v-6.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_eebf2f468ff3c7191508132a7a5efe0c6e7c38ef'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_eebf2f468ff3c7191508132a7a5efe0c6e7c38ef'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_d3458a85a85eef80ca71cff86200ab6bfdbc3ed9'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_d3458a85a85eef80ca71cff86200ab6bfdbc3ed9'][$fileIdentifier] = true;
 
                 require $file;
             }
