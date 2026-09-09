2ad5a089de6e395acf0e034b93f6069798d0f8d3
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-09 13:22:53 +0000

    [BOT] Add build artifact from dev/PCP-5172-update-payment-documentation -> PCP-5172-update-payment-documentation.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_b5cb63da6c2c4c05531ff026daa577b36eccd5cf'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_b5cb63da6c2c4c05531ff026daa577b36eccd5cf'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_b36f1c86e941390fa906fd6951179263e089f3b3'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_b36f1c86e941390fa906fd6951179263e089f3b3'][$fileIdentifier] = true;
 
                 require $file;
             }
