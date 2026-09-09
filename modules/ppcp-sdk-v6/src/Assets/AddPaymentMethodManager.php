f31da0f01144636fbd2fb368c3537fd2fe4c89e1
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-09 12:29:50 +0000

    [BOT] Add build artifact from dev/release/4.1.3 -> release/4.1.3.

--- modules/ppcp-sdk-v6/src/Assets/AddPaymentMethodManager.php
+++ modules/ppcp-sdk-v6/src/Assets/AddPaymentMethodManager.php
@@ -21,7 +21,6 @@ use WooCommerce\PayPalCommerce\SavePaymentMethods\Endpoint\CreatePaymentToken;
 use WooCommerce\PayPalCommerce\SavePaymentMethods\Endpoint\CreateSetupToken;
 use WooCommerce\PayPalCommerce\SdkV6\Endpoint\ClientTokenEndpoint;
 use WooCommerce\PayPalCommerce\SdkV6\Helper\CardFieldStyles;
-use WooCommerce\PayPalCommerce\SdkV6\Helper\MerchantCountrySupport;
 use WooCommerce\PayPalCommerce\Settings\Data\SettingsProvider;
 use WooCommerce\PayPalCommerce\WcGateway\Gateway\CreditCardGateway;
 use WooCommerce\PayPalCommerce\WcGateway\Helper\Environment;
@@ -42,8 +41,7 @@ class AddPaymentMethodManager
     private bool $card_vaulting_enabled;
     private SettingsProvider $settings_provider;
     private CardFieldStyles $card_field_styles;
-    private MerchantCountrySupport $merchant_country_support;
-    public function __construct(AssetGetter $asset_getter, string $version, Environment $environment, Context $context, bool $paypal_vaulting_enabled, bool $card_vaulting_enabled, SettingsProvider $settings_provider, CardFieldStyles $card_field_styles, MerchantCountrySupport $merchant_country_support)
+    public function __construct(AssetGetter $asset_getter, string $version, Environment $environment, Context $context, bool $paypal_vaulting_enabled, bool $card_vaulting_enabled, SettingsProvider $settings_provider, CardFieldStyles $card_field_styles)
     {
         $this->asset_getter = $asset_getter;
         $this->version = $version;
@@ -53,7 +51,6 @@ class AddPaymentMethodManager
         $this->card_vaulting_enabled = $card_vaulting_enabled;
         $this->settings_provider = $settings_provider;
         $this->card_field_styles = $card_field_styles;
-        $this->merchant_country_support = $merchant_country_support;
     }
     /**
      * Enqueues the add-payment-method bootstrap script.
@@ -83,7 +80,7 @@ class AddPaymentMethodManager
      */
     public function should_load_on_current_page(): bool
     {
-        return $this->merchant_country_support->is_supported() && is_user_logged_in() && ($this->paypal_vaulting_enabled || $this->card_vaulting_enabled) && $this->context->is_add_payment_method_page();
+        return is_user_logged_in() && ($this->paypal_vaulting_enabled || $this->card_vaulting_enabled) && $this->context->is_add_payment_method_page();
     }
     /**
      * The configuration data for the add-payment-method bootstrap script.
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_4f5811863dabf72f148256b237ba531bcf136a4c'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_4f5811863dabf72f148256b237ba531bcf136a4c'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_4307230d09bee289b38dd9464c265fd1e0219506'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_4307230d09bee289b38dd9464c265fd1e0219506'][$fileIdentifier] = true;
 
                 require $file;
             }
