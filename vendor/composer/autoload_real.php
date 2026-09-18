40ff57b254aa39f9ab1581a4f43be62f6636207b
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-18 13:10:29 +0000

    [BOT] Add build artifact from dev/PCP-6853-v3-free-trial-subscription-pay-pal-gateway-is-missing-on-block-checkout-and-pay-pal-button-is-missing-on-classic-checkout -> PCP-6853-v3-free-trial-subscription-pay-pal-gateway-is-missing-on-block-checkout-and-pay-pal-button-is-missing-on-classic-checkout.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_4b03bc24d0d2519e6eac9ad91e6b5cb16f09bda8'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_4b03bc24d0d2519e6eac9ad91e6b5cb16f09bda8'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_d6771ee276f636df69ba924e86ca1795abdeb2a7'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_d6771ee276f636df69ba924e86ca1795abdeb2a7'][$fileIdentifier] = true;
 
                 require $file;
             }
