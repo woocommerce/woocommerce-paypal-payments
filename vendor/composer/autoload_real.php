920b72953f8716b6858303bc54e29d706f0eb87b
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-24 12:39:58 +0000

    [BOT] Add build artifact from dev/qa/wildcard-ngrok-for-parallel-tests -> qa/wildcard-ngrok-for-parallel-tests.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_c70e4ffe53484319de6b2cc05937b7cd54453243'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_c70e4ffe53484319de6b2cc05937b7cd54453243'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_7196cec0fdd29853939321d39f059bd7e4c10af1'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_7196cec0fdd29853939321d39f059bd7e4c10af1'][$fileIdentifier] = true;
 
                 require $file;
             }
--- woocommerce-paypal-payments.php
+++ woocommerce-paypal-payments.php
@@ -4,8 +4,8 @@
  * Plugin Name: WooCommerce PayPal Payments
  * Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
  * Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
- * Version: 4.1.3-alpha20260922+qa-wildcard-ngrok-for-parallel-tests.c70e4ff
- * SHA: c70e4ffe53484319de6b2cc05937b7cd54453243
+ * Version: 4.1.3-alpha20260922+qa-wildcard-ngrok-for-parallel-tests.7196cec
+ * SHA: 7196cec0fdd29853939321d39f059bd7e4c10af1
  * Author:      PayPal
  * Author URI:  https://paypal.com/
  * License:     GPL-2.0
