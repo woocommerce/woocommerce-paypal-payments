a19f27803595f4adfa88d4d0cbbf0b406feb9bf5
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-25 14:05:54 +0000

    [BOT] Add build artifact from dev/develop -> develop.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_d6aed56d4598f223d3c3ca4930275d7cd7f71ab3'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_d6aed56d4598f223d3c3ca4930275d7cd7f71ab3'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_852cca9eb19f5e511aed2128a3fd0012ce751bad'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_852cca9eb19f5e511aed2128a3fd0012ce751bad'][$fileIdentifier] = true;
 
                 require $file;
             }
--- woocommerce-paypal-payments.php
+++ woocommerce-paypal-payments.php
@@ -4,8 +4,8 @@
  * Plugin Name: WooCommerce PayPal Payments
  * Plugin URI:  https://woocommerce.com/products/woocommerce-paypal-payments/
  * Description: PayPal's latest complete payments processing solution. Accept PayPal, Pay Later, credit/debit cards, alternative digital wallets local payment types and bank accounts. Turn on only PayPal options or process a full suite of payment methods. Enable global transaction with extensive currency and country coverage.
- * Version: 4.1.3-alpha20260922+develop.d6aed56
- * SHA: d6aed56d4598f223d3c3ca4930275d7cd7f71ab3
+ * Version: 4.1.3-alpha20260922+develop.852cca9
+ * SHA: 852cca9eb19f5e511aed2128a3fd0012ce751bad
  * Author:      PayPal
  * Author URI:  https://paypal.com/
  * License:     GPL-2.0
