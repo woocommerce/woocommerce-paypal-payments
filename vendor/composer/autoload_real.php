501c51c9bad4a034e8ddb8f84481d4e80a7af351
paypal-payments-deploybot <paypal-payments-deploybot@syde.com>
2026-09-25 06:31:01 +0000

    [BOT] Add build artifact from dev/qa/wildcard-ngrok-for-parallel-tests -> qa/wildcard-ngrok-for-parallel-tests.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit844a779aae516bed9e0beccea9d01fda
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit844a779aae516bed9e0beccea9d01fda::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_05ddd95996b0c06850cb3a0f671644f3c651dd9a'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_05ddd95996b0c06850cb3a0f671644f3c651dd9a'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_5ce0d486265c985b2dd3a4ace287535325df5419'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_5ce0d486265c985b2dd3a4ace287535325df5419'][$fileIdentifier] = true;
 
                 require $file;
             }
